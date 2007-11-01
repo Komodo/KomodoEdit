f2c242b131f2a35cb31619853b7ba8a1273e0897
Eric Promislow <ericp@activestate.com>
2007-11-01 22:25:50 +0000

    Bring the mode line back to the top
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@339 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/SciMoz/nsSciMoz.cxx
+++ src/SciMoz/nsSciMoz.cxx
@@ -1,4 +1,5 @@
-/* ***** BEGIN LICENSE BLOCK *****
+/*  -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *  ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  * 
  * The contents of this file are subject to the Mozilla Public License
@@ -33,8 +34,6 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  * 
  * ***** END LICENSE BLOCK ***** */
-
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 //#define SCIMOZ_DEBUG
 
