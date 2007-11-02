53fdc08e90a2144b5fbfb48ef5f9f0988027204c
Eric Promislow <ericp@activestate.com>
2007-11-02 00:58:02 +0000

    no longer needed by the subset of wnd we're using
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@345 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- contrib/pywin/comtypes-0.3.3/comtypes/test/runtests.py
+++
@@ -1,5 +0,0 @@
-import sys
-import comtypes.test
-
-if __name__ == "__main__":
-    sys.exit(comtypes.test.run(sys.argv[1:]))
--- contrib/pywin/comtypes-0.3.3/comtypes/test/setup.py
+++
@@ -1,6 +0,0 @@
-# all the unittests can be converted to exe-files.
-from distutils.core import setup
-import glob
-import py2exe
-
-setup(name='test_*', console=glob.glob("test_*.py"))
