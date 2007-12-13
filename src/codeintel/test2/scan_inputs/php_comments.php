177abb69ec7ba0b97bb7b102854a21125765bc63
Todd Whiteman <toddw@activestate.com>
2007-12-13 20:23:31 +0000

    Integrate change 12471 by toddw from 'devel' branch: ci: php: add test cases for handling multi single line php comments.
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@496 b87d16e7-d29f-4ee1-b5ec-817d645d335f

---
+++ src/codeintel/test2/scan_inputs/php_comments.php
@@ -0,0 +1,15 @@
+<?php
+
+# Function returning a reference:
+#   In this example, the property of the object returned by the find_var
+#   function would be set, not the copy, as it would be without using
+#   reference syntax.
+function ko_testing_php_comments_1() {}
+
+// Function returning a reference:
+//   In this example, the property of the object returned by the find_var
+//   function would be set, not the copy, as it would be without using
+//   reference syntax.
+function ko_testing_php_comments_2() {}
+
+?>
