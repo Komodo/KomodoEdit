228255d9e06a1981e67f03667a2b7e2152009b28
Todd Whiteman <toddw@activestate.com>
2007-12-13 00:41:57 +0000

    Integrate change 12447 by toddw from 'devel' branch: ci: php: update unittest's for testing php references.
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@494 b87d16e7-d29f-4ee1-b5ec-817d645d335f

---
+++ src/codeintel/test2/scan_inputs/php_references.php
@@ -0,0 +1,47 @@
+<?php
+
+// PHP references allow you to make two variables to refer to the same content.
+
+
+// Basic reference assignment
+$b = "string variable";
+$a =& $b;
+
+
+// Pass by reference
+function foo(&$var)
+{
+   $var++;
+}
+
+$c=5; 
+foo($c);
+
+
+// Function returning a reference:
+//   In this example, the property of the object returned by the find_var
+//   function would be set, not the copy, as it would be without using
+//   reference syntax.
+function &find_var($param)
+{
+    return $found_var;
+}
+
+$foo =& find_var($bar);
+$foo->x = 2;
+
+
+// Class reference assignment?
+//$bar =& new fooclass();
+
+
+// Variable in a class using a reference.
+// From bug: http://bugs.activestate.com/show_bug.cgi?id=73819
+class Controller {
+    function Controller()
+    {	
+        $this->load =& load_class('Loader');
+    }
+}
+
+?>
