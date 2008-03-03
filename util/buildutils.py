0af9d0baa40ac870849c51f46b1c9ae3ec10b36e
Trent Mick <trentm@activestate.com>
2008-03-03 17:06:35 +0000

    Integrate change 15638 by trentm from 'devel' branch: Fix "bk package updates". There was a problem in handling version parsing with for a final version number (no 'alpha/beta' part). The actual fix is this part:
        while len(bits) < 6:
            bits.append(None)
    
    which fixes the assumption that there is a full_bits[3] in "short_ver_from_full_ver()"
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@1013 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- util/buildutils.py
+++ util/buildutils.py
@@ -623,17 +623,20 @@ def capture_status(argv):
 
 #---- version string manipulation unexpected
 
-# Recipe: ver (0.4)
+# Recipe: ver (1.1.0)
 def split_full_ver(ver_str):
-    """
-        >>> split_full_ver('4.0.0-alpha3-12345')
-        (4, 0, 0, 'alpha', 3, 12345)
-        >>> split_full_ver('4.1.0-beta-12345')
-        (4, 1, 0, 'beta', None, 12345)
-        >>> split_full_ver('4.1.0-12345')
-        (4, 1, 0, None, None, 12345)
-        >>> split_full_ver('4.1-12345')
-        (4, 1, 0, None, None, 12345)
+    """Split a full version string to component bits.
+
+    >>> split_full_ver('4.0.0-alpha3-12345')
+    (4, 0, 0, 'alpha', 3, 12345)
+    >>> split_full_ver('4.1.0-beta-12345')
+    (4, 1, 0, 'beta', None, 12345)
+    >>> split_full_ver('4.1.0-12345')
+    (4, 1, 0, None, None, 12345)
+    >>> split_full_ver('4.1-12345')
+    (4, 1, 0, None, None, 12345)
+    >>> split_full_ver('4.3.0')
+    (4, 3, 0, None, None, None)
     """
     def _isalpha(ch):
         return 'a' <= ch <= 'z' or 'A' <= ch <= 'Z'
@@ -667,37 +670,81 @@ def split_full_ver(ver_str):
         if i == 0:
             while len(bits) < 3:
                 bits.append(0)
+    
+    while len(bits) < 6:
+        bits.append(None)
+    
     return tuple(bits)
 
-def split_short_ver(ver_str, intify=False):
+_short_ver_re = re.compile("(\d+)(\.\d+)*([a-z](\d+)?)?")
+def split_short_ver(ver_str, intify=False, pad_zeros=None):
     """Parse the given version into a tuple of "significant" parts.
+
+    @param intify {bool} indicates if numeric parts should be converted
+        to integers.
+    @param pad_zeros {int} is a number of numeric parts before any
+        "quality" letter (e.g. 'a' for alpha).
    
-        >>> split_short_ver("4.1.0")
-        ('4', '1', '0')
-        >>> split_short_ver("1.3a2")
-        ('1', '3', 'a', '2')
-        >>> split_short_ver("1.3a2", intify=True)
-        (1, 3, 'a', 2)
+    >>> split_short_ver("4.1.0")
+    ('4', '1', '0')
+    >>> split_short_ver("1.3a2")
+    ('1', '3', 'a', '2')
+    >>> split_short_ver("1.3a2", intify=True)
+    (1, 3, 'a', 2)
+    >>> split_short_ver("1.3a2", intify=True, pad_zeros=3)
+    (1, 3, 0, 'a', 2)
+    >>> split_short_ver("1.3", intify=True, pad_zeros=3)
+    (1, 3, 0)
+    >>> split_short_ver("1", pad_zeros=3)
+    ('1', '0', '0')
     """
-    def _initify(s):
+    def isint(s):
+        try:
+            int(s)
+        except ValueError:
+            return False
+        else:
+            return True
+    def do_intify(s):
         try:
             return int(s)
         except ValueError:
             return s
-    if intify:
-        bits = [_initify(b) for b in re.split("(\.|[a-z])", ver_str)
-                if b != '.']
-    else:
-        bits = [b for b in re.split("(\.|[a-z])", ver_str) if b != '.']
+
+    if not _short_ver_re.match(ver_str):
+        raise ValueError("%r is not a valid short version string" % ver_str)
+
+    hit_quality_bit = False
+    bits = []
+    for bit in re.split("(\.|[a-z])", ver_str):
+        if bit == '.':
+            continue
+        if intify:
+            bit = do_intify(bit)
+        if pad_zeros and not hit_quality_bit and not isint(bit):
+            hit_quality_bit = True
+            while len(bits) < pad_zeros:
+                bits.append(not intify and "0" or 0)
+        bits.append(bit)
+    if pad_zeros and not hit_quality_bit:
+        while len(bits) < pad_zeros:
+            bits.append(not intify and "0" or 0)
     return tuple(bits)
 
-def join_short_ver(ver_tuple):
+def join_short_ver(ver_tuple, pad_zeros=None):
     """Join the given version-tuple, inserting '.' as appropriate.
+
+    @param pad_zeros {int} is a number of numeric parts before any
+        "quality" letter (e.g. 'a' for alpha).
     
-        >>> join_short_ver( ('4', '1', '0') )
-        '4.1.0'
-        >>> join_short_ver( ('1', '3', 'a', '2') )
-        '1.3a2'
+    >>> join_short_ver( ('4', '1', '0') )
+    '4.1.0'
+    >>> join_short_ver( ('1', '3', 'a', '2') )
+    '1.3a2'
+    >>> join_short_ver(('1', '3', 'a', '2'), pad_zeros=3)
+    '1.3.0a2'
+    >>> join_short_ver(('1', '3'), pad_zeros=3)
+    '1.3.0'
     """
     def isint(s):
         try:
@@ -707,8 +754,23 @@ def join_short_ver(ver_tuple):
         else:
             return True
 
+    if pad_zeros:
+        bits = []
+        hit_quality_bit = False
+        for bit in ver_tuple:
+            if not hit_quality_bit and not isint(bit):
+                hit_quality_bit = True
+                while len(bits) < pad_zeros:
+                    bits.append(0)
+            bits.append(bit)
+        if not hit_quality_bit:
+            while len(bits) < pad_zeros:
+                bits.append(0)
+    else:
+        bits = ver_tuple
+
     dotted = []
-    for bit in ver_tuple:
+    for bit in bits:
         if dotted and isint(dotted[-1]) and isint(bit):
             dotted.append('.')
         dotted.append(str(bit))
