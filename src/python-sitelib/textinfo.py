7c0e4f1a6110790323b1e5abb7b8c069b313d511
Trent Mick <trentm@activestate.com>
2008-03-19 18:56:58 +0000

    Integrate change 16580 by trentm from komodo (trunk): fix: "Find in Files" failure when hitting certain XML files http://bugs.activestate.com/show_bug.cgi?id=75631
    r=ericp
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@1179 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/python-sitelib/textinfo.py
+++ src/python-sitelib/textinfo.py
@@ -774,8 +774,9 @@ class TextInfo(object):
         # Try to extract more info from the prolog.
         match = self._xml_prolog_pat.match(head_bytes)
         if not match:
-            log.debug("`%s': could not match XML prolog: '%s'", self.path,
-                      _one_line_summary_from_text(head_bytes, 40))
+            if log.isEnabledFor(logging.DEBUG):
+                log.debug("`%s': could not match XML prolog: '%s'", self.path,
+                          _one_line_summary_from_text(head_bytes, 40))
             return (False, None, None)
         xml_version = match.group("ver")
         xml_encoding = match.group("enc")
@@ -1444,6 +1445,83 @@ def _regex_from_encoded_pattern(s):
         return re.compile(s[1:idx], flags)
     else: # not an encoded regex
         return re.compile(re.escape(s))
+
+# Recipe: text_escape (0.2)
+def _escaped_text_from_text(text, escapes="eol"):
+    r"""Return escaped version of text.
+
+        "escapes" is either a mapping of chars in the source text to
+            replacement text for each such char or one of a set of
+            strings identifying a particular escape style:
+                eol
+                    replace EOL chars with '\r' and '\n', maintain the actual
+                    EOLs though too
+                whitespace
+                    replace EOL chars as above, tabs with '\t' and spaces
+                    with periods ('.')
+                eol-one-line
+                    replace EOL chars with '\r' and '\n'
+                whitespace-one-line
+                    replace EOL chars as above, tabs with '\t' and spaces
+                    with periods ('.')
+    """
+    #TODO:
+    # - Add 'c-string' style.
+    # - Add _escaped_html_from_text() with a similar call sig.
+    import re
+    
+    if isinstance(escapes, basestring):
+        if escapes == "eol":
+            escapes = {'\r\n': "\\r\\n\r\n", '\n': "\\n\n", '\r': "\\r\r"}
+        elif escapes == "whitespace":
+            escapes = {'\r\n': "\\r\\n\r\n", '\n': "\\n\n", '\r': "\\r\r",
+                       '\t': "\\t", ' ': "."}
+        elif escapes == "eol-one-line":
+            escapes = {'\n': "\\n", '\r': "\\r"}
+        elif escapes == "whitespace-one-line":
+            escapes = {'\n': "\\n", '\r': "\\r", '\t': "\\t", ' ': '.'}
+        else:
+            raise ValueError("unknown text escape style: %r" % escapes)
+
+    # Sort longer replacements first to allow, e.g. '\r\n' to beat '\r' and
+    # '\n'.
+    escapes_keys = escapes.keys()
+    try:
+        escapes_keys.sort(key=lambda a: len(a), reverse=True)
+    except TypeError:
+        # Python 2.3 support: sort() takes no keyword arguments
+        escapes_keys.sort(lambda a,b: cmp(len(a), len(b)))
+        escapes_keys.reverse()
+    def repl(match):
+        val = escapes[match.group(0)]
+        return val
+    escaped = re.sub("(%s)" % '|'.join([re.escape(k) for k in escapes_keys]),
+                     repl,
+                     text)
+
+    return escaped
+
+
+def _one_line_summary_from_text(text, length=78,
+        escapes={'\n':"\\n", '\r':"\\r", '\t':"\\t"}):
+    r"""Summarize the given text with one line of the given length.
+    
+        "text" is the text to summarize
+        "length" (default 78) is the max length for the summary
+        "escapes" is a mapping of chars in the source text to
+            replacement text for each such char. By default '\r', '\n'
+            and '\t' are escaped with their '\'-escaped repr.
+    """
+    if len(text) > length:
+        head = text[:length-3]
+    else:
+        head = text
+    escaped = _escaped_text_from_text(head, escapes)
+    if len(text) > length:
+        summary = escaped[:length-3] + "..."
+    else:
+        summary = escaped
+    return summary
 
 
 # Recipe: paths_from_path_patterns (0.5)
