3709cb4ec7f7691b2d8903f5362c253bc8a000b7
Todd Whiteman <toddw@activestate.com>
2008-05-01 22:28:03 +0000

    ci: javascript tests: update test cases for proper javascript argument handling
    (integrated from komodo (trunk) change 18252 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@1327 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/codeintel/test2/scan_inputs/javascript_bug75068.js
+++ src/codeintel/test2/scan_inputs/javascript_bug75068.js
@@ -1,12 +1,20 @@
 // Cile tests for bug:
 // http://bugs.activestate.com/show_bug.cgi?id=75068
 
+//case 0:
+function case0(){
+        var case0_xyz;
+        var b = function b(){
+                case0_xyz=2;
+        };
+}
+
 //case 1:
 function case1(){
         var case1_xyz;
         var c=(function b(){
                 case1_xyz=2;
-        })
+        });
 }
 
 //case 2:
@@ -14,7 +22,7 @@ function case2(){
         var case2_xyz;
         func(function(){
                 case2_xyz=2;
-        })
+        });
 }
 
 //case 3:
@@ -24,4 +32,16 @@ function case3(case3_xyz){
                         case3_xyz = 1;
                 }
         })();
+}
+
+//case 4:
+function case4(case4_xyz){
+        func(function(){
+                case4_xyz=2;
+        });
+}
+
+// case 5:
+function case5(case5_xyz) {
+        case1(function b(){ case5_xyz=2; });
 }
