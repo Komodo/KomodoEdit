694c94c1384b14d476338f5cde829875812fe147
Trent Mick <trentm@activestate.com>
2008-05-22 21:54:31 +0000

    de-crimper'ing. No functional change.
    (integrated from komodo (trunk) change 19070 by trentm)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@1428 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/components/koInitService.unprocessed.py
+++ src/components/koInitService.unprocessed.py
@@ -164,34 +164,7 @@ def _memory_import(name, *args, **kw):
 #----- interal support routines
 
 def _isdir(dirname):
-    """os.path.isdir() doesn't work for UNC mount points. Fake it.
-    
-    # For an existing mount point (want: _isdir() == 1)
-    os.path.ismount(r"\\crimper\apps") -> 1
-    os.path.exists(r"\\crimper\apps") -> 0
-    os.path.isdir(r"\\crimper\apps") -> 0
-    os.listdir(r"\\crimper\apps") -> [...contents...]
-    # For a non-existant mount point (want: _isdir() == 0)
-    os.path.ismount(r"\\crimper\foo") -> 1
-    os.path.exists(r"\\crimper\foo") -> 0
-    os.path.isdir(r"\\crimper\foo") -> 0
-    os.listdir(r"\\crimper\foo") -> WindowsError
-    # For an existing dir under a mount point (want: _isdir() == 1)
-    os.path.mount(r"\\crimper\apps\Komodo") -> 0
-    os.path.exists(r"\\crimper\apps\Komodo") -> 1
-    os.path.isdir(r"\\crimper\apps\Komodo") -> 1
-    os.listdir(r"\\crimper\apps\Komodo") -> [...contents...]
-    # For a non-existant dir/file under a mount point (want: _isdir() == 0)
-    os.path.ismount(r"\\crimper\apps\foo") -> 0
-    os.path.exists(r"\\crimper\apps\foo") -> 0
-    os.path.isdir(r"\\crimper\apps\foo") -> 0
-    os.listdir(r"\\crimper\apps\foo") -> []  # as if empty contents
-    # For an existing file under a mount point (want: _isdir() == 0)
-    os.path.ismount(r"\\crimper\apps\Komodo\latest.komodo-devel.txt") -> 0
-    os.path.exists(r"\\crimper\apps\Komodo\latest.komodo-devel.txt") -> 1
-    os.path.isdir(r"\\crimper\apps\Komodo\latest.komodo-devel.txt") -> 0
-    os.listdir(r"\\crimper\apps\Komodo\latest.komodo-devel.txt") -> WindowsError
-    """
+    """os.path.isdir() doesn't work for UNC mount points. Fake it."""
     if sys.platform[:3] == 'win' and dirname[:2] == r'\\':
         if os.path.exists(dirname):
             return os.path.isdir(dirname)
