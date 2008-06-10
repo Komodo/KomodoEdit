cfe1dd91a98a78b001037035d577740819530809
Trent Mick <trentm@activestate.com>
2008-06-10 19:48:49 +0000

    mk lscolornames: Little task to list the color names used in the mozilla toolkit themes CSS
    (integrated from komodo (trunk) change 19770 by trentm)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@1534 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- Makefile.py
+++ Makefile.py
@@ -15,6 +15,7 @@ from pprint import pprint
 
 from mklib import Task, Configuration, Alias, include
 from mklib import sh
+from mklib import utils
 from mklib.common import MkError
 
 sys.path.insert(0, join(dirname(__file__), "util"))
@@ -22,6 +23,10 @@ import buildsupport
 
 
 include("util/checktasks.py", ns="check")
+
+
+class cfg(Configuration):
+    prefix = "bk"
 
 
 class todo(Task):
@@ -37,4 +42,31 @@ class todo(Task):
     def _dump_pattern_in_path(self, pattern, path):
         os.system('grep -nH "%s" "%s"' % (pattern, path))
 
+
+class lscolornames(Task):
+    """List color names used in the Mozilla toolkit themes."""
+    def make(self):
+        print self.cfg.mozBin
+        print self.cfg.mozSrc
+        themes_dir = join(self.cfg.mozSrc, "mozilla", "toolkit", "themes")
+        
+        color_names = set()
+        for css_path in utils.paths_from_path_patterns(
+                            [themes_dir], includes=["*.css"]):
+            for color_name in self._color_names_from_css_path(css_path):
+                if color_name not in color_names:
+                    color_names.add(color_name)
+                    print color_name
+
+    # This doesn't get colors listed in groups CSS productions like
+    #   border: 1px solid black;
+    _css_color_re = re.compile("(?:\w-)?color\s*: (?P<color>.*?)[;}]")
+    def _color_names_from_css_path(self, css_path):
+        #print css_path
+        css = open(css_path, 'r').read()
+        for hit in self._css_color_re.findall(css):
+            color = hit.strip()
+            if color.endswith("!important"):
+                color = color.rsplit(None, 1)[0]
+            yield color
 
