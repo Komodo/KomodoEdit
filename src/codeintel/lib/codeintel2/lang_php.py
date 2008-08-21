fb82e4fb6019cdef96dfa5514db192fcd34c9a40
Trent Mick <trentm@activestate.com>
2008-08-21 20:36:59 +0000

    Fix some more "fillup" issues. http://bugs.activestate.com/show_bug.cgi?id=78632#c4
    (integrated from komodo (trunk) change 22425 by trentm)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@2039 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/codeintel/lib/codeintel2/lang_php.py
+++ src/codeintel/lib/codeintel2/lang_php.py
@@ -1311,10 +1311,13 @@ class PHPBuffer(UDLBuffer, XMLParsingBufferMixin):
     # - dropped '/', it gets in the way of "</" closing an XML/HTML tag
     # - dropped ' ' It gets in the way of common usage: "function ". This char
     #               could be re-instated once bug 77532 is fixed.
+    # - dropped '!' It gets in the way of "<!" in XML/HTML tag (bug 78632)
+    # - dropped '=' It gets in the way of "<a href=" in XML/HTML cpln (bug 78632)
+    # - dropped ':' It gets in the way of "<a d:blah=" in XML/HTML cpln
     # Bug references:
     #   http://bugs.activestate.com/show_bug.cgi?id=77524
     #   http://bugs.activestate.com/show_bug.cgi?id=77532
-    cpln_fillup_chars = "~`!%^&*()-=+{}[]|;:'\",.<>"
+    cpln_fillup_chars = "~`%^&*()-+{}[]|;'\",.<>"
     #TODO: c.f. cpln_stop_chars stuff in lang_html.py
     # - dropping '[' because need for "<!<|>" -> "<![CDATA[" cpln
     # - dropping '#' because we need it for $form['#foo']
