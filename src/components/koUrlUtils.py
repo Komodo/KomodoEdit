e0a25a731fac917b020843f27674864a10536d1f
Eric Promislow <ericp@activestate.com>
2009-02-04 01:16:51 +0000

    Fixed the class so on Windows pipes are mapped to colons, and the leading slash is stripped off of the path. Code currently not used by Komodo core.
    (integrated from komodo (trunk) change 26297 by ericp)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@2923 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/components/koUrlUtils.py
+++ src/components/koUrlUtils.py
@@ -45,17 +45,36 @@ import logging
 log = logging.getLogger("koUrlUtils")
 
 
+# 2009-02-03: No Komodo core code is currently using this class.
 class koUrlUtils:
     _com_interfaces_ = [components.interfaces.koIUrlUtils]
     _reg_clsid_ = "{0f5df15b-88fd-4b3e-b8cc-e8f001d845fd}"
     _reg_contractid_ = "@activestate.com/koUrlUtils;1"
     _reg_desc_ = "Dumping ground for URL-related utility functions"
+            
+    if sys.platform == "win32":
+        def _fixPath(self, url):
+            path = urlparse.urlparse(url)[2]
+            # URLs come from different sources, and sometimes use "|" instead of ":"
+            # Also Python's urlparse.urlparse puts a '/' at the start of paths,
+            # for URIs that start with the correct "file:///C:...",
+            # and behave correctly for two-slashes ("file://C:...")
+            if len(path) > 3 and path[0] == '/' and path[2] in ":|":
+                # e.g. map "/C|" to "C:"
+                # Assume path[1] is a letter
+                path = path[1] + ":" + path[3:]
+            return os.path.normpath(os.path.normcase(path))
+    else:
+        def _fixPath(self, url):
+            # No need to normcase OSX paths,
+            # because os.path.samefile will correctly ignore case.
+            return os.path.normpath(urlparse.urlparse(url)[2])
 
     def sameURL(self, url1, url2):
         if url1 and url2 and url1.startswith('file:///') and url2.startswith('file:///'):
-            path1 = os.path.normpath(os.path.normcase(urlparse.urlparse(url1)[2]))
-            path2 = os.path.normpath(os.path.normcase(urlparse.urlparse(url2)[2]))
-
+            path1 = self._fixPath(url1)
+            path2 = self._fixPath(url2)
+            
             if sys.platform.startswith("win"):
                 return path1 == path2            
             else:
