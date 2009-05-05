8784b767d3032fa2278e258e15f2825520eb12d7
Todd Whiteman <toddw@activestate.com>
2009-05-05 17:50:59 +0000

    tests: move file status service tests to the "bk test" directory.
    (integrated from komodo (trunk) change 28588 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@3611 b87d16e7-d29f-4ee1-b5ec-817d645d335f

---
+++ test/pyxpcom/test_koFileStatusService.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2000-2009 ActiveState Software Inc.
+# See the file LICENSE.txt for licensing information.
+
+
+import os
+import unittest
+import sys, time
+
+from xpcom import components, COMException
+from xpcom.server import WrapObject, UnwrapObject
+
+import threading
+
+class TestKoFileStatusServiceObserver:
+    _com_interfaces_ = [components.interfaces.nsIObserver]
+    
+    def __init__(self):
+        self.lock = threading.Condition()
+        self.clear()
+        observerSvc = components.classes["@mozilla.org/observer-service;1"].\
+            getService(components.interfaces.nsIObserverService)
+        self.__wrappedSelf = WrapObject(self, components.interfaces.nsIObserver)
+        observerSvc.addObserver(self.__wrappedSelf, 'file_status', 0)
+
+    def __del__(self):
+        observerSvc = components.classes["@mozilla.org/observer-service;1"].\
+            getService(components.interfaces.nsIObserverService)
+        observerSvc.removeObserver(self.__wrappedSelf, 'file_status')
+
+    def clear(self):
+        self.lock.acquire()
+        try:
+            self.subject = None
+            self.topic = None
+            self.updated_uris = set()
+        finally:
+            self.lock.release()
+
+    def observe(self, subject, topic, data):
+        #print "got observe (%s,%s,%s)"%(subject, topic, URI)
+        self.lock.acquire()
+        try:
+            self.subject = subject
+            self.topic = topic
+            for URI in data.split("\n"):
+                print "Got a file_status notification: %r" % (URI, )
+                self.updated_uris.add(URI)
+            self.lock.notifyAll()
+        finally:
+            self.lock.release()
+
+class TestKoFileStatusService(unittest.TestCase):
+    def __init__(self, methodName):
+        unittest.TestCase.__init__(self, methodName)
+        self.__obs = TestKoFileStatusServiceObserver()
+        self.__fileStatusSvc = components.classes["@activestate.com/koFileStatusService;1"] \
+                         .getService(components.interfaces.koIFileStatusService)
+        from xpcom.server import UnwrapObject
+        UnwrapObject(self.__fileStatusSvc).init()
+        self.__filesvc = components.classes["@activestate.com/koFileService;1"] \
+                         .getService(components.interfaces.koIFileService)
+
+    def __del__(self):
+        self.__fileStatusSvc.unload()
+
+    def setUp(self):
+        self.__obs.clear()
+
+    def test_service(self):
+        if sys.platform.startswith("win"):
+            uri = "file:///" + os.path.abspath(__file__).replace("\\", "/")
+        else:
+            uri = "file://" + os.path.abspath(__file__)
+        file = self.__filesvc.getFileFromURI(uri)
+        assert file.exists
+        self.__fileStatusSvc.updateStatusForFiles([file], False)
+        # Give some time to get the status.
+        self.__obs.lock.acquire()
+        try:
+            # XXX: The observer does not work in a standalone environment,
+            #      not sure why, so this will *always* wait 3 seconds.
+            self.__obs.lock.wait(3)
+        finally:
+            self.__obs.lock.release()
+        assert file.sccDirType == 'svn'
+        assert file.sccType == 'svn'
+
+#---- mainline
+
+def suite():
+    return unittest.makeSuite(TestKoFileStatusService)
+
+def test_main():
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())
+
+if __name__ == "__main__":
+    __file__ = os.path.abspath(sys.argv[0]) # won't be necessary in Python 2.3
+    test_main()
