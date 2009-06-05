824c51427d1f4a13ee72c7fe7d3caadb25dad53a
Trent Mick <trentm@activestate.com>
2009-06-05 19:36:38 +0000

    fix the bz2 function name
    (integrated from komodo (trunk) change 29792 by trentm)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@3728 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- util/mozupdate.py
+++ util/mozupdate.py
@@ -751,7 +751,7 @@ def bzip_uncompress_file(src_path, dst_path):
     """Uncompress the source file using bunzip2 and write to the destination."""
     st_mode = os.stat(src_path).st_mode
     data = open(src_path, "rb").read()
-    open(dst_path, "wb").write(bz2.uncompress(data))
+    open(dst_path, "wb").write(bz2.decompress(data))
     os.chmod(dst_path, st_mode)
 
 def _od_path_from_path(path, od_opts=["-c"]):
