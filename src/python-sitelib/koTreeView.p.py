a1006ac5a47b8b6e85e8ae3b98fda1740da62314
Trent Mick <trentm@activestate.com>
2009-06-27 00:01:00 +0000

    drop unneeded comments and normalize a bit. no functional change.
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@3814 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/python-sitelib/koTreeView.p.py
+++ src/python-sitelib/koTreeView.p.py
@@ -44,10 +44,8 @@ class TreeView(object):
     """A base implementation of the nsITreeView.
     
     This should be subclassed to get any useful behaviour.
-    http://www.xulplanet.com/references/xpcomref/ifaces/nsITreeView.html    
+    https://developer.mozilla.org/en/NsITreeView
     """
-    #XXX This should be placed in an 'tree.py' or something with
-    #    documentation and examples of use.
     _com_interfaces_ = [components.interfaces.nsITreeView]
         
     def __init__(self, debug=None):
@@ -60,63 +58,39 @@ class TreeView(object):
         self._tree = None       # set in .setTree()
 
     def isSeparator(self, index):
-        return 0
-    
-    def get_rowCount( self ):
-        # Result: int32
+        return False
+    def get_rowCount(self):
         if self.log:
             self.log.debug("get_rowCount()")
-        return 0
-    def getRowProperties( self, index, properties ):
-        # Result: void - None
-        # In: param0: int32
-        # In: param1: nsISupportsArray
+        return False
+    def getRowProperties(self, index, properties):
         if self.log:
             self.log.debug("getRowProperties(%s, %r)", index, properties)
         pass
     def getCellProperties(self, row, column, properties):
-        # Result: void - None
-        # In: param0: int32
-        # In: param1: wstring
-        # In: param2: nsISupportsArray
         if self.log:
             self.log.debug("getCellProperties()")
         pass
-    # in nsITreeColumn col, in nsISupportsArray properties
-    def getColumnProperties(self, 
-                            column,
-                            properties):
-        # Result: void - None
-        # In: param0: wstring
-        # In: param1: nsIDOMElement
-        # In: param2: nsISupportsArray
+    def getColumnProperties(self, column, properties):
         if self.log:
             self.log.debug("getColumnProperties(column=%s, props=%r)",
-                           column, properties)
-
+                column, properties)
     def isContainer(self, index):
-        # Result: boolean
-        # In: param0: int32
         if self.log:
             self.log.debug("isContainer()")
-        return 0
+        return False
     def isContainerOpen(self, index):
-        # Result: boolean
-        # In: param0: int32
         if self.log:
             self.log.debug("isContainerOpen()")
-        return 0
-    def isContainerEmpty( self, param0 ):
-        # Result: boolean
-        # In: param0: int32
+        return False
+    def isContainerEmpty(self, param0):
         if self.log:
             self.log.debug("isContainerEmpty()")
-        return 0
-    def isSorted( self ):
-        # Result: boolean
+        return False
+    def isSorted(self):
         if self.log:
             self.log.debug("isSorted()")
-        return 0
+        return False
     def canDrop(self, index, orientation):
         if self.log:
             self.log.debug("canDrop()")
@@ -125,22 +99,15 @@ class TreeView(object):
         if self.log:
             self.log.debug("drop()")
         pass
-    def getParentIndex( self, param0 ):
-        # Result: int32
-        # In: param0: int32
+    def getParentIndex(self, param0):
         if self.log:
             self.log.debug("getParentIndex()")
         return -1
-    def hasNextSibling( self, param0, param1 ):
-        # Result: boolean
-        # In: param0: int32
-        # In: param1: int32
+    def hasNextSibling(self, param0, param1):
         if self.log:
             self.log.debug("hasNextSibling()")
         return 0
     def getLevel(self, index):
-        # Result: int32
-        # In: param0: int32
         if self.log:
             self.log.debug("getLevel()")
         return 0
@@ -149,64 +116,38 @@ class TreeView(object):
             self.log.debug("getImageSrc(row=%r, colID=%r)", row, column.id)
         return ''
     def getCellValue(self, row, column):
-        # Result: wstring
-        # In: param0: int32
-        # In: param1: wstring
         if self.log:
             self.log.debug("getCellValue() row: %s, col: %s", row, column.id)
         return ""
     def getCellText(self, row, column):
-        col = column.id
-        # Result: wstring
-        # In: param0: int32
-        # In: param1: wstring
         if self.log:
-            self.log.debug("getCellText() row: %s, col: %s", row, col)
+            self.log.debug("getCellText() row: %s, col: %s", row, column.id)
         return ""
     def setTree(self, tree):
-        # Result: void - None
-        # In: param0: nsITreeBoxObject
-        #XXX may move to be an attribute.
         if self.log:
             self.log.debug("[%s] setTree(tree='%s')", id(self), tree)
         self._tree = tree
     def toggleOpenState(self, index):
-        # Result: void - None
-        # In: param0: int32
         if self.log:
             self.log.debug("toggleOpenState()")
         pass
     def cycleHeader(self, column):
-        # Result: void - None
-        # In: param0: wstring
-        # In: param1: nsIDOMElement
         if self.log:
             self.log.debug("cycleHeader()")
         pass
     def selectionChanged(self):
-        # Result: void - None
         if self.log:
             self.log.debug("selectionChanged()")
         pass
     def cycleCell(self, row, column):
-        # Result: void - None
-        # In: param0: int32
-        # In: param1: wstring
         if self.log:
             self.log.debug("cycleCell()")
         pass
     def isEditable(self, row, column):
-        # Result: boolean
-        # In: param0: int32
-        # In: param1: wstring
         if self.log:
             self.log.debug("isEditable()")
-        return 0
-    def setCellText( self, param0, param1, param2 ):
-        # Result: void - None
-        # In: param0: int32
-        # In: param1: wstring
-        # In: param2: wstring
+        return False
+    def setCellText(self, param0, param1, param2):
         if self.log:
             self.log.debug("setCellText()")
         pass
@@ -214,23 +155,14 @@ class TreeView(object):
         if self.log:
             self.log.debug("setCellValue() row: %s, col: %s, value: %r", row, col.id, vlaue)
     def performAction(self, action):
-        # Result: void - None
-        # In: param0: wstring
         if self.log:
             self.log.debug("performAction(%s)" % action)
         pass
     def performActionOnRow(self, action, row):
-        # Result: void - None
-        # In: param0: wstring
-        # In: param1: int32
         if self.log:
             self.log.debug("performActionOnRow(%s, %s)", action, row)
         pass
     def performActionOnCell(self, action, row, column):
-        # Result: void - None
-        # In: param0: wstring
-        # In: param1: int32
-        # In: param2: wstring
         if self.log:
             self.log.debug("performActionOnCell(%s, %s, %r)", action, row, column)
         pass
