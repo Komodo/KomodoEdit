06a7c08fed4b1aea07b25f62c53aeffd16e00d8d
Todd Whiteman <toddw@activestate.com>
2010-01-26 01:46:11 +0000

    profile: add XRE upgrade handling for people using the K6 nightlies
    (integrated from komodo (trunk) change 39269 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@4436 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/components/koInitService.p.py
+++ src/components/koInitService.p.py
@@ -902,6 +902,28 @@ class KoInitService(object):
             join("XRE", "secmod.db"): join("XRE", "secmod.db"),
         }
 
+        # Get the current version.
+        infoSvc = components.classes["@activestate.com/koInfoService;1"].getService()
+        currVer = infoSvc.version.split(".", 2)[:2]
+        for i in range(len(currVer)):
+            try:
+                currVer[i] = int(currVer[i])
+            except ValueError:
+                pass
+        currVer = tuple(currVer) # e.g. (6,0)
+
+        # Upgrade handling - just for people using 6.0.0 nightlies - can be
+        # removed once we release the first 6.0.0-alpha1.
+        if currVer == (6, 0):
+            host_xre = join(self._hostUserDataDir(currUserDataDir), "XRE")
+            if exists(host_xre):
+                current_xre = join(currUserDataDir, "XRE")
+                if exists(current_xre):
+                    _rmtree(current_xre)
+                os.rename(host_xre, join(currUserDataDir, "XRE"))
+                log.info("moving the host-$HOSTNAME/XRE dir")
+                return
+
         # First determine if we need to upgrade at all.
         # If any of the above exist in the current version's user data
         # dir then do NOT upgrade.
@@ -915,16 +937,6 @@ class KoInitService(object):
             if exists(path):
                 log.debug("not upgrading userdatadir files: '%s' exists", path)
                 return
-
-        # Get the current version.
-        infoSvc = components.classes["@activestate.com/koInfoService;1"].getService()
-        currVer = infoSvc.version.split(".", 2)[:2]
-        for i in range(len(currVer)):
-            try:
-                currVer[i] = int(currVer[i])
-            except ValueError:
-                pass
-        currVer = tuple(currVer) # e.g. (2,3)
 
         # Determine from which Komodo userdatadir we should upgrade.
         basedir, base = os.path.split(dirname(currUserDataDir))
