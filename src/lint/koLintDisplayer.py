d5fcb00361796eefdcef981de8f8ae97285f185e
Todd Whiteman <toddw@activestate.com>
2010-03-08 22:19:17 +0000

    lint: remove ancient linter styling code. No longer need this code since we are now using indicators for lint markers.
    (integrated from komodo (trunk) change 40810 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@4602 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/lint/koLintDisplayer.py
+++ src/lint/koLintDisplayer.py
@@ -69,32 +69,8 @@ class KoLintDisplayer:
         for i in [self.DECORATOR_ERROR, self.DECORATOR_WARNING]:
             scin.indicatorCurrent = i
             scin.indicatorClearRange(0, scin.length)
-        try:
-            prevEndStyled = scin.endStyled
-            scin.startStyling(0, mask)
-            scin.setStyling(scin.length, 0)
-            if scin.endStyled != scin.length:
-                log.error("displayClear() did not work (%d bytes were "
-                          "styled)! Likely this is because this is a "
-                          "re-entrant call to scintilla. displayClear() "
-                          "must not be called within a scintilla OnModified "
-                          "handler, or the like." % scin.endStyled)
-
-            # ensure scin.endStyled = prevEndStyled so the lexer does not get
-            # confused.
-            scin.startStyling(prevEndStyled, mask)
-            scin.setStyling(0,0)
-            assert prevEndStyled == scin.endStyled, \
-                    "unexpected end styled %d/%d!!" \
-                    % (prevEndStyled, scin.endStyled)
-
-            # This code is called by a timer, not as the text is styled for
-            # painting so if we restyled anything currently on screen, we
-            # need to repaint. no "clean" way to do this!
-            scin.tabWidth = scin.tabWidth # this forces a repaint.
-        finally:
-            log.debug("leaving displayClear")
-            self._in_display = 0
+        log.debug("leaving displayClear")
+        self._in_display = 0
     
     def display(self, scin, lintResults, numBits, startPos, styleLen):
         log.debug("entering display")
@@ -172,10 +148,6 @@ class KoLintDisplayer:
                 scin.indicatorCurrent = value
                 scin.indicatorFillRange(start, length)
 
-        # This code is called by a timer, not as the text is styled for
-        # painting so if we restyled anything currently on screen, we need
-        # to repaint. no "clean" way to do this!
-        scin.tabWidth = scin.tabWidth # this forces a repaint.
         assert prevEndStyled == scin.endStyled, \
                 "unexpected end styled %d/%d!!" \
                 % (prevEndStyled, scin.endStyled)
