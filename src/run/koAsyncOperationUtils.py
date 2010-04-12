469111ea743790802d024c483744140ff1bc224a
Todd Whiteman <toddw@activestate.com>
2010-04-12 17:32:23 +0000

    async utils: add helper classes for wrapping an async callback
    (integrated from komodo (trunk) change 42107 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@4775 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/run/koAsyncOperationUtils.py
+++ src/run/koAsyncOperationUtils.py
@@ -61,3 +61,54 @@ class koAsyncOperationBase(object):
     # Private method: called by the koIAsyncService
     def run(self):
         return self.func(*self.args, **self.kwargs)
+
+class koAsyncCallbackWrapper(object):
+    """Utility class to help creating asynchronous callback handlers"""
+
+    _com_interfaces_  = components.interfaces.koIAsyncCallback
+    RESULT_SUCCESSFUL = components.interfaces.koIAsyncCallback.RESULT_SUCCESSFUL
+    RESULT_STOPPED    = components.interfaces.koIAsyncCallback.RESULT_STOPPED
+    RESULT_ERROR      = components.interfaces.koIAsyncCallback.RESULT_ERROR
+
+    def __init__(self, real_callback):
+        self.real_callback = real_callback
+        self.success_fn = None
+        self.stop_fn = None
+        self.failure_fn = None
+
+    def onSuccess(self, fn, *args, **kwargs):
+        self.success_fn = fn
+        self.success_args = args
+        self.success_kwargs = kwargs
+
+    def onStop(self, fn, *args, **kwargs):
+        self.stop_fn = fn
+        self.stop_args = args
+        self.stop_kwargs = kwargs
+
+    def onFailure(self, fn, *args, **kwargs):
+        self.failure_fn = fn
+        self.failure_args = args
+        self.failure_kwargs = kwargs
+
+    def callback(self, result, data):
+        try:
+            if result == self.RESULT_SUCCESSFUL:
+                if self.success_fn:
+                    self.success_fn(*self.success_args, **self.success_kwargs)
+            elif result == self.RESULT_STOPPED:
+                if self.stop_fn:
+                    self.stop_fn(*self.stop_args, **self.stop_kwargs)
+            else:
+                if self.failure_fn:
+                    self.failure_fn(*self.failure_args, **self.failure_kwargs)
+        finally:
+            self.real_callback.callback(result, data)
+
+class koAsyncCallbackWithProgressWrapper(koAsyncCallbackWrapper):
+    """Utility class to help creating asynchronous callback handlers"""
+
+    _com_interfaces_ = components.interfaces.koIAsyncCallbackWithProgress
+
+    def onProgress(self, label, value):
+        self.real_callback.onProgress(label, value)
