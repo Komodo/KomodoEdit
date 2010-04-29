2e53eeb344e0a4233e646e1524d8d3fa56258388
Todd Whiteman <toddw@activestate.com>
2010-04-29 18:36:37 +0000

    diff: add a diff xpcom service to aid in diffing files and directories
    (integrated from komodo (trunk) change 42794 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@4902 b87d16e7-d29f-4ee1-b5ec-817d645d335f

---
+++ src/components/koDiffService.py
@@ -0,0 +1,36 @@
+#!python
+# Copyright (c) 2010 ActiveState Software Inc.
+# See the file LICENSE.txt for licensing information.
+
+# A service that provides file diffing facilities. Mostly a wrapper around
+# certain parts of the python difflibex module.
+
+import os
+import sys
+import logging
+
+from xpcom import components
+
+import difflibex
+
+#log = logging.getLogger("koDiffService")
+
+
+#---- component implementation
+
+class KoDirs(object):
+    _com_interfaces_ = [components.interfaces.koIDiffService]
+    _reg_clsid_ = "{d968bb67-1d39-4e8e-861c-6d4b2b1f0153}"
+    _reg_contractid_ = "@activestate.com/koDiffService;1"
+    _reg_desc_ = "Komodo Diff Service"
+
+    def diffFilepaths(self, left_filepath, right_filepath):
+        return difflibex.diff_multiple_local_filepaths([left_filepath],
+                                                       [right_filepath])
+
+    def diffDirectories(self, left_dirpath, right_dirpath):
+        return difflibex.diff_local_directories(left_dirpath, right_dirpath)
+
+    def diffMultipleFilepaths(self, left_filepaths, right_filepaths):
+        return difflibex.diff_multiple_local_filepaths(left_filepaths,
+                                                       right_filepaths)
