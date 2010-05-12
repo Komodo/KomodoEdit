b40fffe5e1815db0007168335e0dc45b0f1683a0
Todd Whiteman <toddw@activestate.com>
2010-05-12 20:02:00 +0000

    codeintel: support: make the which module py2 and p3 compatible
    (integrated from komodo (trunk) change 43533 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@4991 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/codeintel/support/which.py
+++ src/codeintel/support/which.py
@@ -91,7 +91,11 @@ def _getRegisteredExecutable(exeName):
     if sys.platform.startswith('win'):
         if os.path.splitext(exeName)[1].lower() != '.exe':
             exeName += '.exe'
-        import _winreg
+        try:
+            import _winreg
+        except ImportError:
+            # In Python 3 this module was renamed to winreg.
+            import winreg as _winreg
         try:
             key = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\" +\
                   exeName
@@ -287,17 +291,18 @@ def main(argv):
     try:
         optlist, args = getopt.getopt(argv[1:], 'haVvqp:e:',
             ['help', 'all', 'version', 'verbose', 'quiet', 'path=', 'exts='])
-    except getopt.GetoptError, msg:
+    except getopt.GetoptError:
+        msg = sys.exc_info()[1]
         sys.stderr.write("which: error: %s. Your invocation was: %s\n"\
                          % (msg, argv))
         sys.stderr.write("Try 'which --help'.\n")
         return 1
     for opt, optarg in optlist:
         if opt in ('-h', '--help'):
-            print _cmdlnUsage
+            print(_cmdlnUsage)
             return 0
         elif opt in ('-V', '--version'):
-            print "which %s" % __version__
+            print("which %s" % __version__)
             return 0
         elif opt in ('-a', '--all'):
             all = 1
@@ -321,13 +326,13 @@ def main(argv):
 
     failures = 0
     for arg in args:
-        #print "debug: search for %r" % arg
+        #print("debug: search for %r" % arg)
         nmatches = 0
         for match in whichgen(arg, path=altpath, verbose=verbose, exts=exts):
             if verbose:
-                print "%s (%s)" % match
+                print("%s (%s)" % match)
             else:
-                print match
+                print(match)
             nmatches += 1
             if not all:
                 break
