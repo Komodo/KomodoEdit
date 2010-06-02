9fcd7f3586e3b1f2e06635aa0e067353bfb1f291
Mike Ivanov <mikei@activestate.com>
2010-06-02 23:32:37 +0000

    Added tests for Python 3 ciling
    (integrated from komodo (trunk) change 44260 by mikei)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@5242 b87d16e7-d29f-4ee1-b5ec-817d645d335f

---
+++ src/codeintel/test2/scan_inputs/py3/keyword_only_args.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+def sortwords(*wordlist, case_sensitive=False):
+    pass
+
+def compare(a, b, *, key=None):
+    pass
+
---
+++ src/codeintel/test2/scan_inputs/py3/type_annotations.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+def func() -> int:
+    pass
+
+def funk(self, it:Iterator, recursive: bool):
+    pass
+
+def punk(boo=lambda foo: 'bar'):
+    pass
