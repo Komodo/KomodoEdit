58c42460b252742205a68f41b0963ef894af765c
Todd Whiteman <toddw@activestate.com>
2010-11-04 00:01:44 +0000

    build: pyxpcom: record the last xpcom call that is made into the extra information of the crash reporter data, hopefully this can help in tracking down pyxpcom crashes * for now this will only be available in Komodo nightlies
    (integrated from komodo (trunk) change 58517 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@6627 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- mozilla/patches-new/MOZILLA_1_9_2/__patchinfo__.py
+++ mozilla/patches-new/MOZILLA_1_9_2/__patchinfo__.py
@@ -5,7 +5,8 @@ def applicable(config):
     return hasattr(config, "mozVer") and config.mozVer == 1.92
 
 def patchfile_applicable(config, filepath):
-    if filepath.endswith("xpcom_abort_threaded_cycle_collector.patch"):
+    if (filepath.endswith("xpcom_abort_threaded_cycle_collector.patch") or
+        filepath.endswith("pyxpcom_crashreporter_log_last_call.patch")):
         # Apply this patch when not using a "blessed" build. Typcially blessed
         # builds are only used for official releases.
         return not hasattr(config, "blessed") or not config.blessed
