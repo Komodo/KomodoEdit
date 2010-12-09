1889de27d18e43dc025601c4f938a762e196ab79
Eric Promislow <ericp@activestate.com>
2010-12-09 19:19:50 +0000

    * Display opened projects in the Project subpane, not MRUs * Handle openProject
    (integrated from komodo (ericp-reproject02 branch) change 59204 by ericp)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@6821 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/modules/places/content/places.js
+++ src/modules/places/content/places.js
@@ -2705,7 +2705,7 @@ this.onLoad_aux = function places_onLoad_aux() {
     window.setTimeout(function() {
             ko.places.manager.initialize();
         }, 1);
-    ko.places._updateSubpanelFromState();
+    ko.places.projects._updateSubpanelFromState();
     
     // Wait until ko.projects.manager exists before
     // init'ing the projects view tree.
@@ -2713,183 +2713,19 @@ this.onLoad_aux = function places_onLoad_aux() {
     var launch_createProjectMRUView = function() {
         if (ko.projects && ko.projects.manager) {
             clearInterval(mruProjectViewerID);
-            ko.places.createProjectMRUView();
+            ko.places.projects.createProjectMRUView();
         }
     };
     mruProjectViewerID = setInterval(launch_createProjectMRUView, 50);
-    ko.places.initProjectMRUCogMenu();
 }
-
-this.initProjectMRUCogMenu = function() {
-    var srcMenu = document.getElementById("popup_project");
-    var destMenu = document.getElementById("placesSubpanelProjectsToolsPopup");
-    var srcNodes = srcMenu.childNodes;
-    var node, newNode;
-    var len = srcNodes.length;
-    for (var i = 0; i < len; i++) {
-        node = srcNodes[i];
-        if (node.getAttribute("skipCopyToCogMenu") != "true") {
-            newNode = node.cloneNode(false);
-            newNode.id = node.id + "_places_projects_cog";
-            destMenu.appendChild(newNode);
-        }
-    }
-};
 
 this.onUnload = function places_onUnload() {
     ko.places.manager.finalize();
     ko.places.viewMgr.finalize();
-    ko.places.projectsTreeView.terminate();
+    ko.places.projects.terminate();
     ko.places.manager = ko.places._viewMgr = null;
 };
 
-this.createProjectMRUView = function() {
-    this.projectsTree = document.getElementById("placesSubpanelProjects");
-    this.projectsTreeView = new this.recentProjectsTreeView();
-    if (!this.projectsTreeView) {
-        throw new Error("couldn't create a recentProjectsTreeView");
-    }
-    this.projectsTree.treeBoxObject
-                        .QueryInterface(Components.interfaces.nsITreeBoxObject)
-                        .view = this.projectsTreeView;
-    this.projectsTreeView.initialize();    
-};
-
-// Methods for dealing with the projects tree context menu.
-
-this._projectTestLabelMatcher = /^t:project\|(.+)\|(.+)$/;
-this.initProjectsContextMenu = function(event, menupopup) {
-    var row = {};
-    this.projectsTree.treeBoxObject.getCellAt(event.pageX, event.pageY, row, {},{});
-    var index = row.value;
-    if (index == -1) {
-        // Means that we're clicking in white-space below.
-        // Clear the selection, and return.
-        this.projectsTreeView.selection.clearSelection();
-        event.stopPropagation();
-        event.preventDefault();
-        return false;
-    }
-    var selectedUrl = this.projectsTreeView.getCellValue(index);
-    var selectionInfo = {};
-    var currentProject = ko.projects.manager.currentProject;
-    selectionInfo.currentProject = (currentProject
-                                    && currentProject.url == selectedUrl);
-    selectionInfo.projectIsDirty = selectionInfo.currentProject && currentProject.isDirty;
-    var projectIsOpen = false;
-    var windows = ko.windowManager.getWindows();
-    for (var win, i = 0; win = windows[i]; i++) {
-        var otherProject = win.ko.projects.manager.currentProject;
-        if (otherProject && otherProject.url == selectedUrl) {
-            projectIsOpen = true;
-            break;
-        }
-    }
-    selectionInfo.projectIsOpen = projectIsOpen;
-    
-    var childNodes = menupopup.childNodes;
-    for (var menuNode, i = 0; menuNode = childNodes[i]; i++) {
-        var disableNode = false;
-        var directive = menuNode.getAttribute('disableIf');
-        if (directive) {
-            if ((directive in selectionInfo) && selectionInfo[directive]) {
-                disableNode = true;
-            } 
-        }
-        if (!disableNode
-            && !!(directive = menuNode.getAttribute('disableUnless'))
-            && (!(directive in selectionInfo)
-                || !selectionInfo[directive])) {
-            disableNode = true;
-        }
-        if (disableNode) {
-            menuNode.setAttribute('disabled', true);
-        } else {
-            menuNode.removeAttribute('disabled');
-        }
-    }
-    return true;
-};
-
-this.onProjectTreeDblClick = function(event) {
-    if (event.which != 1) {
-        return;
-    }
-    var row = {};
-    this.projectsTree.treeBoxObject.getCellAt(event.pageX, event.pageY, row, {},{});
-    var index = row.value;
-    if (index != -1) {
-        var uri = this.projectsTreeView.getCellValue(index);
-        var currentProject = ko.projects.manager.currentProject;
-        if (!currentProject || currentProject.url != uri) {
-            ko.projects.open(uri);
-        } else {
-            this.showProjectInPlaces();
-        }
-    }
-    event.stopPropagation();
-    event.preventDefault();
-};
-
-this.showProjectInPlaces = function() {
-    ko.places.manager.moveToProjectDir(ko.projects.manager.currentProject);
-};
-
-this.closeProject = function() {
-    ko.projects.manager.closeProject(ko.projects.manager.currentProject);
-};
-
-this.saveProject = function() {
-    ko.projects.manager.saveProject(ko.projects.manager.currentProject);
-};
-
-this.saveProjectAs = function() {
-    ko.projects.saveProjectAs(ko.projects.manager.currentProject);
-};
-
-this.revertProject = function() {
-    ko.projects.manager.revertProject(ko.projects.manager.currentProject);
-};
-
-this.openProjectInNewWindow = function() {
-    this._openProject(true);
-};
-
-this.openProjectInCurrentWindow = function() {
-    this._openProject(false);
-}
-
-this.removeProjectListing = function() {
-    var index = this.projectsTreeView.selection.currentIndex;
-    if (index == -1) {
-        log.debug("removeProjectListing: No index");
-        return;
-    }
-    var uri = this.projectsTreeView.getCellValue(index);
-    if (!uri) {
-        log.debug("removeProjectListing: No uri at index:" + index);
-        return;
-    }
-    ko.mru.deleteValue('mruProjectList', uri, true/*notify */);
-}
-
-this._openProject = function(inNewWindow) {
-    var index = this.projectsTreeView.selection.currentIndex;
-    if (index == -1) {
-        return;
-    }
-    var uri = this.projectsTreeView.getCellValue(index);
-    if (inNewWindow) {
-        ko.launch.newWindow(uri);
-    } else {
-        ko.projects.open(uri);
-    }
-};
-
-this.editProjectProperties = function() {
-    var item = ko.places.getItemWrapper(ko.projects.manager.currentProject.url, 'project');
-    ko.projects.fileProperties(item, null, true);
-};
 
 this.getFocusedPlacesView = function() {
     if (xtk.domutils.elementInFocus(document.getElementById('placesViewbox'))) {
@@ -2897,37 +2733,6 @@ this.getFocusedPlacesView = function() {
     }
     return null;
 };
-
-
-this.toggleSubpanel = function() {
-    var button = document.getElementById("placesSubpanelToggle");
-    var state = button.getAttribute("state");
-    switch(state) {
-    case "collapsed":
-        button.setAttribute("state", "open");
-        break;
-    case "open":
-    default:
-        button.setAttribute("state", "collapsed");
-        break;
-    }
-    this._updateSubpanelFromState();
-};
-
-this._updateSubpanelFromState = function() {
-    var button = document.getElementById("placesSubpanelToggle");
-    var deck = document.getElementById("placesSubpanelDeck");
-    var state = button.getAttribute("state");
-    switch(state) {
-    case "collapsed":
-        deck.collapsed = true;
-        break;
-    case "open":
-    default:
-        deck.collapsed = false;
-        break;
-    }
-}
 
 this.updateFilterViewMenu = function() {
     // Find which node is checked.  Then update the menu, maintaining
@@ -2973,109 +2778,6 @@ this.updateFilterViewMenu = function() {
         }
     }
 };
-
-xtk.include("treeview");
-this.recentProjectsTreeView = function() {
-    xtk.dataTreeView.apply(this, []);
-    this._atomService = Components.classes["@mozilla.org/atom-service;1"].
-                            getService(Components.interfaces.nsIAtomService);
-}
-this.recentProjectsTreeView.prototype = new xtk.dataTreeView();
-this.recentProjectsTreeView.prototype.constructor = this.recentProjectsTreeView;
-
-this.recentProjectsTreeView.prototype.initialize = function() {
-    this._resetRows();
-    var observerSvc = Components.classes["@mozilla.org/observer-service;1"].
-            getService(Components.interfaces.nsIObserverService);
-    observerSvc.addObserver(this, "mru_changed", false);
-    this.pref_observer_names = [ "showProjectPath" ];
-    _placePrefs.prefObserverService.addObserverForTopics(this,
-                                         this.pref_observer_names.length,
-                                         this.pref_observer_names, true);
-};
-
-this.recentProjectsTreeView.prototype.terminate = function() {
-    _placePrefs.prefObserverService.removeObserverForTopics(this,
-                                            this.pref_observer_names.length,
-                                            this.pref_observer_names);
-    var observerSvc = Components.classes["@mozilla.org/observer-service;1"].
-                    getService(Components.interfaces.nsIObserverService);
-    try {
-        observerSvc.removeObserver(this, "mru_changed");
-    } catch(ex) {
-        // Sometimes during shutdown this throws a 0x80004005 exception
-    }
-};
-
-this.recentProjectsTreeView.prototype._resetRows = function() {
-    var showProjectPath = _globalPrefs.getPref("places").getBooleanPref('showProjectPath');
-    var rows = ko.mru.getAll("mruProjectList").map(function(uri) {
-            if (!showProjectPath) {
-                var m = PROJECT_URI_REGEX.exec(uri);
-                if (m) {
-                    return [uri, decodeURIComponent(m[1])];
-                }
-            }
-            var path = ko.uriparse.URIToPath(uri);
-            var name, lastSlash, lastDot;
-            if (!showProjectPath) {
-                var lastSlash = uri.lastIndexOf('/');
-                if (lastSlash > -1) {
-                    var lastDot = uri.lastIndexOf(".");
-                    if (lastDot > -1
-                        && ['.komodoproject', '.kpf'].indexOf(uri.substr(lastDot)) > -1) {
-                        // Standard -- ends with ".komodoproject" or ".kpf"
-                        path = path.substring(lastSlash + 1, lastDot);
-                    } else {
-                        path = path.substring(lastSlash + 1);
-                    }
-                }
-                // else Do nothing
-            } else {
-                var lastDot = path.lastIndexOf(".");
-                if (lastDot > -1
-                    && ['.komodoproject', '.kpf'].indexOf(path.substr(lastDot)) > -1) {
-                    path = path.substr(0, lastDot);
-                }
-            }
-            return [uri, path];
-        });
-    rows.reverse();
-    this.rows = rows;
-};
-this.recentProjectsTreeView.prototype.observe = function(subject, topic, data) {
-    if (data == "mruProjectList") {
-        this._resetRows();
-    } else if (topic == "showProjectPath") {
-        this._resetRows();
-    }
-};
-// NSITreeView methods.
-this.recentProjectsTreeView.prototype.getCellText = function(index, column) {
-    var row = this.rows[index];
-    var currentProject = ko.projects.manager.currentProject;
-    if (currentProject
-        && currentProject.isDirty
-        && currentProject.url == row[0]) {
-        return row[1] + "*";
-    } else {
-        return row[1];
-    }
-};
-this.recentProjectsTreeView.prototype.getCellValue = function(index, column) {
-    return this.rows[index][0];
-};
-this.recentProjectsTreeView.prototype.getImageSrc = function(index, column) {
-    return 'chrome://komodo/skin/images/project_icon.png'
-};
-this.recentProjectsTreeView.prototype.getCellProperties = function(index, column, properties) {
-    var row = this.rows[index];
-    var currentProject = ko.projects.manager.currentProject;
-    if (currentProject && currentProject.url == row[0]) {
-        properties.AppendElement(this._atomService.getAtom("projectActive"));
-    }
-};
-
 
 //---- internal support stuff
 
---
+++ src/modules/places/content/project_places.js
@@ -0,0 +1,311 @@
+// Copyright (c) 2000-2011 ActiveState Software Inc.
+// See the file LICENSE.txt for licensing information.
+
+// Define the ko.places.projects namespace
+
+if (typeof(ko) == 'undefined') {
+    var ko = {};
+}
+if (!('places' in ko)) {
+    ko.places = {};
+}
+if (!('projects' in ko.places)) {
+    ko.places.projects = {};
+}
+
+(function() {
+
+var _globalPrefs;
+var _placePrefs;
+var _bundle = Components.classes["@mozilla.org/intl/stringbundle;1"]
+    .getService(Components.interfaces.nsIStringBundleService)
+    .createBundle("chrome://places/locale/places.properties");
+
+this.createProjectMRUView = function() {
+    _globalPrefs = (Components.classes["@activestate.com/koPrefService;1"].
+                   getService(Components.interfaces.koIPrefService).prefs);
+    _placePrefs = _globalPrefs.getPref("places");
+    this.projectsTree = document.getElementById("placesSubpanelProjects");
+    this.projectsTreeView = new this.PlaceProjectsTreeView();
+    if (!this.projectsTreeView) {
+        throw new Error("couldn't create a PlaceProjectsTreeView");
+    }
+    this.projectsTree.treeBoxObject
+                        .QueryInterface(Components.interfaces.nsITreeBoxObject)
+                        .view = this.projectsTreeView;
+    this.projectsTreeView.initialize();
+    this.initProjectMRUCogMenu();
+    this.manager = new PlacesProjectManager(this);
+    ko.projects.manager.setViewMgr(this.manager);
+};
+
+function PlacesProjectManager(owner) {
+    this.owner = owner;
+}
+
+PlacesProjectManager.prototype = {
+  addProject: function(project) {
+        this.owner.projectsTreeView.addProject(project);
+    },
+  refresh: function(project) {
+        this.owner.projectsTreeView.tree.invalidate();
+    },
+  removeProject: function(project) {
+        dump("PlacesProjectManager.removeProject\n");
+    },
+  
+  _EOF_ : null
+};
+
+this.initProjectMRUCogMenu = function() {
+    var srcMenu = document.getElementById("popup_project");
+    var destMenu = document.getElementById("placesSubpanelProjectsToolsPopup");
+    var srcNodes = srcMenu.childNodes;
+    var node, newNode;
+    var len = srcNodes.length;
+    for (var i = 0; i < len; i++) {
+        node = srcNodes[i];
+        if (node.getAttribute("skipCopyToCogMenu") != "true") {
+            newNode = node.cloneNode(false);
+            newNode.id = node.id + "_places_projects_cog";
+            destMenu.appendChild(newNode);
+        }
+    }
+};
+
+this.terminate = function() {
+    this.projectsTreeView.terminate();
+};
+
+// Methods for dealing with the projects tree context menu.
+
+this._projectTestLabelMatcher = /^t:project\|(.+)\|(.+)$/;
+this.initProjectsContextMenu = function(event, menupopup) {
+    var row = {};
+    this.projectsTree.treeBoxObject.getCellAt(event.pageX, event.pageY, row, {},{});
+    var index = row.value;
+    if (index == -1) {
+        // Means that we're clicking in white-space below.
+        // Clear the selection, and return.
+        this.projectsTreeView.selection.clearSelection();
+        event.stopPropagation();
+        event.preventDefault();
+        return false;
+    }
+    var selectedUrl = this.projectsTreeView.getCellValue(index);
+    var selectionInfo = {};
+    var currentProject = ko.projects.manager.currentProject;
+    selectionInfo.currentProject = (currentProject
+                                    && currentProject.url == selectedUrl);
+    selectionInfo.projectIsDirty = selectionInfo.currentProject && currentProject.isDirty;
+    var projectIsOpen = false;
+    var windows = ko.windowManager.getWindows();
+    for (var win, i = 0; win = windows[i]; i++) {
+        var otherProject = win.ko.projects.manager.currentProject;
+        if (otherProject && otherProject.url == selectedUrl) {
+            projectIsOpen = true;
+            break;
+        }
+    }
+    selectionInfo.projectIsOpen = projectIsOpen;
+    
+    var childNodes = menupopup.childNodes;
+    for (var menuNode, i = 0; menuNode = childNodes[i]; i++) {
+        var disableNode = false;
+        var directive = menuNode.getAttribute('disableIf');
+        if (directive) {
+            if ((directive in selectionInfo) && selectionInfo[directive]) {
+                disableNode = true;
+            } 
+        }
+        if (!disableNode
+            && !!(directive = menuNode.getAttribute('disableUnless'))
+            && (!(directive in selectionInfo)
+                || !selectionInfo[directive])) {
+            disableNode = true;
+        }
+        if (disableNode) {
+            menuNode.setAttribute('disabled', true);
+        } else {
+            menuNode.removeAttribute('disabled');
+        }
+    }
+    return true;
+};
+
+this.onProjectTreeDblClick = function(event) {
+    if (event.which != 1) {
+        return;
+    }
+    var row = {};
+    this.projectsTree.treeBoxObject.getCellAt(event.pageX, event.pageY, row, {},{});
+    var index = row.value;
+    if (index != -1) {
+        var uri = this.projectsTreeView.getCellValue(index);
+        var currentProject = ko.projects.manager.currentProject;
+        if (!currentProject || currentProject.url != uri) {
+            ko.projects.open(uri);
+        } else {
+            this.showProjectInPlaces();
+        }
+    }
+    event.stopPropagation();
+    event.preventDefault();
+};
+
+this.showProjectInPlaces = function() {
+    ko.places.manager.moveToProjectDir(ko.projects.manager.currentProject);
+};
+
+this.closeProject = function() {
+    ko.projects.manager.closeProject(ko.projects.manager.currentProject);
+};
+
+this.saveProject = function() {
+    ko.projects.manager.saveProject(ko.projects.manager.currentProject);
+};
+
+this.saveProjectAs = function() {
+    ko.projects.saveProjectAs(ko.projects.manager.currentProject);
+};
+
+this.revertProject = function() {
+    ko.projects.manager.revertProject(ko.projects.manager.currentProject);
+};
+
+this.openProjectInNewWindow = function() {
+    this._openProject(true);
+};
+
+this.openProjectInCurrentWindow = function() {
+    this._openProject(false);
+}
+
+this._openProject = function(inNewWindow) {
+    var defaultDirectory = null;
+    var defaultFilename = null;
+    var title = _bundle.GetStringFromName("openProject.title");
+    var defaultFilterName = _bundle.GetStringFromName("komodoProject.message");
+    var filterNames = [_bundle.GetStringFromName("komodoProject.message"),
+                       _bundle.GetStringFromName("all.message")];
+    filename = ko.filepicker.openFile(defaultDirectory /* =null */,
+                                      defaultFilename /* =null */,
+                                      title /* ="Open File" */,
+                                      defaultFilterName /* ="All" */,
+                                      filterNames /* =null */)
+    if (filename == null) return;
+    uri = ko.uriparse.localPathToURI(filename);
+    if (inNewWindow) {
+        ko.launch.newWindow(uri);
+    } else {
+        ko.projects.open(uri);
+    }
+};
+
+this.editProjectProperties = function() {
+    var item = ko.places.getItemWrapper(ko.projects.manager.currentProject.url, 'project');
+    ko.projects.fileProperties(item, null, true);
+};
+
+this.toggleSubpanel = function() {
+    var button = document.getElementById("placesSubpanelToggle");
+    var state = button.getAttribute("state");
+    switch(state) {
+    case "collapsed":
+        button.setAttribute("state", "open");
+        break;
+    case "open":
+    default:
+        button.setAttribute("state", "collapsed");
+        break;
+    }
+    this._updateSubpanelFromState();
+};
+
+this._updateSubpanelFromState = function() {
+    var button = document.getElementById("placesSubpanelToggle");
+    var deck = document.getElementById("placesSubpanelDeck");
+    var state = button.getAttribute("state");
+    switch(state) {
+    case "collapsed":
+        deck.collapsed = true;
+        break;
+    case "open":
+    default:
+        deck.collapsed = false;
+        break;
+    }
+}
+
+xtk.include("treeview");
+this.PlaceProjectsTreeView = function() {
+    xtk.dataTreeView.apply(this, []);
+    this._atomService = Components.classes["@mozilla.org/atom-service;1"].
+                            getService(Components.interfaces.nsIAtomService);
+}
+this.PlaceProjectsTreeView.prototype = new xtk.dataTreeView();
+this.PlaceProjectsTreeView.prototype.constructor = this.PlaceProjectsTreeView;
+
+this.PlaceProjectsTreeView.prototype.initialize = function() {
+    this._resetRows();
+    var observerSvc = Components.classes["@mozilla.org/observer-service;1"].
+            getService(Components.interfaces.nsIObserverService);
+    this.pref_observer_names = [ "showProjectPath" ];
+    _placePrefs.prefObserverService.addObserverForTopics(this,
+                                         this.pref_observer_names.length,
+                                         this.pref_observer_names, true);
+};
+
+this.PlaceProjectsTreeView.prototype.terminate = function() {
+    _placePrefs.prefObserverService.removeObserverForTopics(this,
+                                            this.pref_observer_names.length,
+                                            this.pref_observer_names);
+};
+
+this.PlaceProjectsTreeView.prototype._resetRows = function() {
+    this.rows = [];
+};
+
+this.PlaceProjectsTreeView.prototype.observe = function(subject, topic, data) {
+    if (data == "mruProjectList") {
+        this._resetRows();
+    } else if (topic == "showProjectPath") {
+        this._resetRows();
+    }
+};
+
+this.PlaceProjectsTreeView.prototype.addProject = function(project) {
+    var url = project.url
+    var row = [url, ko.uriparse.baseName(url)];
+    this.rows.push(row);
+    this.tree.rowCountChanged(this.rows.length - 2, 1);
+};
+
+// NSITreeView methods.
+this.PlaceProjectsTreeView.prototype.getCellText = function(index, column) {
+    var row = this.rows[index];
+    var currentProject = ko.projects.manager.currentProject;
+    if (currentProject
+        && currentProject.isDirty
+        && currentProject.url == row[0]) {
+        return row[1] + "*";
+    } else {
+        return row[1];
+    }
+};
+this.PlaceProjectsTreeView.prototype.getCellValue = function(index, column) {
+    return this.rows[index][0];
+};
+this.PlaceProjectsTreeView.prototype.getImageSrc = function(index, column) {
+    return 'chrome://komodo/skin/images/project_icon.png'
+};
+this.PlaceProjectsTreeView.prototype.getCellProperties = function(index, column, properties) {
+    return; //@@@@ -- figure out which project is active.
+    var row = this.rows[index];
+    var currentProject = ko.projects.manager.currentProject;
+    if (currentProject && currentProject.url == row[0]) {
+        properties.AppendElement(this._atomService.getAtom("projectActive"));
+    }
+};
+
+}).apply(ko.places.projects);
