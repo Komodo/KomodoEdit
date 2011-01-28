31b107a946e9e4b3fbd143b525f490ef6652efe7
Eric Promislow <ericp@activestate.com>
2011-01-28 23:08:02 +0000

    Prefs: Fix http://bugs.activestate.com/show_bug.cgi?id=89268 '"Edit->Current File Settings" dialog shows project information' r=toddw
    (integrated from komodo (trunk) change 60641 by ericp)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@7179 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/chrome/komodo/content/pref/file-properties.p.js
+++ src/chrome/komodo/content/pref/file-properties.p.js
@@ -48,7 +48,7 @@ xtk.include("domutils");
 function OnPreferencePageLoading(prefset) {
     try {
         data.importFrame = document.getElementById("importFrame");
-        g_isProject = parent.part.type == "project";
+        g_isProject = (parent.part && parent.part.type == "project");
         data.importFrame.setAttribute("collapsed", !g_isProject);
         data.readonly = document.getElementById("readonly");
         data.permissions_label = document.getElementById("file_permissions_label");
