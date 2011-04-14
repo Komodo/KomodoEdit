d6ff2e308d284cb53ef56c92732e736d7c8476a0
Todd Whiteman <toddw@activestate.com>
2011-04-14 18:25:36 +0000

    ci: python: remove already implemented todo note (no functional change)
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@7789 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/codeintel/lib/codeintel2/pythoncile1.py
+++ src/codeintel/lib/codeintel2/pythoncile1.py
@@ -45,7 +45,6 @@
 #
 #TODO:
 # - type inferencing: asserts
-# - type inferencing: return statements
 # - type inferencing: calls to isinstance
 # - special handling for None may be required
 # - Comments and doc strings. What format?
