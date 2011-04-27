ec92c6a0c613ea1a69ed23a5792437c813810d15
Todd Whiteman <toddw@activestate.com>
2011-04-27 17:20:25 +0000

    build: don't use "/" at end of directory path in Python, as Py 2.5 doesn't like this
    (integrated from komodo (trunk) change 63072 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@7888 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/udl/ludditelib/__init__.py
+++ src/udl/ludditelib/__init__.py
@@ -41,7 +41,7 @@ if os.path.exists(os.path.join(os.path.dirname(__file__), "../chromereg.py")):
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 elif os.path.exists(os.path.join(os.path.dirname(__file__), "../../sdk/pylib/chromereg.py")):
     # in a source directory
-    sys.path.append(os.path.join(os.path.dirname(__file__), "../../sdk/pylib/"))
+    sys.path.append(os.path.join(os.path.dirname(__file__), "../../sdk/pylib"))
 elif os.path.exists(os.path.join(os.path.dirname(__file__), "../../../../src/sdk/pylib/chromereg.py")):
     # in a build tree, on osx (sys.executable is broken, reach into the source tree)
-    sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../src/sdk/pylib/"))
+    sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../src/sdk/pylib"))
