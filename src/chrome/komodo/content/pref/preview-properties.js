66e1fee85393c70c3a7ff12d5b55cd5d484d4b12
Todd Whiteman <toddw@activestate.com>
2011-08-31 16:57:00 +0000

    file properties: allow changing the configured preview browser pref
    (integrated from komodo (trunk) change 65968 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@8654 b87d16e7-d29f-4ee1-b5ec-817d645d335f

---
+++ src/chrome/komodo/content/pref/preview-properties.js
@@ -0,0 +1,61 @@
+var log = ko.logging.getLogger("prefs::preview-properties");
+var gDefaultBrowserName = null;
+
+function LoadAvailableBrowsers(browserType)
+{
+    var menulist = document.getElementById("browser-select-menulist");
+    var menupopup = document.getElementById("browser-select-menupopup");
+    gDefaultBrowserName = menulist.label;
+    // Load the menuitems, though we must remove the oncommand attribute.
+    ko.uilayout.populatePreviewToolbarButton(menupopup);
+    var menuitem = menupopup.firstChild;
+    var selectedItem = null;
+    while (menuitem) {
+        if (browserType && menuitem.getAttribute("value") == browserType) {
+            selectedItem = menuitem;
+        }
+        menuitem.removeAttribute("oncommand");
+        menuitem = menuitem.nextSibling;
+    }
+    if (selectedItem) {
+        menulist.selectedItem = selectedItem;
+    }
+}
+
+function ClearPreviewSettings() {
+    try {
+        document.getElementById('preview').value = null;
+        var menulist = document.getElementById('browser-select-menulist');
+        menulist.setAttribute('label', gDefaultBrowserName);
+        menulist.setAttribute('value', null);
+    } catch (e) {
+        log.exception(e);
+    }
+}
+
+function OnPreferencePageLoading(prefset) {
+    try {
+        var browserType;
+        if (prefset.hasStringPref("preview_browser")) {
+            browserType = prefset.getStringPref("preview_browser");
+        }
+        LoadAvailableBrowsers(browserType);
+    } catch (e) {
+        log.exception(e);
+    }
+}
+
+function OnPreferencePageOK(prefset) {
+    try {
+        var menulist = document.getElementById('browser-select-menulist');
+        if (!menulist.value) {
+            prefset.setStringPref("preview_browser", "");
+        } else {
+            prefset.setStringPref("preview_browser", menulist.value);
+        }
+        return true;
+    } catch (e) {
+        log.exception(e);
+    }
+}
+
