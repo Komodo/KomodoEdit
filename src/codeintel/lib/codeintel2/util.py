4c5dd45cdd1a448c75a0687aaf1ab0d1864398cf
Todd Whiteman <toddw@activestate.com>
2011-09-16 00:15:08 +0000

    codeintel: drop unused util functions
    (integrated from komodo (trunk) change 66267 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@8757 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/codeintel/lib/codeintel2/util.py
+++ src/codeintel/lib/codeintel2/util.py
@@ -687,27 +687,6 @@ def make_short_name_dict(names, length=3):
         values.sort(CompareNPunctLast)
     return outdict
 
-#----  cachedmethod from http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/325205
-
-def cachedmethod(function):
-    return types.MethodType(Memoize(function), None)
-
-class Memoize:
-    def __init__(self,function):
-        self._cache = {}
-        self._callable = function
-            
-    def __call__(self, *args, **kwds):
-        cache = self._cache
-        key = self._getKey(*args,**kwds)
-        try: return cache[key]
-        except KeyError:
-            cachedValue = cache[key] = self._callable(*args,**kwds)
-            return cachedValue
-    
-    def _getKey(self,*args,**kwds):
-        return kwds and (args, set(kwds)) or args    
-
 def makePerformantLogger(logger):
     """Replaces the info() and debug() methods with dummy methods.
 
