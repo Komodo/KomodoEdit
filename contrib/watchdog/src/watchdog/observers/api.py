457264b001158da9d608d0bd4a946ea2e1ae07ad
Todd Whiteman <toddw@activestate.com>
2011-10-18 17:24:29 +0000

    contrib: watchdog: handle the case where a watch could be deleted before the events were emitted
    (integrated from komodo (trunk) change 66922 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@8930 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- contrib/watchdog/src/watchdog/observers/api.py
+++ contrib/watchdog/src/watchdog/observers/api.py
@@ -247,7 +247,12 @@ class EventDispatcher(DaemonThread):
             :class:`queue.Empty`
         """
         event, watch = event_queue.get(block=True, timeout=timeout)
-        self.dispatch_event(event, watch)
+        try:
+            self.dispatch_event(event, watch)
+        except KeyError:
+            # Watch was removed before we could emit the events, issue:
+            # https://github.com/gorakhargosh/watchdog/issues/77
+            pass
         event_queue.task_done()
 
     def on_thread_exit(self):
