e3faf42c2003a80a677d9e6310dfcf609f0c9436
Todd Whiteman <toddw@activestate.com>
2011-10-27 23:38:44 +0000

    build: komodo: update to build a single "komodo" executable, instead of using a separate "komodo" shell script to launch the "komodo-bin" executable.
    (integrated from komodo (trunk) change 67194 by toddw)
    
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@9038 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/main/xre/app/nsKomodoApp.p.cpp
+++ src/main/xre/app/nsKomodoApp.p.cpp
@@ -35,55 +35,123 @@
  * 
  * ***** END LICENSE BLOCK ***** */
 
+#include "nsXPCOMGlue.h"
 #include "nsXULAppAPI.h"
-#ifdef XP_WIN
+#if defined(XP_WIN)
 #include <windows.h>
 #include <stdlib.h>
-#ifdef KOMODO_USE_WMAIN
-// we want a wmain entry point
-#include "nsWindowsWMain.cpp"
+#elif defined(XP_UNIX)
+#include <sys/time.h>
+#include <sys/resource.h>
 #endif
-#endif
-#ifdef XP_MACOSX
-#include <CoreFoundation/CoreFoundation.h>
-#endif /* XP_MACOSX */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+#include "plstr.h"
+#include "prprf.h"
+#include "prenv.h"
+
 #include "nsCOMPtr.h"
 #include "nsILocalFile.h"
 #include "nsStringGlue.h"
 
+#ifdef XP_WIN
+// we want a wmain entry point
+#include "nsWindowsWMain.cpp"
+#define snprintf _snprintf
+#define strcasecmp _stricmp
+#endif
+#include "BinaryPath.h"
+
+#include "nsXPCOMPrivate.h" // for MAXPATHLEN and XPCOM_DLL
+
+#include "mozilla/Telemetry.h"
+
 #include "koStart.h"
 
-int main(int argc, char* argv[])
+static void Output(const char *fmt, ... )
 {
-    int rv;
+  va_list ap;
+  va_start(ap, fmt);
+
+#if defined(XP_WIN) && !MOZ_WINCONSOLE
+  PRUnichar msg[2048];
+  _vsnwprintf(msg, sizeof(msg)/sizeof(msg[0]), NS_ConvertUTF8toUTF16(fmt).get(), ap);
+  MessageBoxW(NULL, msg, L"XULRunner", MB_OK | MB_ICONERROR);
+#else
+  vfprintf(stderr, fmt, ap);
+#endif
+
+  va_end(ap);
+}
+
+/**
+ * A helper class which calls NS_LogInit/NS_LogTerm in its scope.
+ */
+class ScopedLogging
+{
+public:
+  ScopedLogging() { NS_LogInit(); }
+  ~ScopedLogging() { NS_LogTerm(); }
+};
+
+XRE_CreateAppDataType XRE_CreateAppData;
+XRE_FreeAppDataType XRE_FreeAppData;
+#ifdef XRE_HAS_DLL_BLOCKLIST
+XRE_SetupDllBlocklistType XRE_SetupDllBlocklist;
+#endif
+XRE_TelemetryAccumulateType XRE_TelemetryAccumulate;
+XRE_mainType XRE_main;
+
+static const nsDynamicFunctionLoad kXULFuncs[] = {
+    { "XRE_CreateAppData", (NSFuncPtr*) &XRE_CreateAppData },
+    { "XRE_FreeAppData", (NSFuncPtr*) &XRE_FreeAppData },
+#ifdef XRE_HAS_DLL_BLOCKLIST
+    { "XRE_SetupDllBlocklist", (NSFuncPtr*) &XRE_SetupDllBlocklist },
+#endif
+    { "XRE_TelemetryAccumulate", (NSFuncPtr*) &XRE_TelemetryAccumulate },
+    { "XRE_main", (NSFuncPtr*) &XRE_main },
+    { nsnull, nsnull }
+};
+
+static int do_main(const char *exePath, int argc, char* argv[])
+{
+  nsCOMPtr<nsILocalFile> appini;
+#ifdef XP_WIN
+  // exePath comes from mozilla::BinaryPath::Get, which returns a UTF-8
+  // encoded path, so it is safe to convert it
+  nsresult rv = NS_NewLocalFile(NS_ConvertUTF8toUTF16(exePath), PR_FALSE,
+                                getter_AddRefs(appini));
+#else
+  nsresult rv = NS_NewNativeLocalFile(nsDependentCString(exePath), PR_FALSE,
+                                      getter_AddRefs(appini));
+#endif
+  if (NS_FAILED(rv)) {
+    Output("Couldn't get nsILocalFile for '%s'", exePath);
+    return 255;
+  }
+
+  appini->SetNativeLeafName(NS_LITERAL_CSTRING("application.ini"));
+  nsXREAppData *appData;
+  rv = XRE_CreateAppData(appini, &appData);
+  if (NS_FAILED(rv)) {
+    Output("Couldn't read application.ini");
+    return 255;
+  }
+
+#ifdef KOSTART_PLACEHOLDER
+    // The special Komodo startup gymnastics are only done when the
+    // properly configured koStart.h|c are built in as part of the
+    // *Komodo* build. After the default Mozilla build (in
+    // Mozilla-devel/...) we simply start the XRE.
+    return XRE_main(argc, argv, appData);
+#else
     int retval = 0;
     KoStartOptions options;
     KoStartHandle mutex = KS_BAD_HANDLE;
     KoStartHandle theMan = KS_BAD_HANDLE;
-
-    nsCOMPtr<nsILocalFile> appini;
-    nsresult r = XRE_GetBinaryPath(argv[0], getter_AddRefs(appini));
-    if (NS_FAILED(r)) {
-      fprintf(stderr, "Couldn't calculate the application directory. [%s]\n", argv[0]);
-      return 255;
-    }
-    appini->SetNativeLeafName(NS_LITERAL_CSTRING("application.ini"));
-    nsXREAppData *appData;
-    r = XRE_CreateAppData(appini, &appData);
-    if (NS_FAILED(r)) {
-      nsCAutoString pathName;
-      appini->GetNativePath(pathName);
-      fprintf(stderr, "Couldn't read application.ini [%s]\n", pathName.get());
-      return 255;
-    }
-
-    #ifdef XP_MACOSX
-        // MaC OSX: Disable press-and-hold for OSX 10.7, see bug 90870
-        CFPreferencesSetAppValue(CFSTR("ApplePressAndHoldEnabled"),
-                                 kCFBooleanFalse,
-                                 kCFPreferencesCurrentApplication);
-        CFPreferencesAppSynchronize(kCFPreferencesCurrentApplication);
-    #endif /* XP_MACOSX */
 
     // Handle command line arguments.
     rv = KoStart_HandleArgV(argc, argv, &options);
@@ -131,6 +199,92 @@ main_exit:
         KoStart_ReleaseTheMan(theMan);
     }
     return retval;
+#endif // USE_KOSTART
+}
+
+int main(int argc, char* argv[])
+{
+  char exePath[MAXPATHLEN];
+
+  nsresult rv = mozilla::BinaryPath::Get(argv[0], exePath);
+  if (NS_FAILED(rv)) {
+    Output("Couldn't calculate the application directory.\n");
+    return 255;
+  }
+
+  char *lastSlash = strrchr(exePath, XPCOM_FILE_PATH_SEPARATOR[0]);
+  if (!lastSlash || (lastSlash - exePath > MAXPATHLEN - sizeof(XPCOM_DLL) - 1))
+    return 255;
+
+  strcpy(++lastSlash, XPCOM_DLL);
+
+  int gotCounters;
+#if defined(XP_UNIX)
+  struct rusage initialRUsage;
+  gotCounters = !getrusage(RUSAGE_SELF, &initialRUsage);
+#elif defined(XP_WIN)
+  // GetProcessIoCounters().ReadOperationCount seems to have little to
+  // do with actual read operations. It reports 0 or 1 at this stage
+  // in the program. Luckily 1 coincides with when prefetch is
+  // enabled. If Windows prefetch didn't happen we can do our own
+  // faster dll preloading.
+  IO_COUNTERS ioCounters;
+  gotCounters = GetProcessIoCounters(GetCurrentProcess(), &ioCounters);
+  if (gotCounters && !ioCounters.ReadOperationCount)
+#endif
+  {
+      XPCOMGlueEnablePreload();
+  }
+
+
+  rv = XPCOMGlueStartup(exePath);
+  if (NS_FAILED(rv)) {
+    Output("Couldn't load XPCOM.\n");
+    return 255;
+  }
+
+  rv = XPCOMGlueLoadXULFunctions(kXULFuncs);
+  if (NS_FAILED(rv)) {
+    Output("Couldn't load XRE functions.\n");
+    return 255;
+  }
+
+#ifdef XRE_HAS_DLL_BLOCKLIST
+  XRE_SetupDllBlocklist();
+#endif
+
+  if (gotCounters) {
+#if defined(XP_WIN)
+    XRE_TelemetryAccumulate(mozilla::Telemetry::EARLY_GLUESTARTUP_READ_OPS,
+                            int(ioCounters.ReadOperationCount));
+    XRE_TelemetryAccumulate(mozilla::Telemetry::EARLY_GLUESTARTUP_READ_TRANSFER,
+                            int(ioCounters.ReadTransferCount / 1024));
+    IO_COUNTERS newIoCounters;
+    if (GetProcessIoCounters(GetCurrentProcess(), &newIoCounters)) {
+      XRE_TelemetryAccumulate(mozilla::Telemetry::GLUESTARTUP_READ_OPS,
+                              int(newIoCounters.ReadOperationCount - ioCounters.ReadOperationCount));
+      XRE_TelemetryAccumulate(mozilla::Telemetry::GLUESTARTUP_READ_TRANSFER,
+                              int((newIoCounters.ReadTransferCount - ioCounters.ReadTransferCount) / 1024));
+    }
+#elif defined(XP_UNIX)
+    XRE_TelemetryAccumulate(mozilla::Telemetry::EARLY_GLUESTARTUP_HARD_FAULTS,
+                            int(initialRUsage.ru_majflt));
+    struct rusage newRUsage;
+    if (!getrusage(RUSAGE_SELF, &newRUsage)) {
+      XRE_TelemetryAccumulate(mozilla::Telemetry::GLUESTARTUP_HARD_FAULTS,
+                              int(newRUsage.ru_majflt - initialRUsage.ru_majflt));
+    }
+#endif
+  }
+
+  int result;
+  {
+    ScopedLogging log;
+    result = do_main(exePath, argc, argv);
+  }
+
+  XPCOMGlueShutdown();
+  return result;
 }
 
 #if defined( XP_WIN ) && defined( WIN32 ) && !defined(__GNUC__)
