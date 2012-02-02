805922b2a94218b1db4739264808f658feef6294
Eric Promislow <ericp@activestate.com>
2012-02-02 20:59:46 +0000

    Ensure the Python3 sample is appropriate for openkomodo. Ref http://bugs.activestate.com/show_bug.cgi?id=92574
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@9657 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/samples/python3_sample.p.py
+++ src/samples/python3_sample.p.py
@@ -60,44 +60,6 @@ print("Wrapped text:\n%s" % s)
 #     created with "Tabstop" placeholders for the class name and body
 #     code.
 
-# #if WITH_DEBUGGING
-# Debugging:
-#   1. Set a breakpoint by clicking the left margin on line 59 ("sum = 0.0").
-#   2. Press 'F5' to invoke the debugger; click "OK" to accept the default.
-#   3. Press 'F11' to step into "sum = 0.0".
-#   4. View variables and output on the Debug tab.
-#   5. See the "Debug" menu for additional debug commands.
-#   6. Press 'Shift'+'F5' to stop.
-
-def printTotal(priceList):
-    '''Prints a total of all prices in the list'''
-    sum = 0.0
-    for price in prices:
-        sum = sum + price
-    print("the sum of the prices is", sum)
-
-prices = [5.50, 6.25, 7.00, 3.15]
-printTotal(prices)
-
-# Interactive Shell:
-#   1. Select lines 57 to 65, and then press 'Ctrl'+'C'.
-#   2. Select Tools|Interactive Shell|Start New Python Shell.
-#   3. Press 'Ctrl'+'V'.
-#   4. Press 'Enter'. The output is displayed in the Python Shell beneath
-#      the code snippet.
-# #endif
-
-# #if WITH_CODE_BROWSER
-# Code Browsing:
-#   1. If necesssary, enable Komodo's code intelligence (Edit|Preferences|Code Intelligence).
-#   2. Select View|Tabs|Code Browser.
-#   3. On the Code tab, click the plus sign next to "python_sample".
-#   4. If necessary, display the Code Description pane by clicking the
-#      "Show/Hide Description" button at the bottom of the Code Browser.
-#   5. Select "Imports". The Code Description pane indicates that the file
-#      uses the string module.
-# #endif
-
 # More:
 #   - Press 'F1' to view the Komodo User Guide.
 #   - Select Help|Tutorial|Python Tutorial for more about Komodo and Python.
