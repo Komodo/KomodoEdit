91e30dd25968fd42af8c8b14aa9a6081f89bdd63
Mark Yen <marky@activestate.com>
2012-02-28 21:54:27 +0000

    find: set endOffset when doing a find in multiple open files and looping http://bugs.activestate.com/show_bug.cgi?id=93056
    (integrated from komodo (trunk) change 69808 by marky)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@9848 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/chrome/komodo/content/find/find_functions.js
+++ src/chrome/komodo/content/find/find_functions.js
@@ -444,9 +444,11 @@ this._setupAndFindNext = function _SetupAndFindNext(editor, context, pattern, mo
             // documents" should reach this code.)
             text = scimoz.text;
             if (_findSvc.options.searchBackward) {
-                startOffset = ko.stringutils.bytelength(text.slice(0, text.length - 1));
+                startOffset = scimoz.length;
+                endOffset = 0;
             } else {
                 startOffset = 0;
+                endOffset = scimoz.length;
             }
             contextOffset = 0;
 
