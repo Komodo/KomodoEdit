73d6621b2e2770341aec7f0f2ff6d54bb816115e
Todd Whiteman <toddw@activestate.com>
2012-04-13 20:50:43 +0000

    ci: gencix: python: update string helper overrides
    (integrated from komodo (trunk) change 70835 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@10034 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/codeintel/support/gencix/python/helpers/string_helper.py
+++ src/codeintel/support/gencix/python/helpers/string_helper.py
@@ -1,4 +1,7 @@
 # string_helper.py
 
 # Override the "string.split" to return a list.
-function_overrides = {'split': {'returns': 'list'}}
+function_overrides = {
+    'split': {'returns': 'list'},
+    'rsplit': {'returns': 'list'},
+}
