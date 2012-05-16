16d6d41bd2f14546c1632816987ebdb762ff418f
Todd Whiteman <toddw@activestate.com>
2012-05-16 00:42:52 +0000

    fix: Preferences: Node.js: Ensure the interpreter is properly detected http://bugs.activestate.com/show_bug.cgi?id=91959 rn=
    (integrated from komodo (trunk) change 71423 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@10168 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/chrome/komodo/content/pref/pref-nodejs.js
+++ src/chrome/komodo/content/pref/pref-nodejs.js
@@ -5,7 +5,6 @@ var log = ko.logging.getLogger("pref-nodejs");
 //log.setLevel(ko.logging.LOG_INFO);
 
 //---- globals
-var prefExecutable = null;
 var programmingLanguage = "Nodejs";
 var _bundle = Components.classes["@mozilla.org/intl/stringbundle;1"]
             .getService(Components.interfaces.nsIStringBundleService)
@@ -21,25 +20,38 @@ function OnPreferencePageOK(prefset)
 }
 
 function PrefNodejs_OnLoad() {
-    parent.prefLog.setLevel(ko.logging.LOG_DEBUG);
+    PrefNodejs_PopulateInterps();
+    parent.hPrefWindow.onpageload();
+}
 
+function PrefNodejs_PopulateInterps()
+{
     var availInterpList = document.getElementById("nodejsDefaultInterpreter");
-
+    // remove any existing items and add a "finding..." one
     availInterpList.removeAllItems();
-    availInterpList.appendItem(_bundle.GetStringFromName("findOnPath.label"),'');
 
+    // get a list of installed Node interpreters
+    var nodeAppInfoEx = Components.classes["@activestate.com/koAppInfoEx?app=NodeJS;1"].
+                            createInstance(Components.interfaces.koIAppInfoEx);
+    var numFound = new Object();
+    var availInterps = nodeAppInfoEx.FindExecutables(numFound);
+
+    availInterpList.appendItem(_bundle.GetStringFromName("findOnPath.label"),'');
     var found = false;
     // populate the tree listing them
-    if (parent.hPrefWindow.prefset.hasStringPref('nodejsDefaultInterpreter')) {
-        prefExecutable = parent.hPrefWindow.prefset.getStringPref('nodejsDefaultInterpreter');
-        if (prefExecutable) {
-            availInterpList.appendItem(prefExecutable, prefExecutable);
-        } else {
-            document.getElementById("no-avail-interps-message").removeAttribute("collapsed");
+    if (availInterps.length == 0) {
+        // tell the user no interpreter was found and direct them to
+        // ActiveState to get one
+        document.getElementById("no-avail-interps-message").removeAttribute("collapsed");
+    } else {
+        availInterpList.selectedIndex = 0;
+        for (var i=0; i < availInterps.length; i++) {
+            availInterpList.appendItem(availInterps[i], availInterps[i]);
         }
+        // First one on the list is either the preferenced interpreter or the
+        // first one found on the path.
+        availInterpList.selectedIndex = 1;
     }
-
-    parent.hPrefWindow.onpageload();
 }
 
 function loadNodejsExecutable()
