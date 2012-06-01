f011ed1232f3a5a2058359d44583ae1be930e4ef
Eric Promislow <ericp@activestate.com>
2012-06-01 20:22:52 +0000

    Managed to generate CIX'es for Ruby 1.9.2 and 1.9.3
    (integrated from komodo (trunk) change 71727 by ericp)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@10257 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/codeintel/support/gencix/ruby/gen-from-ri-dir.rb
+++ src/codeintel/support/gencix/ruby/gen-from-ri-dir.rb
@@ -17,6 +17,7 @@ o = OptionParser.new do |opt_parser|
   opt_parser.on('--ri-dir DIR', String) { |val| opts['ri-dir'] = val }
   opt_parser.on('--lib-dir DIR', String) { |val| opts['lib-dir'] = val }
   opt_parser.on('--output-dir DIR', String) { |val| opts['output-dir'] = val }
+  opt_parser.on('-g', '--generate-lines') { |val| opts['generate-lines'] = val }
   opt_parser.on('-o', '--output-file FILE', String) { |val| opts['output-file'] = val }
   opt_parser.on('-v', '--verbose') { |val| opts['verbose'] = val }
   opt_parser.on('-w', '--warnings') { |val| opts['warnings'] = val }
@@ -63,14 +64,16 @@ class CacheForest
   def initialize(opts)
     @riDir = riDir = opts['ri-dir']
     @libDir = opts['lib-dir']
+    @generateLines = opts['generate-lines']
     @cache = RDoc::RI::Store.new(riDir, nil) # 2nd arg => cache.type, not used
     @cache.load_cache
     @cacheDirs = Dir.entries(riDir).find_all{|p| p != "." && p != ".." && File.directory?(riDir + "/" + p)}.sort
     # For comments:
-    @@line_limit = 6
-    @@size_limit = 256
+    @@line_limit = 12
+    @@size_limit = 10000
     @@size_ptn = Regexp.new("^.{#{@@size_limit}}\\S*")
     @failedToLoads = []
+    @failedToLoadSubpackages = []
     @rejectedModules = []
     #@tracker = {}
   end
@@ -78,11 +81,13 @@ class CacheForest
   def report
     $stderr.puts "# unloadable methods: #{@failedToLoads.size}"
     $stderr.puts "# rejected modules: #{@rejectedModules.size}"
+    $stderr.puts "# unloadable subpackages: #{@failedToLoadSubpackages.size}"
+    
   end
   
   @@badNames = ["ARGF.class", "Addrinfo", "BasicObject", "Etc", "BasicSocket",
                 "CacheForest", "CGI", "Debugger", "FileUtils",
-                "IPSocket", "LineCache", "Logger", "Monitor",
+                "IPSocket", "LineCache", "Logger",
                 "OptionParser", "PP", "PrettyPrint", "RDoc", "TraceLineNumbers",
                 "URI"]
   
@@ -92,7 +97,16 @@ class CacheForest
         name["FRAME_"] || name["IB_STATE_"] || name["RB_EVENT_"] || name["SCRIPT_LINES"] || \
         name["SINGLE_"] || name["STOP_REASON_"]
     end
-    @builtin_names << "ARGF.class"
+    #@builtin_names << "ARGF.class"
+    if !@builtin_names.include?("Monitor")
+      begin
+        require "monitor"
+        @builtin_names << "Monitor"
+        @builtin_names << "MonitorMixin"
+      rescue LoadError
+        $stderr.puts("Can't load monitor")
+      end
+    end
     @builtin_names.each do |name|
       if @cacheDirs.include?(name)
         @cacheDirs.delete(name)
@@ -107,10 +121,10 @@ class CacheForest
     parts = full_name.split('::')
     if path.nil?
       path, fullPath = get_src_file(m, parts.clone)
-      if path.nil?
-        @rejectedModules << fq_module_name
-        return
-      end
+      #if path.nil?
+      #  @rejectedModules << fq_module_name
+      #  return
+      #end
     end
     return m, path, fullPath
   end
@@ -163,15 +177,46 @@ class CacheForest
     @fout = $stdout; #File.open(File.join(outDir, outFile), 'wb')
     @fout.puts(<<"_EOD_")
 <?xml version="1.0" encoding="utf-8" ?>
-<codeintel version="2.0" name="Ruby 1.9" description="Cix data for Ruby 1.9">
-  <file lang="Ruby" mtime="#{Time.now.to_i}" path="ruby-1.9.cix">
+<codeintel version="2.0" name="Ruby #{RUBY_VERSION}" description="Cix data for Ruby #{RUBY_VERSION}">
+  <file lang="Ruby" mtime="#{Time.now.to_i}" path="ruby-#{RUBY_VERSION}.cix">
 _EOD_
     ind = "    "
     @module_name = "Kernel"
     m = @cache.load_class @module_name
+    @isBuiltin = true
+    #XXX !!!!
     writeTopLevelNames(ind, "*", m)
+    @isBuiltin = false
+    @blobs = {} # map an array of XML scopes to blob names
+    @currBlobLibs = [] # a stack
     @cacheDirs.each do | cacheDir |
-      writeBlobFromCache(ind, cacheDir)
+        writeBlobFromCache(ind, cacheDir)
+    end
+    @blobs.sort.each do | libname, scopeArray |
+      next if scopeArray.size == 0
+      @fout.puts(%Q(#{ind}<scope name="#{libname}" ilk="blob" lang="Ruby">))
+      # Collapse duplicate outer things
+      fixedScopeArray = []
+      scPrevLines = scopeArray[0].split("\n")
+      prevFirstLine = scPrevLines[0]
+      lim = scopeArray.size
+      i = 1
+      while i < lim
+        sc = scopeArray[i]
+        scLines = sc.split("\n")
+        if (currFirstLine = scLines[0]) == prevFirstLine
+          fixedScopeArray += scPrevLines[0 .. -2]
+          scPrevLines = scLines[1 .. -1]
+        else
+          fixedScopeArray += scPrevLines
+          prevFirstLine = currFirstLine
+          scPrevLines = scLines
+        end
+        i += 1
+      end
+      fixedScopeArray += scPrevLines
+      @fout.puts(fixedScopeArray.join("\n"))
+      @fout.puts(%Q(#{ind}</scope>))
     end
     @fout.puts(<<'_EOD_')
   </file>
@@ -230,7 +275,7 @@ _EOD_
     @fout.write(%Q(#{ind}<scope name="#{name}" ilk="function"))
     begin
       if args && args[:classFunction]
-        @fout.write(%Q( attributes="__classmethods__))
+        @fout.write(%Q( attributes="__classmethod__))
         if name == 'new '
           @fout.write(%Q __ctor__ )
         end
@@ -282,20 +327,24 @@ _EOD_
       # Do the nested stuff
       ind2 = ind + "  "
       begin
-        (liveObject.included_modules - [Kernel]).each do |iname|
+        (liveObject.included_modules - [Kernel]).each do |inameMod|
+          iname = inameMod.name
           next if iname["#<"]
           @fout.puts(%Q(#{ind2}<import symbol="#{iname}" />))
         end
       rescue
+        $stderr.puts("Error getting included modules from #{name}: #{$!}")
       end
       # Now get modules, classes, other constants, and methods found on this object.
       if ilk == "namespace"
-        methods = (liveObject.instance_methods - Module.methods).sort
+        methods = (liveObject.instance_methods + liveObject.methods - Module.methods).sort
       else
         methods = (liveObject.instance_methods - liveObject.superclass.instance_methods).sort
       end
-      liveObject.included_modules.each do | mod |
-        methods -= mod.instance_methods
+      if name != "Object" && name != "BasicObject"
+        liveObject.included_modules.each do | mod |
+          methods -= mod.instance_methods
+        end
       end
       
       methods.each do |name2Sym|
@@ -312,6 +361,7 @@ _EOD_
       if ilk == "class"
         methods = liveObject.singleton_methods - liveObject.superclass.singleton_methods
         liveObject.included_modules.each do | mod |
+          next if mod == Kernel
           methods -= mod.methods
         end
         methods.sort.each do |name2Sym|
@@ -335,8 +385,14 @@ _EOD_
       
       return if ["Class", "Module", "Object"].include?(name)
       return if ["Struct::Group", "Struct::Passwd", "Struct::Tms"].include?(liveObject.name)
+      #if name == "File"
+      #  puts "Stop here"
+      #end
       sortedObjects = liveObject.constants
       begin
+        if sortedObjects.include?(:Constants)
+          sortedObjects -= liveObject.class_eval("Constants").constants
+        end
         sortedObjects = sortedObjects.sort
       rescue
         $stderr.puts "Error sorting: #{$!}"
@@ -345,12 +401,13 @@ _EOD_
         name2 = name2sym.to_s
         liveObject2 = rdocObject2 = type2 = nil
         begin
-          if ilk == "class"
-            fqname2 = fqname + "." + name2
-          else
-            fqname2 = fqname + "::" + name2
-          end
-          liveObject2 = liveObject.class_eval(fqname2)
+          fqname2 = fqname + "::" + name2
+          #if ilk == "class"
+          #  fqname2 = fqname + "." + name2
+          #else
+          #  fqname2 = fqname + "::" + name2
+          #end
+          liveObject2 = liveObject.class_eval(name2)
           if liveObject2.is_a?(Module) || liveObject2.is_a?(Class)
             begin
               rdocObject2 = @cache.load_class(liveObject2.name)
@@ -372,72 +429,252 @@ _EOD_
   end
   
   def writeConstantItem(ind, name, liveObject)
-    @fout.puts(%Q(#{ind}<variable attributes="__const__" citdl="#{liveObject.class.to_s}" name="#{name}" />))
+    #TODO Add line/lineend attributes
+    @fout.puts(%Q(#{ind}<variable name="#{name}" attributes="__const__" citdl="#{liveObject.class.to_s}" />))
   end
 
   # Here we write out info obtained just from the rdoc/ri cache, but
   # don't instantiate any objects.
   
+  def getFileLines(path)
+    return [] unless @generateLines
+    begin
+      lines = []
+      File.open(File::join(@libDir, path + ".rb"), 'r') do |fd|
+        fd.readlines.each_with_index do |item, index|
+          if item =~ /\A\s*(?:def|class|module|end)\b/
+            lines << [index + 1, item]
+          end
+        end
+      end
+    rescue
+    end
+    return lines
+  end
+  
+  def getLineBoundaries(name, type, lines)
+    ptn = /\A(\s*)#{type}\s*#{name}/
+    lineStart = nil
+    nextIdx = 0
+    targetPtn = otherPtn = nil
+    firstInnerIdx = 0
+    lines.each_with_index do |linePair, i|
+      index, line = linePair
+      if m = (ptn.match(line))
+        lineStart = index
+        targetPtn = /\A#{m[1]}end\b/
+        otherPtn = /\A#{m[1]}(?:class|module|def)\b/
+        firstInnerIdx = i + 1
+        break
+      end
+    end
+    return [nil, nil, []] if !lineStart
+    nesting = 0
+    lines[firstInnerIdx .. -1].each do | i, line |
+      if line =~ otherPtn
+        nesting += 1
+      elsif line =~ targetPtn
+        nesting -= 1
+        if nesting <= 0
+          return [lineStart, i, lines[firstInnerIdx .. i]]
+        end
+      end
+    end
+    return [nil, nil, []]
+  end
+
+  def getRequirablePathFromPath(path)
+    match = /\Aext\/(.*)\/[^\/]+\.c\Z/.match(path)
+    if match
+      return match[1]
+    else
+      match = /\Aext\/.*?\/lib\/(.*)\Z/.match(path)
+      if match
+        return match[1]
+      else
+        return path
+      end
+    end
+  end
+  
   def writeBlobFromCache(ind, cacheDir)
     m, path, fullPath = load_module(cacheDir)
-    return if !m || blank?(path)
-    @fout.puts(%Q(#{ind}<scope ilk="blob" lang="Ruby" name="#{path}">))
-    begin
-      writeModuleOrClass(ind + "  ", cacheDir, cacheDir, @riDir + "/" + cacheDir, m)
-    ensure
-      @fout.puts(%Q(#{ind}</scope>))
-    end
-  end
-
-  def writeModuleOrClass(ind, name, fqName, currDir, m)
-    if m.module?
-      writeModuleInfo(ind, name, fqName, currDir, m)
+    return if !m
+    if blank?(path)
+      # Just guess what the path would be
+      if cacheDir[':']
+        $stderr.puts("can't get a path for lib #{cacheDir}, giving up")
+        @rejectedModules << cacheDir
+        return
+      else
+        cpath = path = cacheDir.downcase
+        begin
+          require cpath
+        rescue LoadError
+          return
+        end
+        $stderr.puts("Faking it for library #{cacheDir}")
+      end
+      lines = []
     else
-      writeClassInfo(ind, name, fqName, currDir, m)
+      lines = getFileLines(path)
+      cpath = getRequirablePathFromPath(path)
+    end
+    return if cpath.ends_with?(".c")
+    @currBlob = ""
+    @blobs[cpath] = [] if !@blobs.has_key?(cpath)
+    peers = writeModuleOrClass(ind + "  ", cacheDir, cacheDir, @riDir + "/" + cacheDir, m, path, lines)
+    @blobs[cpath] << @currBlob
+    peers.each do | peerName |
+      writePeerBlobFromCache(ind, peerName)
+    end
+  end
+  
+  def writePeerBlobFromCache(ind, cacheDir)
+    # cacheDir is probably fully-qualified, but we want to start at the top name,
+    # but pull info from an ri-subdir
+    m, path, fullPath = load_module(cacheDir)
+    return if !m || blank?(path)
+    lines = getFileLines(path)
+    cpath = getRequirablePathFromPath(path)
+    return if cpath.ends_with?(".c")
+    @currBlobLibs.push(@currBlob)
+    @currBlob = ""
+    @blobs[cpath] = [] if !@blobs.has_key?(cpath)
+    peers = []
+    begin
+      # Write out module and class headers for the leading parts until we get to the main item.
+      parts = cacheDir.split("::")
+      leadingParts = parts[0 .. -2]
+      writeLeadingParts(ind + "  ", leadingParts, lines) do | ind2, narrowedLines |
+        peers = writeModuleOrClass(ind2, parts[-1], cacheDir, @riDir + "/" + cacheDir.gsub('::', '/'), m, path, lines)
+      end
+    rescue
+      $stderr.puts("Error in writeModuleOrClass:#{$!}")
+    end
+    @blobs[cpath] << @currBlob
+    @currBlob = @currBlobLibs.pop
+    peers.each do | peerName |
+      writePeerBlobFromCache(ind, peerName)
+    end
+  end
+  
+  def writeLeadingParts(ind, parts, lines, &blk)
+    # Find the start of the first part, either a module or class
+    if parts.size == 0
+      blk.call(ind, lines)
+      return
+    end
+    part = parts[0]
+    ptn = /\A(\s*)(class|module)\s+(.*?#{part})/
+    ws = cixIlk = ilk = target = startingIndex = nil
+    lines.each_with_index do | linePair, i |
+      m = ptn.match(linePair[1])
+      if m
+        ws = m[1]
+        ilk = m[2]
+        cixIlk = ilk == "module" ? "namespace" : "class"
+        target = m[3]
+        startingIndex = i
+        break
+      end
+    end
+    # If we don't know what it is, guess and call it a module (namespace)
+    stag = %Q(#{ind}<scope name="#{part}" ilk="#{cixIlk || "namespace"}")
+    if !ws.nil?
+      lineStart, lineEnd, narrowedLines = getLineBoundaries(target, ilk, lines)
+      if lineStart && lineEnd
+        stag += %Q( line="#{lineStart}" lineend="#{lineEnd}")
+      end
+    end
+    @currBlob += stag + ">\n"
+    begin
+      writeLeadingParts(ind + "  ", parts[1 .. -1], narrowedLines || [], &blk)
+    rescue
+      $stderr.puts("Error writing nested writeLeadingParts: #{$!}")
+    ensure
+      @currBlob += %Q(#{ind}</scope>\n)
     end
   end
 
-  def writeModuleInfo(ind, name, fqName, currDir, m)
-    @fout.puts(%Q(#{ind}<scope ilk="namespace" name="#{name}">\n))
+  def writeModuleOrClass(ind, name, fqName, currDir, m, path, flines)
+    if m.module?
+      ilk = "namespace"
+      rubyIlk = "module"
+    else
+      ilk = rubyIlk = "class"
+    end
+    lineStart, lineEnd, innerLines = getLineBoundaries(name, rubyIlk, flines)
+    peers = []
+    stag = %Q(#{ind}<scope name="#{name}" ilk="#{ilk}")
+    if ilk == "class" && (superclass = m.superclass)
+      stag += %Q( classrefs="#{superclass}")
+    end
+    if lineStart && lineEnd
+      stag += %Q( line="#{lineStart}" lineend="#{lineEnd}")
+    end
+    stag += ">\n"
+    @currBlob += stag
     ind2 = ind + "  "
     begin
-      writeBlobItems(ind2, fqName, m)
-      writePackageChildren(ind2, fqName, "::", currDir)
+      begin
+        (m.includes.map(&:full_name) - ["Kernel"]).sort.each do |iname|
+          next if iname["#<"]
+          @currBlob += %Q(#{ind2}<import symbol="#{iname}" />\n)
+        end
+      rescue
+        $stderr.puts("Error getting include out of #{rubyIlk} #{name}")
+      end
+      writeBlobItems(ind2, fqName, m, innerLines)
+      peers = writePackageChildren(ind2, fqName, "::", currDir, path, innerLines)
     ensure
-      @fout.puts(%Q(#{ind}</scope>\n));
+      @currBlob += %Q(#{ind}</scope>\n)
     end
+    return peers
   end
   
-  def writeClassInfo(ind, name, fqName, currDir, m)
-    @fout.puts(%Q(#{ind}<scope ilk="class" name="#{name}">\n))
-    ind2 = ind + "  "
-    begin
-      writeBlobItems(ind2, fqName, m)
-      writePackageChildren(ind2, fqName, "::", currDir)
-    ensure
-      @fout.puts(%Q(#{ind}</scope>\n));
-    end
-  end
-  
-  def writePackageChildren(ind, fqName, fqSep, currDir)
+  def writePackageChildren(ind, fqName, fqSep, currDir, path, flines)
+    peers = []
     kids = Dir.entries(currDir).find_all{|p| p != "." && p != ".." && File.directory?(currDir + "/" + p)}.sort
     kids.each do | dirName |
       clsName = fqName + fqSep + dirName
-      m = @cache.load_class(clsName)
+      #m = @cache.load_class(clsName)
+      m, path2, fullPath2 = load_module(clsName)
       if !m
-        $stderr.puts("Couldn't get class #{clsName}: {$!}")
+        @failedToLoadSubpackages << clsName
+        next
       end
-      writeModuleOrClass(ind, dirName, clsName, currDir + "/" + dirName, m)
+      if path2.nil?
+        path2 = path
+        flines2 = flines
+      elsif path2 == path
+        flines2 = flines
+      else
+        #peers << [ dirName, m, path2]
+        peers << clsName
+        next
+      end
+      writeModuleOrClass(ind, dirName, clsName, currDir + "/" + dirName, m, path2, flines2)
     end
+    return peers
   end
   
-  def writeBlobItems(ind, fqName, m)
+  def writeBlobItems(ind, fqName, m, flines)
     methods = m.method_list
     methods.each do |func|
       if func.visibility == :public
-        func_name = func.name[0]
-        writeMethod(ind + "  ", fqName, m, func_name, func.singleton)
+        func_name = func.name
+        func_name = func_name[0] unless func_name.class == String
+        writeMethod(ind, fqName, m, func_name, func.singleton, flines)
       end
+    end
+    attributes = m.attributes.sort { |a1, a2| a1.name <=> a2.name }
+    attributes.each do | attr |
+      @currBlob += %Q(#{ind}<scope ilk="function" name="#{e1(attr.name)}" />\n) if attr.rw["R"]
+      @currBlob += %Q(#{ind}<scope ilk="function" name="#{e1(attr.name)}=" />\n) if attr.rw["W"]
+    end
+    m.constants.map(&:name).find_all{|s| s =~ /\A[A-Z_]+\Z/}.each do |s|
+      @currBlob += %Q(#{ind}<variable name="#{s}" attributes="__const__" citdl="Object" />\n)
     end
   end
   
@@ -445,10 +682,20 @@ _EOD_
     s1 = s.gsub(/<\/?(?:code|em|i|tt)\b.*?>/, '');
     s2 = s1.gsub('&', '&amp;').gsub('<', '&lt;').gsub('>', '&gt;').gsub('\'', '&apos;').gsub('"', '&quot;')
     s3 = s2.gsub(/[^\t\n\x20-\x7f]/, '')
-    s3
+    s4 = s3.gsub('#=', '=') # marshal artefact?
+    s4
   end
   
-  def writeMethod(ind, fqName, m, func_name, isSingleton)
+  def writeMethod(ind, fqName, m, func_name, isSingleton, flines)
+    return if func_name =~ /\A\W+\Z/
+    if isSingleton
+      attributes = "__classmethod__"
+      attributes += " __ctor__" if func_name == "new"
+      prefix = "#"
+    else
+      prefix = ""
+      attributes = nil
+    end
     prefix = isSingleton ? "" : "#"
     begin
       func = @cache.load_method(fqName.gsub('.', "::"), prefix + func_name)
@@ -456,8 +703,6 @@ _EOD_
       $stderr.puts("Can't load method #{fqName.gsub('.', "::")} == #{prefix + func_name}")
       return
     end
-    #func = @cache.load_method(fqName.gsub('.', "::"), prefix + func_name)
-    attributes = func_name == "new" ? "__ctor__" : nil
     x = []
     x.push([:name, e1(func_name)])
     x.push([:ilk, :function.to_s])
@@ -470,6 +715,13 @@ _EOD_
       end 
     end
     x.push([:attributes, attributes]) if attributes
+    
+    lineStart, lineEnd, innerLines = getLineBoundaries(func_name == "new" ? "initialize" : func_name, "def", flines)
+    if lineStart
+      x.push([:line, lineStart])
+      x.push([:lineend, lineEnd || lineStart])
+    end
+    
     doc = shorten(flatten_comments(func.comment))
     x.push([:doc, doc]) if doc.size > 0
     
@@ -477,8 +729,8 @@ _EOD_
     x.each do |name, val|
       s += %Q{ #{name.to_s}="#{val}"}
     end
-    s += " />"
-    @fout.puts(s)
+    s += " />\n"
+    @currBlob += s
     puts "Stop here" if false
   end
   
@@ -501,6 +753,8 @@ _EOD_
         s << part.parts.map {|i| flatten_comments(i)}.join("&#xA;")
       elsif part.class == RDoc::Markup::List
         s << part.items.map {|i| flatten_comments(i)}.join("&#xA;")
+      elsif part.class == RDoc::Markup::Rule
+        # do nothing, it's a horiz bar
       else
         $stderr.puts("Found unknown part type #{part.class.name}")
       end
