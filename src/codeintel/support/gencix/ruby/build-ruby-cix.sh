6a694e032b874c47c9eb3ac45758e2077ab0f2a2
Eric Promislow <ericp@activestate.com>
2012-06-01 20:20:37 +0000

    Shell script for running the 1.9 cix-builder
    (integrated from komodo (trunk) change 71684 by ericp)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@10255 b87d16e7-d29f-4ee1-b5ec-817d645d335f

---
+++ src/codeintel/support/gencix/ruby/build-ruby-cix.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+RUBY=/home/ericp/opt/ruby-1.9.2-p136/bin/ruby
+
+$RUBY gen-from-ri-dir.rb --ri-dir /home/ericp/lab/ruby/ruby-1.9.3-p125/.ext/rdoc --lib-dir=/home/ericp/lab/ruby/ruby-1.9.3-p125/lib
+
+
