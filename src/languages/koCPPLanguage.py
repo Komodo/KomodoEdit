e5d4035eb58a5dd4dc0582acad9d7c1b4b601978
Eric Promislow <ericp@activestate.com>
2012-06-11 21:54:53 +0000

    Editor: update C++ keywords. Ref http://bugs.activestate.com/show_bug.cgi?id=92933
    (integrated from komodo (trunk) change 71901 by ericp)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@10307 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/languages/koCPPLanguage.py
+++ src/languages/koCPPLanguage.py
@@ -136,13 +136,13 @@ int main(int argc, char *argv[])
     
     _keywords = ["and", "and_eq",  "asm", "auto", "bitand", "bitor", "bool", "break",
                  "case", "catch", "char", "class", "compl", "const", "const_cast",
-                 "continue", "default", "delete", "do", "double", "else", "enum",
+                 "continue", "default", "delete", "do", "double", "dynamic_cast", "else", "enum",
                  "explicit", "export", "extern", "false", "float", "for", "friend", "goto",
-                 "if", "inline", "int", "long", "mutable", "new", "not", "not_eq", "operator",
+                 "if", "inline", "int", "long", "mutable", "namespace", "new", "not", "not_eq", "operator",
                  "or", "or_eq", "private", "protected", "public", "register",
                  "reinterpret_cast", "return", "short", "signed", "sizeof", "static",
                  "static_cast", "struct", "switch", "template", "this", "throw", "true",
-                 "try", "typedef", "typename", "union", "unsigned", "using", "virtual", "void", "volatile",
+                 "try", "typedef", "typeid", "typename", "union", "unsigned", "using", "virtual", "void", "volatile",
                  "wchar_t", "while", "xor", "xor_eq"]
 
     # XXX uncomment these to integrate the info and linter classes
