af94f7022a8e2a6993d51f5f004c6eb1d77781d6
Eric Promislow <ericp@activestate.com>
2012-07-17 22:41:45 +0000

    Places: Prefs: Read the show-full-path-tooltip at startup, not just when the pref changes. Ref http://bugs.activestate.com/show_bug.cgi?id=94871 rn=
    (integrated from komodo (trunk) change 72631 by ericp)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@10519 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/modules/places/content/places.js
+++ src/modules/places/content/places.js
@@ -2991,6 +2991,7 @@ this.onLoad_aux = function places_onLoad_aux() {
     if (!_placePrefs.hasPref('syncAllFiles')) {
         _placePrefs.setBooleanPref('syncAllFiles', true);
     }
+    this.handle_show_fullPath_tooltip();
     _placePrefs.prefObserverService.addObserverForTopics(
         this, 1, ['show_fullPath_tooltip'], 1);
                                                          
@@ -3130,12 +3131,16 @@ this.onLoad_aux = function places_onLoad_aux() {
     mruProjectViewerID = setInterval(launch_createProjectMRUView, 50);
 }
 
+this.handle_show_fullPath_tooltip = function() {
+    var show_fullPath_tooltip = _placePrefs.getBooleanPref("show_fullPath_tooltip");
+    var tooltip = show_fullPath_tooltip ? "places-files-tree-popup" : null;
+    document.getElementById("places-files-tree-body").
+             setAttribute("tooltip", tooltip);
+};
+
 this.observe = function(subject, topic, data) {
     if (topic == "show_fullPath_tooltip") {
-        var show_fullPath_tooltip = _placePrefs.getBooleanPref("show_fullPath_tooltip");
-        var tooltip = show_fullPath_tooltip ? "places-files-tree-popup" : null;
-        document.getElementById("places-files-tree-body").
-            setAttribute("tooltip", tooltip);
+        this.handle_show_fullPath_tooltip();
     }
 }
 
