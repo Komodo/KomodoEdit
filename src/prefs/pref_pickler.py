2ace6735bd243dd9d91071ea0dc677c4b3613028
Todd Whiteman <toddw@activestate.com>
2012-11-28 00:25:05 +0000

    prefs: pickler: fixup dependencies and ensure it uses fresh prefs
    (integrated from komodo (trunk) change 75264 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@11184 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/prefs/pref_pickler.py
+++ src/prefs/pref_pickler.py
@@ -5,22 +5,29 @@ than the xml version - bug 96273.
 """
 
 import os
+import sys
+from os.path import abspath, dirname, exists, join
+
+# Remove the old picked version - if it exists.
+modpath = abspath(__file__)
+sys.path.append(dirname(dirname(dirname(modpath))))
+from bkconfig import supportDir
+filename = join(supportDir, "prefs.xmlc")
+if exists(filename):
+    os.remove(filename)
 
 from xpcom import _xpcom  # Necessary to initialize xpcom.
 from xpcom import components
 from xpcom.server import UnwrapObject
 
+# Load the Komodo preferences via xpcom.
 prefs = components.classes["@activestate.com/koPrefService;1"].\
                     getService(components.interfaces.koIPrefService).prefs
 defaultPrefs = UnwrapObject(prefs)
-# Walk up the chain until we find the top-most parent - as that's the default
-# prefs.
+
+# Walk up the pref chain, the top-most parent is *always* the default prefs.
 while defaultPrefs.parent:
     defaultPrefs = defaultPrefs.parent
 
-koDirSvc = components.classes["@activestate.com/koDirs;1"].\
-    getService(components.interfaces.koIDirs)
-filename = os.path.join(koDirSvc.supportDir, "prefs.xml")
-
 # Serialize it.
-defaultPrefs.serializeToFileFast(filename + "c")
+defaultPrefs.serializeToFileFast(filename)
