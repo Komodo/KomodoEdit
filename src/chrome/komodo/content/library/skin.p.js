08b3324d343f85a854b1f07469b6d998df33368d
Nathan Rijksen <nathanr@activestate.com>
2013-02-14 18:44:43 +0000

    Replace copyright with MPL licenseing block in ko.less, ko.skin and ko.openfiles
    (integrated from komodo (trunk) change 77002 by nathanr)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@11770 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/chrome/komodo/content/library/skin.p.js
+++ src/chrome/komodo/content/library/skin.p.js
@@ -1,5 +1,6 @@
-// Copyright (c) 2000-2013 ActiveState Software Inc.
-// See the file LICENSE.txt for licensing information.
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
  
 /* Komodo Skinning
  *
