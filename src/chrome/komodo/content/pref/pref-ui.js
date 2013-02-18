555d2d94b989f914c88c18eca806dd962e8a4574
Nathan Rijksen <nathanr@activestate.com>
2013-02-18 23:47:29 +0000

    Added UI to switch theme and toggle GTK detection - Bug #97507 r=marky
    (integrated from komodo (trunk) change 77095 by nathanr)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@11823 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/chrome/komodo/content/pref/pref-ui.js
+++ src/chrome/komodo/content/pref/pref-ui.js
@@ -1,31 +1,62 @@
 function PrefUi_OnLoad() {
     
+    const {interfaces: Ci} = Components;
     Components.utils.import("resource://gre/modules/Services.jsm");
-    
-    var menuList = document.getElementById('koSkin_custom_icons');
-    var items    = menuList.querySelectorAll('menuitem');
-    
-    for (let item of items)
+
+    var parseItems = function(items, dir)
     {
-        if (item.getAttribute('value') == '')
+        for (let item of items)
         {
-            continue;
+            let value = item.getAttribute('value');
+            if (value == '' ||
+                value.match(/^$|\/|\\/)) // skip full / empty paths
+            {
+                continue;
+            }
+
+            var file = Services.io.newURI("resource://app/chrome/" + dir + "/" +
+                                          item.getAttribute('value') +
+                                          "/chrome.manifest", null,null)
+                        .QueryInterface(Ci.nsIFileURL).file;
+
+            if (file.exists())
+            {
+                item.setAttribute('value', file.path);
+            }
+            else
+            {
+                item.parentNode.removeChild(item);
+            }
         }
-        
-        var file = Services.io.newURI("resource://app/chrome/iconsets/" +
-                                      item.getAttribute('value') +
-                                      "/chrome.manifest", null,null)
-                    .QueryInterface(Components.interfaces.nsIFileURL).file;
-        
-        if (file.exists())
-        {
-            item.setAttribute('value', file.path);
-        }
-        else
-        {
-            item.parentNode.removeChild(item);
-        }
-    }
+    };
     
+    // Load Icons
+    var items = document.querySelectorAll('#koSkin_custom_icons menuitem');
+    parseItems(items, 'iconsets');
+
+    // Load Skins
+    items = document.getElementById('koSkin_custom_skin')
+                            .querySelectorAll('menuitem');
+    parseItems(items, 'skins');
+    
+    // Hide the skin selector if there are no skins
+    document.getElementById('pref_appearance_skin_hbox').collapsed = (items.length == 0);
+    
+    // Run default actions (load prefs and their selected state)
     parent.hPrefWindow.onpageload();
+
+    // If gtk detection is true, disable the skin selection
+    if (window.navigator.platform.toLowerCase().indexOf("linux") != -1)
+    {
+        var checkboxDetect  = document.getElementById('koSkin_use_gtk_detection');
+        var menuSkin        = document.getElementById('koSkin_custom_skin');
+
+        var setSkinState = function()
+        {
+            menuSkin.disabled = checkboxDetect.checked;
+        }
+
+        checkboxDetect.addEventListener('click', setSkinState);
+        setSkinState();
+    }
 }
