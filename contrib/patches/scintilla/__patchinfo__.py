354495ae060f5965a8469b809d9877e79ecc2cce
Todd Whiteman <toddw@activestate.com>
2013-05-04 00:30:45 +0000

    macosx: editor: still need this abandon paint,  to avoid flickering - bug 97214 * but only apply this patch for the Mac - bug 98677
    (integrated from komodo (trunk) change 78969 by toddw)
    
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@12588 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- contrib/patches/scintilla/__patchinfo__.py
+++ contrib/patches/scintilla/__patchinfo__.py
@@ -1,5 +1,13 @@
 """Add these files to the lexers/scintilla tree."""
 
+import sys
+
+def patchfile_applicable(config, filepath):
+    if filepath.endswith("perf_no_abandon_paint.patch"):
+        # Only apply on Darwin - bug 98677.
+        return sys.platform == "darwin"
+    return True
+
 def remove(config):
     return [
         "lexers/LexTCL.cxx",
