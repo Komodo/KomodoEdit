2795422ed92fb19bea6eceed57ba86ee7200daf7
Nathan Rijksen <nathanr@activestate.com>
2013-06-11 23:21:08 +0000

    Skins: Convert skin init scripts to use URI's instead of paths
    (integrated from komodo (trunk) change 79683 by nathanr)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@12785 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/modules/gtk_dark/content/manifest/init.js
+++ src/modules/gtk_dark/content/manifest/init.js
@@ -1,9 +1,7 @@
 if (koSkin.skinHasChanged)
 {
-    Components.utils.import("resource://gre/modules/Services.jsm");
-    var file = Services.io.newURI("chrome://iconset-" +
-                                  "light" +
-                                  "/content/manifest/chrome.manifest", null,null)
-                .QueryInterface(Components.interfaces.nsIFileURL).file;
-    ko.prefs.setStringPref(koSkin.PREF_CUSTOM_ICONS, file.path);
+    ko.prefs.setStringPref(
+        koSkin.PREF_CUSTOM_ICONS,
+        "chrome://iconset-light/content/manifest/chrome.manifest"
+    );
 }
