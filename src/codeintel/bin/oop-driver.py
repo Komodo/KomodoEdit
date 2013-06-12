6077a7ac24198a4209ceb4943b04fa2008a1b199
Mark Yen <marky@activestate.com>
2013-06-12 22:34:17 +0000

    codeintel: limit resource usage in codeintel process (r=toddw) http://bugs.activestate.com/show_bug.cgi?id=93455
    (integrated from komodo (trunk) change 79720 by marky)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@12800 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/codeintel/bin/oop-driver.py
+++ src/codeintel/bin/oop-driver.py
@@ -70,6 +70,15 @@ def main(argv=[]):
 
     log = logging.getLogger("codeintel.oop.executable")
 
+    try:
+        set_process_limits()
+    except:
+        log.exception("Failed to set process memory/CPU limits")
+    try:
+        set_idle_priority()
+    except:
+        log.exception("Failed to set process CPU priority")
+
     old_sys_path = set(abspath(join(p)) for p in sys.path)
 
     for relpath in args.import_path:
@@ -95,6 +104,63 @@ def main(argv=[]):
     driver = Driver(db_base_dir=args.database_dir,
                     fd_in=fd_in, fd_out=fd_out)
     driver.start()
+
+def set_idle_priority():
+    """Attempt to set the process priority to idle"""
+    try:
+        os.nice(20)
+    except AttributeError:
+        pass # No os.nice on Windows
+    if sys.platform.startswith("linux"):
+        import platform
+        # Try using a syscall to set io priority...
+        __NR_ioprio_set = { # see Linux sources, Documentation/block/ioprio.txt
+            "i386": 289,
+            "x86_64": 251,
+        }.get(platform.machine())
+        if __NR_ioprio_set is not None:
+            libc = ctypes.cdll.LoadLibrary(ctypes.util.find_library("c"))
+            IOPRIO_WHO_PROCESS = 1
+            IOPRIO_CLASS_IDLE = 3
+            IOPRIO_CLASS_SHIFT = 13
+            libc.syscall(__NR_ioprio_set, IOPRIO_WHO_PROCESS, 0,
+                         IOPRIO_CLASS_IDLE << IOPRIO_CLASS_SHIFT)
+    elif sys.platform.startswith("win"):
+        from ctypes import wintypes
+        SetPriorityClass = ctypes.windll.kernel32.SetPriorityClass
+        SetPriorityClass.argtypes = [wintypes.HANDLE, wintypes.DWORD]
+        SetPriorityClass.restype = wintypes.BOOL
+        HANDLE_CURRENT_PROCESS = -1
+        IDLE_PRIORITY_CLASS = 0x00000040
+        SetPriorityClass(HANDLE_CURRENT_PROCESS, IDLE_PRIORITY_CLASS)
+        # On Vista+, this sets the I/O priority to very low
+        PROCESS_MODE_BACKGROUND_BEGIN = 0x00100000
+        SetPriorityClass(HANDLE_CURRENT_PROCESS, PROCESS_MODE_BACKGROUND_BEGIN)
+
+def set_process_limits():
+    if sys.platform.startswith("win"):
+        """Pre-allocate (but don't commit) a 1GB chunk of memory to prevent it
+        from actually being used by codeintel; this acts as a limit on the
+        amount of memory we can actually use.  It has no effects on performance
+        (since we're only eating address space, not RAM/swap) but helps to
+        prevent codeintel from blowing up the system.
+        """
+        from ctypes import wintypes
+        kernel32 = ctypes.WinDLL("kernel32", use_last_error=True)
+        VirtualAlloc = kernel32.VirtualAlloc
+        VirtualAlloc.argtypes = [wintypes.LPVOID, wintypes.ULONG, wintypes.DWORD, wintypes.DWORD]
+        VirtualAlloc.restype = wintypes.LPVOID
+        MEM_RESERVE = 0x00002000
+        MEM_TOP_DOWN = 0x00100000
+        PAGE_NOACCESS = 0x01
+        # we can only eat about 1GB; trying for 2GB causes the allocation to
+        # (harmlessly) fail, which doesn't accomplish our goals
+        waste = VirtualAlloc(None, 1<<30, MEM_RESERVE|MEM_TOP_DOWN, PAGE_NOACCESS)
+        if waste:
+            log.debug("Successfullly allocated: %r", waste)
+        else:
+            log.debug("Failed to reduce address space: %s",
+                      ctypes.WinError(ctypes.get_last_error()).strerror)
 
 
 if __name__ == '__main__':
