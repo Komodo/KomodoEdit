99cdad70ca8cf1728297599682b9832d6fb203b8
Eric Promislow <ericp@activestate.com>
2013-07-29 23:56:22 +0000

    More Python3-compatible codeintel fixes, and update the test file to include arrays with annotations.
    (integrated from komodo (trunk) change 80920 by ericp)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@13254 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/codeintel/test2/scan_inputs/py3/keyword_only_args.py
+++ src/codeintel/test2/scan_inputs/py3/keyword_only_args.py
@@ -8,3 +8,21 @@ def compare(a, b, *, key=None):
 
 def f(a: int=1):
     print(a)
+
+class Haulable(object):
+    pass
+
+class PackAnimal(object):
+    pass
+
+class Distance(object):
+    pass
+
+class Pack(object):
+    def sub(self) -> int:
+        return "any kind of object anyway"
+
+    def haul(self, item: Haulable, *vargs: PackAnimal) -> Distance:
+        pass
+
+print(Pack().sub())
