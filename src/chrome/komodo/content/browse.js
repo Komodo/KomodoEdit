e47e54fe4a2605215bb4b7f499091bc7ad0f3f7d
Carey Hoffman <careyh@activestate.com>
2013-08-07 21:53:43 +0000

    fix: Komodo UI: Update link and phrasing of bug report menu option http://bugs.activestate.com/show_bug.cgi?id=99883 Also implemented the tooltiptext in the menu which wasn't being used
    (integrated from komodo (trunk) change 81132 by careyh)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@13332 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/chrome/komodo/content/browse.js
+++ src/chrome/komodo/content/browse.js
@@ -241,7 +241,7 @@ this.updateHelpLanguagesPopup = function browse_UpdateHelpLanguagesPopup() {
 var tag2uri = {
     'mailLists': "http://aspn.activestate.com/ASPN/Mail/Browse/Threaded/komodo-discuss",
     'community': "http://community.activestate.com/products/Komodo",
-    'bugs': "http://bugs.activestate.com/query.cgi?format=specific&product=Komodo",
+    'bugs': "http://bugs.activestate.com/enter_bug.cgi?product=Komodo",
     'contactus': "http://www.activestate.com/company/contact-us"
 };
 
