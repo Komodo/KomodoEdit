a2d617f118d302259937213c813511943451722c
Todd Whiteman <toddw@activestate.com>
2013-09-26 19:12:13 +0000

    scintilla patches: don't need this cocoa specific patch anymore
    (integrated from komodo (trunk) change 82354 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@13759 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- contrib/patches/scintilla/__patchinfo__.py
+++ contrib/patches/scintilla/__patchinfo__.py
@@ -3,9 +3,6 @@
 import sys
 
 def patchfile_applicable(config, filepath):
-    if filepath.endswith("perf_no_abandon_paint.patch"):
-        # Only apply on Darwin - bug 98677.
-        return sys.platform == "darwin"
     return True
 
 def remove(config):
