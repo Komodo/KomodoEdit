07cb5bbaad3309c8998a49c2216dc55528fda4fe
Todd Whiteman <toddw@activestate.com>
2013-12-16 18:45:30 +0000

    benchmarks: add more and xpcom service for reporting timing information * only enabled in dev builds
    (integrated from komodo (trunk) change 83778 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@14205 b87d16e7-d29f-4ee1-b5ec-817d645d335f

---
+++ src/chrome/komodo/content/library/benchmark.js
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Benchmark APIs -- wraps around the XPCOM koIBenchmark service.
+ */
+const {Cc, Ci} = require("chrome");
+
+var koBenchmark = Cc["@activestate.com/koBenchmark;1"].getService(Ci.koIBenchmark);
+
+    /**
+     * Records the start time for the given event name.
+     */
+exports.startTiming = function(name) {
+    return koBenchmark.startTiming(name);
+}
+
+    /**
+     * Records the end time for the given event name. Must have a matching startTiming call.
+     */
+exports.endTiming = function(name) {
+    return koBenchmark.endTiming(name)
+}
+
+    /**
+     * Reports that the given event took the given time duration.
+     */
+exports.addTiming = function(name, duration) {
+    return koBenchmark.addTiming(name, duration);
+}
+
+    /**
+     * Reports an event occurred (at the current time).
+     */
+exports.addEvent = function(name) {
+    return koBenchmark.addEvent(name);
+}
+
+    /**
+     * Dump current benchmark results to stdout.
+     */
+exports.display = function() {
+    return koBenchmark.display();
+}
---
+++ src/python-sitelib/benchmark.py
@@ -0,0 +1,135 @@
+import time
+from collections import defaultdict
+
+_g_reporter = None
+
+class BenchReporter(object):
+    def __init__(self, time0=None, name=""):
+        self.time0 = time0 if time0 is not None else time.time()
+        self.name = name
+        self._depth = 0
+        self._starts = defaultdict(list)
+        # Entries are a list of (time_started, duration, depth) tuples.
+        self._entries = defaultdict(list)
+        self._missing_starts = defaultdict(list)
+        self._events = []
+        # Always report at shutdown time.
+        import atexit
+        atexit.register(self.display)
+
+    def startTiming(self, name):
+        self._starts[name].append((time.time(), self._depth))
+        self._depth += 1
+
+    def endTiming(self, name):
+        """End of a timing"""
+        now = time.time()
+        start = self._starts[name]
+        if not start:
+            self._missing_starts[name].append(now)
+        else:
+            start_time, depth = start.pop()
+            duration = now - start_time
+            self._entries[name].append((now - duration, duration, depth))
+        self._depth -= 1
+
+    def addTiming(self, name, duration):
+        self._entries[name].append((time.time() - duration, duration, self._depth))
+
+    def addEvent(self, name):
+        self._events.append((name, time.time()))
+
+    def displayEvents(self, limit=None):
+        if self._events:
+            print ""
+            print "Events:"
+            for name, t in self._events:
+                print "  %-73s at %0.5f" % (name, t - self.time0)
+
+    def display(self, order="by-time", limit=None):
+        """Display the reports.
+        
+        The `order` argument can be used to order the results in a particular
+        manner. Supported variations are:
+            "by-time": reports are displayed in chronological order
+            "by-spent": reports are displayed in order of the time spent
+
+        The `limit` argument is used to limit the number of reports displayed.
+        """
+        if order == "by-time":
+            print "Timings:"
+            entries_by_time = defaultdict(list)
+            for name, timings in self._entries.items():
+                for start, spent, depth in timings:
+                    entries_by_time[start].append((name, spent, depth))
+
+            for start in sorted(entries_by_time):
+                entries = entries_by_time[start]
+                for name, spent, depth in entries:
+                    print "  %-65s %0.5f at %0.5f" % (depth * " " + name, spent, start - self.time0)
+        else:
+            print "Unknown sort order %r" % (order, )
+
+        self.displayEvents()
+
+def initialise(t=None):
+    global _g_reporter
+    if _g_reporter is None:
+        _g_reporter = BenchReporter(t)
+    return True
+
+class bench(object):
+    """Decorator to benchmark a function call, records the time it takes to execute."""
+    def __init__(self, name=None):
+        self.name = name
+    def __call__(self, fn):
+        if self.name is None:
+            self.name = fn.__name__
+        this = self
+        def fn_wrap(*args, **kwargs):
+            if _g_reporter is None:
+                # Perform the regular call.
+                return fn(*args, **kwargs)
+            _g_reporter.startTiming(this.name)
+            try:
+                result = fn(*args, **kwargs)
+            finally:
+                _g_reporter.endTiming(this.name)
+            return result
+        return fn_wrap
+
+def startTiming(name):
+    if _g_reporter is None:
+        initialise()
+    _g_reporter.startTiming(name)
+
+def endTiming(name):
+    if _g_reporter is None:
+        initialise()
+    _g_reporter.endTiming(name)
+
+def addTiming(name, duration):
+    if _g_reporter is None:
+        initialise()
+    _g_reporter.addTiming(name, duration)
+
+def addEvent(name):
+    if _g_reporter is None:
+        initialise()
+    _g_reporter.addEvent(name)
+
+def display():
+    assert _g_reporter is not None
+    _g_reporter.display()
+
+import inspect
+def klass(cls, ismethod=inspect.ismethod):
+    """Wrap all class functions with benchmarked versions"""
+    clsname = cls.__name__
+    for name in dir(cls):
+        if name.startswith("_") and name != "__init__":
+            continue
+        item = getattr(cls, name)
+        if ismethod(item):
+            wrappeditem = bench("%s.%s" % (clsname, name))(item)
+            setattr(cls, name, wrappeditem)
