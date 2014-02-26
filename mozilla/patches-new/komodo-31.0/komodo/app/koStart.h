bd341fb13aabc7686b9742a4c60bfa166208a9ae
Todd Whiteman <toddw@activestate.com>
2014-02-26 22:37:57 +0000

    mozilla: patches: add mozilla 31 based patch set
    (integrated from komodo (trunk) change 84834 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@14329 b87d16e7-d29f-4ee1-b5ec-817d645d335f

---
+++ mozilla/patches-new/komodo-31.0/komodo/app/koStart.h
@@ -0,0 +1,24 @@
+/* Copyright (c) 2000-2005 ActiveState Corporation.
+ * See the file LICENSE.txt for licensing information.
+ *
+ * Startup support for an XRE-based Komodo.
+ */
+
+#ifndef __KO_START_H__
+#define __KO_START_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* This is a placeholder for the real koStart.* code (dropped in as part of
+ * the *Komodo* build).
+ */
+#define KOSTART_PLACEHOLDER 1
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !__KO_START_H__ */
+
