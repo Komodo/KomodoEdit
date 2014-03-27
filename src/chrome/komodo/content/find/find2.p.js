7d63375dd71315dce0811a6bb8115ef08ac1b13a
Todd Whiteman <toddw@activestate.com>
2014-03-27 19:09:48 +0000

    fix: Find: For replace, always populate the replace textbox with the last replace text https://bugs.activestate.com/show_bug.cgi?id=91177 rn=
    (integrated from komodo (trunk) change 85272 by toddw)
    
    git-svn-id: http://svn.openkomodo.com/repos/openkomodo/trunk@14363 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- src/chrome/komodo/content/find/find2.p.js
+++ src/chrome/komodo/content/find/find2.p.js
@@ -1019,6 +1019,17 @@ function _init() {
 
     // Setup the UI for the mode, as appropriate.
     var mode = args.mode || "find";
+    var verb;
+    if (mode.slice(0, 7) === "replace") {
+        verb = "replace";
+    } else {
+        verb = "find";
+    }
+
+    if (verb == "replace") {
+        // When in replace mode - restore the last replaced text.
+        widgets.repl.value = ko.mru.get("find-replacementMru", 0);
+    }
 
     // - Setup for there being a current project.
     var curr_proj = opener.ko.projects.manager.currentProject;
@@ -1063,12 +1074,6 @@ function _init() {
         "replaceinlastfiles": true
     };
     if (mode in in_files_modes) {
-        var verb;
-        if (mode.slice(0, 7) === "replace") {
-            verb = "replace";
-        } else {
-            verb = "find";
-        }
         var lastInFilesContext = _g_prefs.getStringPref("find-lastInFilesContext");
         var mode_context_from_pref = {
             // The 'mode' and 'find-lastInFilesContext' (same as <menulist>
