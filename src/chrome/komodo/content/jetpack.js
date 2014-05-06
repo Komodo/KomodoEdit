510e38cb3766c9914ff258df966db2d9e9ae4433
Todd Whiteman <toddw@activestate.com>
2014-05-06 10:04:14 -0700

    jetpack: re-implement the custom namespaces - using one resolver
    (integrated from trunk branch change a5b9a96 by Todd Whiteman <toddw@activestate.com>)

--- src/chrome/komodo/content/jetpack.js
+++ src/chrome/komodo/content/jetpack.js
@@ -14,6 +14,35 @@ const [JetPack, require] = (function() {
     const { main, Loader, resolve, resolveURI } =
         Cu.import('resource://gre/modules/commonjs/toolkit/loader.js', {}).Loader;
 
+    /* Populate requirePaths with category entries */
+    const catMan = Cc["@mozilla.org/categorymanager;1"]
+                        .getService(Ci.nsICategoryManager);
+    let requirePaths = {};
+    let setRequirePaths = function() {
+        // Resolving things to ko.* via the custom resolver...
+        requirePaths["x-komodo-internal://"] = "x-komodo-internal://";
+        // Komodo API fallback...
+        requirePaths["ko/"] = "chrome://komodo/content/library/";
+        // Default path
+        requirePaths[''] = 'resource://gre/modules/commonjs/';
+
+        let entries = catMan.enumerateCategory('require-path');
+        while (entries.hasMoreElements()) {
+            let entry = entries.getNext().QueryInterface(Ci.nsISupportsCString);
+            let uri = catMan.getCategoryEntry('require-path', entry);
+            if (entry && entry[entry.length-1] != "/") {
+                // Needs a trailing slash in order to map correctly.
+                entry += "/";
+            }
+            requirePaths[entry] = uri;
+        }
+    }
+    setRequirePaths();
+    /* Reload require paths on addon install */
+    Components.utils.import("resource://gre/modules/AddonManager.jsm");
+    AddonManager.addInstallListener({onInstallEnded: setRequirePaths});
+    // TODO: May need to reset "loader.modules" when the add-on is loaded?
+
     var loader;
     let my_resolve = function (id, requirer) {
         if (id.startsWith("ko/") && !(id in loader.modules)) {
@@ -48,17 +77,10 @@ const [JetPack, require] = (function() {
         // Being loaded via Components.utils.import
         this.EXPORTED_SYMBOLS = ["require", "JetPack"];
     }
+
     // Note that "paths" is required; _something_ needs to name the modules
-    const paths = {
-        // Resolving things to ko.* via the custom resolver...
-        "x-komodo-internal://": "x-komodo-internal://",
-        // Komodo API fallback...
-        "ko/": "chrome://komodo/content/library/",
-        // Default path
-        '': 'resource://gre/modules/commonjs/',
-    }
     loader = Loader({resolve: my_resolve,
-                     paths: paths,
+                     paths: requirePaths,
                      globals: globals});
 
     const JetPack = {
