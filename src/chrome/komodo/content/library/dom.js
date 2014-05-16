c29cbe5f7bc31e11fc6512aeb5db5dfcd648e13a
Nathan Rijksen <n.rijksen@gmail.com>
2014-05-16 12:23:43 -0400

    dom: fleshing out the dom API
    (integrated from trunk branch change e4b7a2a by Nathan Rijksen <n.rijksen@gmail.com>)

--- src/chrome/komodo/content/library/dom.js
+++ src/chrome/komodo/content/library/dom.js
@@ -3,22 +3,9 @@
 * https://github.com/dciccale/ki.js
 */
 if (typeof module === 'undefined') module = {}; // debugging helper
-module.exports = function(parent) {
+(function(parent) {
 
     /* === MAIN CONSTRUCTION LOGIC === */
-
-    /*
-     * queryObject function (internal use)
-     * query = selector, dom element or function
-     */
-    function queryObject(query) {
-        if (query && query.nodeType)
-            this._elements = [query];
-        else if ('' + query === query)
-            this._elements = parent.querySelectorAll(query);
-
-        this.length = this._elements.length;
-    }
 
     /*
      * $ main function
@@ -51,7 +38,8 @@ module.exports = function(parent) {
      *
      * @returns {Node}
      */
-    var createFlexElement = function(html) {
+    var createFlexElement = function(html)
+    {
         var tmp = document.createElement("div");
         tmp.innerHTML = html;
         return tmp.firstChild;
@@ -62,24 +50,36 @@ module.exports = function(parent) {
      *
      * @param   {Object} elems
      * @param   {string|object} insert
-     * @param   {boolean} preferPrepend
+     * @param   {object} opts {where: prepend|after|before}
      *
      * @returns {void} 
      */
-    var insertIntoElem = function(elems, insert, preferPrepend)
+    var insertIntoElem = function(elems, insert, opts)
     {
+        opts = opts || {};
+
         var __insert = false
         if (typeof insert == 'string')
             __insert = createFlexElement(insert);
 
-        return elems.each(function() {
+        return elems.each(function()
+        {
             if (insert.koDom)
                 var _insert = insert.first()
             else
                 var _insert = __insert ? __insert.cloneNode(true) : insert;
                 
-            if (preferPrepend && this.firstChild)
+            if (opts.where == "prepend" && this.firstChild)
                 this.insertBefore(_insert, this.firstChild);
+            else if (opts.where == "after")
+            {
+                if (this.nextSibling)
+                    this.parentNode.insertBefore(_insert, this.nextSibling);
+                else
+                    this.parentNode.appendChild(_insert);
+            }
+            else if (opts.where == "before")
+                this.parentNode.insertBefore(_insert, this);
             else
                 this.appendChild(_insert);
         });
@@ -87,10 +87,23 @@ module.exports = function(parent) {
 
     /* === FUNCTION CHAIN === */
 
+    /*
+     * queryObject function (internal use)
+     * query = selector, dom element or function
+     */
+    function queryObject(query)
+    {
+        this._elements = [];
+        
+        // Use push.apply to force array type
+        if (query && query.nodeType)
+            this._elements.push.apply(this._elements, [query]);
+        else if ('' + query === query)
+            this._elements.push.apply(this._elements,  parent.querySelectorAll(query));
+    }
+
     // set query object prototype
     queryObject.prototype = {
-
-        _elements: [],
 
         // default length
         length: 0,
@@ -104,10 +117,12 @@ module.exports = function(parent) {
          * action = function
          * return this
          */
-        on: function(event, action) {
-            return this.each(function(elem) {
-                elem.addEventListener(event, action);
-            })
+        on: function(event, action)
+        {
+            return this.each(function()
+            {
+                this.addEventListener(event, action);
+            });
         },
 
         /*
@@ -116,10 +131,12 @@ module.exports = function(parent) {
          * action = function
          * return this
          */
-        off: function(event, action) {
-            return this.each(function(elem) {
-                elem.removeEventListener(event, action);
-            })
+        off: function(event, action)
+        {
+            return this.each(function()
+            {
+                this.removeEventListener(event, action);
+            });
         },
 
         /*
@@ -140,8 +157,13 @@ module.exports = function(parent) {
          * Set text content of elem
          * @param   {string} value
          */
-        text: function(value) {
-            return this.each(function() {
+        text: function(value)
+        {
+            if (value === undefined)
+                return this.first().textContent;
+
+            return this.each(function()
+            {
                this.textContent = value;
             });
         },
@@ -150,18 +172,40 @@ module.exports = function(parent) {
          * Set html content of elem
          * @param   {string|object} value String or queryObject
          */
-        html: function(value) {
-            this.each(function() {
+        html: function(value)
+        {
+            if (value === undefined)
+                return this.first().innerHTML;
+
+            this.each(function()
+            {
                 this.innerHTML = "";
             });
             return this.append(value);
         },
 
         /**
+         * Get outer html of elem
+         */
+        outerHtml: function()
+        {
+            return this.first().outerHTML;
+        },
+
+        /**
+         * Empty the contents of an element
+         */
+        empty: function()
+        {
+            return this.text("");
+        },
+
+        /**
          * Append content to elem
          * @param   {string|object} value String or queryObject
          */
-        append: function(elem) {
+        append: function(elem)
+        {
             insertIntoElem(this, elem);
             return this;
         },
@@ -170,8 +214,105 @@ module.exports = function(parent) {
          * Prepend content to elem
          * @param   {string|object} value String or queryObject
          */
-        prepend: function(elem) {
-            insertIntoElem(this, elem, true /* prepend */);
+        prepend: function(elem)
+        {
+            insertIntoElem(this, elem, {where: "prepend"});
+        },
+
+        /**
+         * Insert content after element
+         * @param   {string|object} value String or queryObject
+         */
+        after: function(elem)
+        {
+            insertIntoElem(this, elem, {where: "after"});
+            return this;
+        },
+
+        /**
+         * Insert content before element
+         * @param   {string|object} value String or queryObject
+         */
+        before: function(elem)
+        {
+            insertIntoElem(this, elem, {where: "before"});
+            return this;
+        },
+
+        /**
+         * Set / get value
+         * @param   {String|Void} value
+         */
+        value: function(value)
+        {
+            // Todo: support different value types
+            var valueKey = 'value';
+
+            if (value === undefined)
+                return this.first()[valueKey];
+
+            return this.each(function()
+            {
+                this[valueKey] = value;
+            });
+        },
+
+        /**
+         * Delete element
+         */
+        delete: function()
+        {
+            return this.each(function()
+            {
+                this.parentNode.removeChild(this);
+            });
+        },
+
+        /**
+         * Makes element visible
+         */
+        show: function()
+        {
+            return this.each(function()
+            {
+                this.style.visibility = "visible";
+            });
+        },
+
+        /**
+         * Makes element invisible
+         */
+        hide: function()
+        {
+            return this.each(function()
+            {
+                this.style.visibility = "collapse";
+            });
+        },
+
+        addClass: function(className)
+        {
+            return this.each(function()
+            {
+                this.classList.add(className);
+            });
+        },
+
+        removeClass: function(className)
+        {
+            return this.each(function()
+            {
+                this.classList.remove(className);
+            });
+        },
+
+        /**
+         * Focus on element
+         */
+        focus: function()
+        {
+            this.first().focus();
+            return this;
         },
 
         // for some reason is needed to get an array-like
@@ -179,8 +320,13 @@ module.exports = function(parent) {
         splice: function() { return this._elements.splice.call(arguments); },
 
         // Use first entry
-        first: function() { return this._elements[0] }
+        first: function() { return this.element(0); },
+
+        last: function() { return this.element(-1); },
+
+        element:  function(k) { return this._elements.slice(k || 0)[0]; }
     }
 
-    return $;
-};
+    module.exports = $;
+    
+})(window.document);
