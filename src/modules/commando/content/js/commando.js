4a1e45ce11afd3da53065a0aa4d64ce7980f51be
Nathan Rijksen <n.rijksen@gmail.com>
2014-06-04 16:51:18 -0400

    Commando: Fleshed out the library further
    - DOM rendering optimization
    - using doT instead of mustache (faster)
    - Assign unique id's for search queries
    - Misc smaller improvements/additions
    
    (integrated from trunk branch change d5eb324 by Nathan Rijksen <n.rijksen@gmail.com>)

--- src/modules/commando/content/js/commando.js
+++ src/modules/commando/content/js/commando.js
@@ -2,15 +2,20 @@
     
     const {Cc, Ci}  = require("chrome");
     const $         = require("ko/dom");
-    const mustache  = require("contrib/mustache");
-    const log       = require("ko/logging").getLogger("commando")
+    const doT       = require("contrib/dot");
+    const log       = require("ko/logging").getLogger("commando");
+    const uuidGen   = require("sdk/util/uuid");
 
-    log.setLevel(require("ko/logging").LOG_DEBUG);
+    //log.setLevel(require("ko/logging").LOG_DEBUG);
 
     var local = {
         scopes: {},
         elemCache: {},
-        templateCache: {}
+        templateCache: {},
+        resultCache: [],
+        resultsReceived: 0,
+        resultsRendered: 0,
+        searchingUuid: null
     };
 
     var elems = {
@@ -31,7 +36,10 @@
     var init = function()
     {
         log.debug('Starting Commando');
-        elem('search').on("input", onSearch);
+        elem('search').on("keydown", onKeyNav);
+        elem('results').on("keydown", onKeyNav);
+        elem('results').on("select", onSelectResult);
+        elem('search').on("keyup", onSearch);
         elem('scope').on("change", function(e) { elem('search').focus(); });
     }
 
@@ -47,58 +55,99 @@
     {
         if ( ! (name in elems.template)) return undefined;
         if ( ! (name in local.templateCache))
-            local.templateCache[name] = elems.template[name]().html();
+            local.templateCache[name] = doT.template(elems.template[name]().html());
 
-        return $(mustache.render(local.templateCache[name], params));
+        return local.templateCache[name](params);
+    }
+
+    var onKeyNav = function(e)
+    {
+        log.debug("Event: onKeyNav");
+
+        var results = elem('results');
+        if ( ! results.visible()) return;
+        var resultCount = results.element().getRowCount();
+        var selIndex = results.element().selectedIndex;
+
+        switch (e.keyCode)
+        {
+            case 40: // down arrow
+            case 9:  // tab
+                log.debug("Navigate Down in Results");
+                e.preventDefault();
+
+                if (selIndex+1 == resultCount)
+                    results.element().selectedIndex = 0;
+                else
+                    results.element().selectedIndex++;
+                    
+                break;
+            case 38: // up arrow
+                log.debug("Navigate Up in Results");
+                e.preventDefault();
+
+                if (selIndex == 0)
+                    results.element().selectedIndex = resultCount-1;
+                else
+                    results.element().selectedIndex--;
+
+                break;
+        }
     }
 
     var onSearch = function(e, noDelay = false)
     {
+        log.debug("Event: onSearch");
+        if (local.prevSearchValue == e.target.value) return;
+
+        // Prevent multiple searches in a short amount of time
         if ( ! noDelay)
         {
-            if ("searchTimer" in local)
-                window.clearTimeout(local.searchTimer);
-
             log.debug("Delaying Search");
-            local.searchTimer = window.setTimeout(
+            window.setTimeout(
                 onSearch.bind(this, e, true),
-                ko.prefs.getLong("commando_search_delay", 100)
+                ko.prefs.getLong("commando_search_delay", 50)
             );
+            return;
         }
 
-        log.debug("Starting Search");
-        var scope = elem('scope').element().selectedItem._scope;
-        var handler = require(scope.handler);
+        local.searchingUuid = uuidGen.uuid();
+        local.resultCache = [];
+        local.resultsReceived = 0;
+        local.resultsRendered = 0;
+        local.prevSearchValue = e.target.value;
 
-        handler.onSearch(e.target.value, undefined, onResults);
+        // perform onSearch
+        log.debug(local.searchingUuid + " - Starting Search for: " + e.target.value);
+        getScopeHandler().onSearch(e.target.value, local.searchingUuid);
     }
 
-    var onResults = function(results)
+    var onSelectResult = function(e)
     {
-        var resultElem = elem('results');
-        resultElem.empty();
-        
-        for (let result of results)
-        {
-            resultElem.append(template('resultItem', result));
+        var selected = elem('results').element().selectedIem;
+        getScopeHandler().onSelectResult(selected);
+    }
 
-        }
+    var getScope = function()
+    {
+        return elem('scope').element().selectedItem._scope;
+    }
 
-        if (results.length > 0)
-            resultElem.addClass("has-results");
-        else
-            resultElem.removeClass("has-results");
+    var getScopeHandler = function()
+    {
+        var scope = getScope();
+        return require(scope.handler);
     }
 
     /* Public Methods */
-    
+
     this.showCommando = function()
     {
         log.debug("Showing Commando");
 
         var panel = elem('panel');
         var search = elem('search');
-        
+
         let left = window.innerWidth / 2;
         left -= panel.element().getBoundingClientRect().width / 2;
         panel.element().openPopup(undefined, undefined, left, 100);
@@ -114,22 +163,115 @@
         opts.id = id;
         local.scopes[id] = opts;
 
-        var scopeElem = template('scopeMenuItem', opts);
+        var scopeElem = $(template('scopeMenuItem', opts));
         scopeElem.element()._scope = local.scopes[id];
 
         elem('scopesSeparator').before(
             scopeElem
         );
+
+        if ( ! elem('scope').element().selectedItem.nodeName != 'menuitem')
+        {
+            elem('scope').element().selectedItem = scopeElem.element()
+        }
     }
 
     this.unregisterScope = function(id)
     {
         if ( ! (id in local.scopes)) return;
-        
+
         log.debug("Unregistering Scope: " + id);
 
         $("#scope-" + id).delete();
         delete local.scopes[id];
+    }
+
+    this.renderResult = function(result, searchUuid)
+    {
+        if (local.searchingUuid != searchUuid)
+        {
+            log.debug(searchUuid + " - Skipping result for old search uuid");
+            return;
+        }
+
+        local.resultsReceived++;
+        local.resultCache.push(result);
+
+        window.clearTimeout(local.renderResultsTimer);
+        local.renderResultsTimer = window.setTimeout(function()
+        {
+            this.renderResults(local.resultCache, searchUuid, true);
+            local.resultCache = [];
+        }.bind(this), ko.prefs.getLong("commando_result_render_delay", 10));
+    }
+
+    this.renderResults = function(results, searchUuid, cacheOrigin)
+    {
+        if (local.searchingUuid != searchUuid)
+        {
+            log.debug(searchUuid + " - Skipping "+results.length+" results for old search uuid: " + searchUuid);
+            return;
+        }
+
+        if ( ! cacheOrigin)
+            local.resultsReceived += results.length;
+
+        if (local.resultsRendered === 0)
+            this.empty(); // Force empty results
+
+        log.debug(searchUuid + " - Rendering Results");
+
+        var resultElem = elem('results');
+        var maxResults = ko.prefs.getLong("commando_search_max_results", 50);
+        maxResults -= local.resultsRendered;
+        results = results.slice(0, maxResults);
+        local.resultsRendered += results.length;
+
+        var resultElem = elem('results');
+        for (let result of results)
+        {
+            result.subscope = local.subscope;
+            resultEntry = $.createElement(template('resultItem', result));
+            resultEntry.resultData = result;
+            resultElem.element().appendChild(resultEntry);
+        }
+
+        resultElem.addClass("has-results");
+        resultElem.css("maxHeight", (window.screen.availHeight / 2) + "px");
+    }
+
+    this.onSearchComplete = function(scope, searchUuid)
+    {
+        if (local.searchingUuid != searchUuid) return;
+
+        if (local.resultsReceived == 0)
+        {
+            this.renderResult({
+                id: "",
+                name: "No Results",
+                classList: "no-result-msg non-interact"
+            }, searchUuid);
+        }
+    }
+
+    this.getSubscope = function()
+    {
+        return local.subscope;
+    }
+
+    this.setSubscope = function(subscope)
+    {
+        local.subscope = subscope;
+    }
+
+    this.empty = function()
+    {
+        log.debug("Emptying Results");
+
+        var resultElem = elem('results');
+        resultElem.empty();
+        resultElem.removeClass("has-results");
+        local.resultsRendered = 0;
     }
 
     init();
