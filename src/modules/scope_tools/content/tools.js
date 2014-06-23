ad377336699805b8c72c9f663321766adb0ce91d
Nathan Rijksen <n.rijksen@gmail.com>
2014-06-23 15:26:10 -0400

    Commando: Added commands to tools scope
    (integrated from trunk branch change f5fb566 by Nathan Rijksen <n.rijksen@gmail.com>)

--- src/modules/scope_tools/content/tools.js
+++ src/modules/scope_tools/content/tools.js
@@ -7,12 +7,12 @@
     const tbSvc     = Cc["@activestate.com/koToolbox2Service;1"]
                         .getService(Ci.koIToolbox2Service);
 
+    var local = {};
+
     //log.setLevel(require("ko/logging").LOG_DEBUG);
 
-    this.onSearch = function(query, uuid)
+    var searchTools = function(query, uuid)
     {
-        log.debug(uuid + " - Starting Scoped Search");
-
         // Get ordered list of best language scope (a *list* for
         // multi-language files).
         var currView = ko.views.manager.currentView;
@@ -32,13 +32,14 @@
             });
         }
 
-        var resultArr = [];
+        var tools = [];
         var results = tbSvc.findTools(query, langs.length, langs, {});
         for (let k in results)
         {
             let result = results[k];
 
-            resultArr.push({
+            tools.push({
+                classList: "compact",
                 id: result.path_id,
                 name: result.name,
                 description: result.type + " - " + result.subDir,
@@ -50,7 +51,68 @@
             });
         }
 
-        commando.renderResults(resultArr, uuid);
+        commando.renderResults(tools, uuid);
+    }
+
+    var searchCommands = function(query, uuid)
+    {
+        if (!local.commands)
+        {
+            local.commands = [];
+            var desc;
+            var commandname;
+
+            if (!ko.keybindings.manager.commanditems)
+            {
+                // Load the keybinding description information.
+                ko.keybindings.manager.parseGlobalData();
+            }
+
+            var commanditems = ko.keybindings.manager.commanditems;
+            for (var i=0; i < commanditems.length; i++) {
+                commandname = commanditems[i].name;
+                desc = commanditems[i].desc;
+                if (!commandname || !desc) {
+                    continue;
+                }
+                local.commands.push({
+                    classList: "compact",
+                    id: commandname,
+                    name: desc,
+                    description: commandname,
+                    icon: "chrome://fugue/skin/icons/external.png"
+                });
+            }
+        }
+
+        var commands = local.commands.slice(); // clone
+        query = query.toLowerCase();
+        var words = query.split(" ");
+        
+        // Filter out empty word entries.
+        words = words.filter(function(w) !!w);
+        if (words)
+        {
+            commands = commands.filter(function(command)
+            {
+                var text = (command.id + command.name).toLowerCase();
+                if (words.every(function(w) text.indexOf(w) != -1))
+                {
+                    return true;
+                }
+                return false;
+            });
+        }
+
+        commando.renderResults(commands, uuid);
+    }
+
+    this.onSearch = function(query, uuid)
+    {
+        log.debug(uuid + " - Starting Scoped Search");
+
+        searchTools(query, uuid);
+        searchCommands(query, uuid);
     }
 
     this.onSelectResult = function(selectedItems)
