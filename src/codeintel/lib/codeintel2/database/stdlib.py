2e33c6aab5825080f1229ca1b05517450bd82511
Todd Whiteman <toddw@activestate.com>
2014-06-24 09:49:08 -0700

    codeintel: memory reporting: fix signature of reportMemory call
    (integrated from trunk branch change 525b745 by Todd Whiteman <toddw@activestate.com>)

--- src/codeintel/lib/codeintel2/database/stdlib.py
+++ src/codeintel/lib/codeintel2/database/stdlib.py
@@ -247,7 +247,7 @@ class StdLib(object):
         return cplns
         
 
-    def reportMemory(self, reporter, closure=None):
+    def reportMemory(self):
         """
         Report on memory usage from this StdLib.
         @returns {dict} memory usage; keys are the paths, values are a dict of
