f281dea786b057878573bf42bd14da63ebb62eb5
Nathan Rijksen <n.rijksen@gmail.com>
2014-07-02 14:24:33 -0400

    Commando: Split tools and commands into seperate scopes
    (integrated from trunk branch change 7271024 by Nathan Rijksen <n.rijksen@gmail.com>)

--- src/modules/scope_tools/content/tools.js
+++ src/modules/scope_tools/content/tools.js
@@ -11,9 +11,15 @@
 
     //log.setLevel(require("ko/logging").LOG_DEBUG);
 
-    var searchTools = function(query, uuid)
+    this.onShow = function()
     {
-        log.debug(uuid + " - Searching Tools");
+        commando.search("");
+    }
+
+    this.onSearch = function(query, uuid)
+    {
+        log.debug(uuid + " - Starting Scoped Search");
+
         // Get ordered list of best language scope (a *list* for
         // multi-language files).
         var currView = ko.views.manager.currentView;
@@ -48,7 +54,6 @@
 
                 scope: "scope-tools",
                 data: {
-                    type: "tool",
                     tool: result
                 }
             });
@@ -57,94 +62,20 @@
         commando.renderResults(tools, uuid);
     }
 
-    var searchCommands = function(query, uuid)
-    {
-        log.debug(uuid + " - Searching Commands");
-
-        if (!local.commands)
-        {
-            local.commands = [];
-            var desc;
-            var commandname;
-
-            if (!ko.keybindings.manager.commanditems)
-            {
-                // Load the keybinding description information.
-                ko.keybindings.manager.parseGlobalData();
-            }
-
-            var commanditems = ko.keybindings.manager.commanditems;
-            for (var i=0; i < commanditems.length; i++)
-            {
-                commandname = commanditems[i].name;
-                desc = commanditems[i].desc;
-                if (!commandname || !desc) continue;
-                local.commands.push({
-                    classList: "compact",
-                    id: commandname,
-                    name: desc,
-                    description: commandname,
-                    icon: "chrome://fugue/skin/icons/external.png",
-                    scope: "scope-tools",
-                    data: {
-                        type: "command"
-                    }
-                });
-            }
-        }
-
-        var commands = local.commands.slice(); // clone
-        query = query.toLowerCase();
-        var words = query.split(" ");
-        
-        // Filter out empty word entries.
-        words = words.filter(function(w) !!w);
-        if (words)
-        {
-            commands = commands.filter(function(command)
-            {
-                var text = (command.id + command.name).toLowerCase();
-                if (words.every(function(w) text.indexOf(w) != -1))
-                {
-                    return true;
-                }
-                return false;
-            });
-        }
-
-        commando.renderResults(commands, uuid);
-    }
-
-    this.onSearch = function(query, uuid)
-    {
-        log.debug(uuid + " - Starting Scoped Search");
-
-        searchTools(query, uuid);
-        searchCommands(query, uuid);
-    }
-
     this.onSelectResult = function(selectedItems)
     {
         log.debug("Invoking Tools");
 
-        var uris = []
+        var uris = [];
         for (let item in selectedItems)
         {
             let resultData = selectedItems[item].resultData;
             try {
-                if (resultData.data.type == "command")
-                {
-                    log.debug("Invoking command: " + resultData.id);
-                    ko.commands.doCommandAsync(resultData.id);
-                }
-                else
-                {
-                    // Brute force async
-                    setTimeout(function() {
-                        log.debug("Invoking tool: " + resultData.id);
-                        ko.toolbox2.invokeTool(resultData.data.koTool);
-                    }, 0)
-                }
+                // Brute force async
+                setTimeout(function() {
+                    log.debug("Invoking tool: " + resultData.id);
+                    ko.toolbox2.invokeTool(resultData.data.koTool);
+                }, 0)
             } catch(e) {
                 log.exception(e, "Failed to invoke tool: ");
             }
