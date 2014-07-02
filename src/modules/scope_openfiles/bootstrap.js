7294681c2dd5505930ecc6def452d1bd0ca16473
Nathan Rijksen <n.rijksen@gmail.com>
2014-07-02 15:33:20 -0400

    Commando: Added Open Files scope
    (integrated from trunk branch change fdbe47f by Nathan Rijksen <n.rijksen@gmail.com>)

---
+++ src/modules/scope_openfiles/bootstrap.js
@@ -0,0 +1,73 @@
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+Cu.import('resource://gre/modules/Services.jsm');
+
+var startupData;
+
+function loadIntoWindow(window) {
+    try {
+        window.require.setRequirePath("scope-openfiles/", "chrome://scope-openfiles/content/");
+        var commando = window.require("commando/commando");
+        commando.registerScope("scope-openfiles", {
+            name: "Open Files",
+            icon: "chrome://icomoon/skin/icons/files.png",
+            handler: "scope-openfiles/openfiles"
+        });
+    } catch (e) {
+        Cu.reportError("Commando: Exception while registering scope 'openfiles'");
+        Cu.reportError(e);
+    }
+}
+
+function unloadFromWindow(window) {
+    if (!window) return;
+    var commando = window.require("commando/commando");
+    commando.unregisterScope("project-commands");
+}
+
+var windowListener = {
+    onOpenWindow: function(aWindow) {
+        // Wait for the window to finish loading
+        let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+        domWindow.addEventListener("komodo-post-startup", function onLoad() {
+            domWindow.removeEventListener("komodo-post-startup", onLoad, false);
+            loadIntoWindow(domWindow);
+        }, false);
+    },
+
+    onCloseWindow: function(aWindow) {},
+    onWindowTitleChange: function(aWindow, aTitle) {}
+};
+
+function startup(data, reason) {
+    startupData = data;
+
+    // Load into any existing windows
+    let windows = Services.wm.getEnumerator("Komodo");
+    while (windows.hasMoreElements()) {
+        let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+        loadIntoWindow(domWindow);
+    }
+
+    // Load into any new windows
+    Services.wm.addListener(windowListener);
+}
+
+function shutdown(data, reason) {
+    // When the application is shutting down we normally don't have to clean
+    // up any UI changes made
+    if (reason == APP_SHUTDOWN) return;
+
+    // Stop listening for new windows
+    Services.wm.removeListener(windowListener);
+
+    // Unload from any existing windows
+    let windows = Services.wm.getEnumerator("Komodo");
+    while (windows.hasMoreElements()) {
+        let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+        unloadFromWindow(domWindow);
+    }
+}
+
+function install(data, reason) {}
+
+function uninstall(data, reason) {}
---
+++ src/modules/scope_openfiles/content/openfiles.js
@@ -0,0 +1,73 @@
+(function() {
+    const log       = require("ko/logging").getLogger("commando-scope-commands")
+    const commando  = require("commando/commando");
+    const {Cc, Ci}  = require("chrome");
+
+    const partSvc   = Cc["@activestate.com/koPartService;1"].getService(Ci.koIPartService);
+    const ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+
+    log.setLevel(require("ko/logging").LOG_DEBUG);
+
+    this.onShow = function()
+    {
+        commando.search("");
+    }
+
+    this.onSearch = function(query, uuid)
+    {
+        log.debug(uuid + " - Starting Scoped Search");
+
+        var curProject = partSvc.currentProject;
+
+        if (curProject)
+            var curPath = curProject.liveDirectory;
+        else
+            var curPath = ioService.newURI(ko.places.getDirectory(), null, null).path;
+
+        var editorViews = ko.views.manager.getAllViews();
+        for (let editorView of editorViews)
+        {
+            var fullPath = editorView.koDoc.file ? editorView.koDoc.file.path : '';
+            var path = fullPath.indexOf(curPath) === 0 ? fullPath.substr(curPath.length) : fullPath;
+
+            if (path.toLowerCase().indexOf(query.toLowerCase()) == -1 &&
+                editorView.title.toLowerCase().indexOf(query.toLowerCase()) == -1)
+                continue;
+
+            // Todo: Normalize weight?
+            var weight = editorView.koDoc.fileAccessNo;
+            weight += editorView.koDoc.fileLastAccessed;
+
+            var words = query.split(/\s+/);
+            for (let word of words)
+            {
+                if (path.indexOf(word))
+                    weight += 1000;
+            }
+
+            commando.renderResult({
+                id: editorView.uid.number,
+                name: editorView.title,
+                description: path, // todo: highlight matched portions
+                icon: "moz-icon://" + path + "?size=32",
+                weight: weight,
+                scope: "scope-openfiles",
+                data: {
+                    fullPath: fullPath
+                }
+            }, uuid);
+        }
+    }
+
+    this.sort = function(current, previous)
+    {
+        if ( ! current || ! previous) return 0;
+        return previous.name.localeCompare(current.name) > 0 ? 1 : -1;
+    }
+
+    this.onSelectResult = function(selectedItems)
+    {
+
+    }
+
+}).apply(module.exports);
