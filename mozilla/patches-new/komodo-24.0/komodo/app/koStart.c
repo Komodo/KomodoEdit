cee719c056a75c3aec93b180c7152430b3691370
Todd Whiteman <toddw@activestate.com>
2014-07-23 15:42:20 -0700

    build: mozilla: remove old patch files

--- mozilla/patches-new/komodo-24.0/komodo/app/koStart.c
+++
@@ -1,9 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
-/* This is a placeholder for the real koStart.* code (dropped in as part of
- * the *Komodo* build).
- */
-
