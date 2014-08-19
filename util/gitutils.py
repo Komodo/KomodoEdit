8c3bb376c8d478ab1de52f972780f3a702d9795e
Todd Whiteman <toddw@activestate.com>
2014-08-19 12:32:52 -0700

    util: gitutils: link needs to be KomodoEdit repo

--- util/gitutils.py
+++ util/gitutils.py
@@ -53,4 +53,4 @@ if __name__ == '__main__':
     else:
         gitrev = revision_from_buildnum(revision)
         print("git rev: %s" % (gitrev, ))
-        print("https://github.com/Komodo/KomodoIDE/commit/%s" % (gitrev, ))
+        print("https://github.com/Komodo/KomodoEdit/commit/%s" % (gitrev, ))
