206fc2c2fca5e88fad1330fdcebdd1ff826c1592
Nathan Rijksen <n.rijksen@gmail.com>
2014-09-26 16:54:26 -0400

    Features: Added kopy.io integration
    You can now post your code on kopy.io right from the editor context menu
    rn=
    
    (integrated from trunk branch change d02abc2 by Nathan Rijksen <n.rijksen@gmail.com>)

---
+++ src/modules/kopy/bootstrap.js
@@ -0,0 +1,69 @@
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+Cu.import('resource://gre/modules/Services.jsm');
+
+var startupData;
+
+function loadIntoWindow(window) {
+    try {
+        window.require.setRequirePath("kopy/", "chrome://kopy/content/");
+        window.require('kopy/kopy').load();
+    } catch (e) {
+        Cu.reportError("kopy: Exception while initializing");
+        Cu.reportError(e);
+    }
+}
+
+function unloadFromWindow(window) {
+    if (!window) return;
+    window.require('kopy/kopy').unload();
+}
+
+/* Boilerplate below */
+
+var windowListener = {
+    onOpenWindow: function(aWindow) {
+        // Wait for the window to finish loading
+        let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+        domWindow.addEventListener("komodo-post-startup", function onLoad() {
+            domWindow.removeEventListener("komodo-post-startup", onLoad, false);
+            loadIntoWindow(domWindow);
+        }, false);
+    },
+
+    onCloseWindow: function(aWindow) {},
+    onWindowTitleChange: function(aWindow, aTitle) {}
+};
+
+function startup(data, reason) {
+    startupData = data;
+
+    // Load into any existing windows
+    let windows = Services.wm.getEnumerator("Komodo");
+    while (windows.hasMoreElements()) {
+        let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+        loadIntoWindow(domWindow);
+    }
+
+    // Load into any new windows
+    Services.wm.addListener(windowListener);
+}
+
+function shutdown(data, reason) {
+    // When the application is shutting down we normally don't have to clean
+    // up any UI changes made
+    if (reason == APP_SHUTDOWN) return;
+
+    // Stop listening for new windows
+    Services.wm.removeListener(windowListener);
+
+    // Unload from any existing windows
+    let windows = Services.wm.getEnumerator("Komodo");
+    while (windows.hasMoreElements()) {
+        let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+        unloadFromWindow(domWindow);
+    }
+}
+
+function install(data, reason) {}
+
+function uninstall(data, reason) {}
---
+++ src/modules/kopy/content/kopy.js
@@ -0,0 +1,132 @@
+(function() {
+    const log       = require("ko/logging").getLogger("kopy");
+    const {Cc, Ci}  = require("chrome");
+    const prefs     = ko.prefs;
+    const menu      = require("ko/menu");
+    const commands  = require("ko/commands");
+    const editor    = require("ko/editor");
+
+    var context = [
+        {
+            select: menu.context.editorContext,
+            after: "#editor-context-makeSnippet"
+        }
+    ];
+
+    this.load = function()
+    {
+        commands.register("kopy", this.share.bind(this, undefined), {
+            label: "kopy: Share Code via kopy.io"
+        });
+
+        menu.register({
+            id: "kopy",
+            label: "Share Code via kopy.io",
+            image: "chrome://kopy/skin/icon.png",
+            command: ko.commands.doCommandAsync.bind(ko.commands, "cmd_kopy"),
+            context: context
+        });
+    }
+
+    this.unload = function()
+    {
+        menu.unregister("kopy", context);
+    }
+
+    this.share = function(data, language)
+    {
+        var locale;
+        var useClipboard = prefs.getBoolean("kopy_copy_to_clipboard", true);
+        var showInBrowser = prefs.getBoolean("kopy_show_in_browser", true);
+
+        if ( ! useClipboard && ! showInBrowser)
+        {
+            locale = "Could not share code via kopy.io; both clipboard and browser settings are disabled";
+            ko.statusBar.AddMessage(locale, 5000, true);
+            return;
+        }
+
+        locale = "You are about to share your current code selection with others, \
+                     this means anyone with access to the URL can view your code.\
+                     Are you sure you want to do this?";
+        if ( ! require("ko/dialogs").confirm(locale, {doNotAskPref: "kopy_donotask"}))
+        {
+            log.debug("kopy cancelled by confirmation");
+            return;
+        }
+
+        if ( ! data)
+        {
+            if ( ! editor.available())
+            {
+                locale = "Please enter the code that you would like to share"; // todo: localize
+                data = require("ko/dialogs").prompt(locale, {});
+
+                if ( ! data) return;
+            }
+            else
+            {
+                data = editor.getSelection();
+                if (data == "")
+                {
+                    data = editor.getValue();
+                }
+                language = editor.getLanguage();
+            }
+        }
+
+        if ( ! language)
+        {
+            language = 'null';
+        }
+
+        var params = require("sdk/querystring").stringify(
+        {
+            data: data,
+            language: language,
+            scheme: prefs.getStringPref("editor-scheme").replace(/\-(dark|light)$/, '.$1')
+        });
+
+        var baseUrl = ko.prefs.getString("kopy_baseurl", "http://kopy.io");
+        var httpReq = new window.XMLHttpRequest({mozSystem: true});
+        httpReq.open("post", baseUrl + '/documents', true);
+        httpReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        httpReq.setRequestHeader("Content-length", params.length);
+        httpReq.setRequestHeader("Connection", "close");
+        httpReq.send(params);
+
+        httpReq.onload = function ()
+        {
+            try
+            {
+                var key = JSON.parse(this.responseText).key;
+            }
+            catch (e)
+            {
+                var errorMsg = "kopy.io: Code sharing failed, malformed response";
+                log.warn(errorMsg + ": " + this.responseText);
+                ko.statusBar.AddMessage(errorMsg, "kopy", 5000, true);
+            }
+
+            var url = baseUrl + '/' + key;
+            if (useClipboard)
+            {
+                require("sdk/clipboard").set(url);
+                ko.statusBar.AddMessage("URL copied to keyboard: " + url, "kopy", 5000, true);
+            }
+
+            if (showInBrowser)
+            {
+                ko.browse.openUrlInDefaultBrowser(url);
+            }
+        };
+
+        httpReq.onerror = function(e)
+        {
+            var errorMsg = "kopy.io: HTTP Request Failed: " + e.target.status;
+            log.warn(errorMsg);
+            ko.statusBar.AddMessage(errorMsg, "kopy", 5000, true);
+        }
+    }
+
+}).apply(module.exports);
