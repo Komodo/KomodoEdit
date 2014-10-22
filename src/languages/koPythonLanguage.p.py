6861d0d76f835b34bd0fd141620184f0da960762
Todd Whiteman <toddw@activestate.com>
2014-10-22 12:57:20 -0700

    python: comments: allow "#" as a comment delimiter (for backward compat)
    (integrated from master branch change 1864ec9 by Todd Whiteman <toddw@activestate.com>)

--- src/languages/koPythonLanguage.p.py
+++ src/languages/koPythonLanguage.p.py
@@ -130,7 +130,7 @@ class KoPythonCommonLanguage(KoLanguageBase):
     namedBlockDescription = 'Python functions and classes'
     # XXX read url from some config file
     downloadURL = 'http://www.activestate.com/activepython'
-    commentDelimiterInfo = { "line": [ "# " ]  }
+    commentDelimiterInfo = { "line": [ "# ", "#" ]  }
     _indent_open_chars = ':{[('
     _lineup_open_chars = "([{" 
     _lineup_close_chars = ")]}"
