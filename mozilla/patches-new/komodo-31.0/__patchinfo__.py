c17a9206fb9571ba29d3a3d0eab88afeda09f97d
Todd Whiteman <toddw@activestate.com>
2014-11-19 15:46:07 -0800

    build: mozilla: drop mozApp check
    (integrated from master branch change 67a8b56 by Todd Whiteman <toddw@activestate.com>)

--- mozilla/patches-new/komodo-31.0/__patchinfo__.py
+++ mozilla/patches-new/komodo-31.0/__patchinfo__.py
@@ -2,8 +2,7 @@
 """Apply these patches if the target mozApp is Komodo."""
 
 def applicable(config):
-    return config.mozApp == "komodo" and \
-           config.patch_target == "komodoapp" and \
+    return config.patch_target == "komodoapp" and \
            config.mozVer == 31.0
 
 
