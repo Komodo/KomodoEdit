7bfc30a1800e8f9897e91eec29c6cf183ecfb8f3
Todd Whiteman <toddw@activestate.com>
2014-11-26 17:02:30 -0800

    build: komodo app: support for mozilla 35

--- src/main/xre/app/application.p.ini.h
+++ src/main/xre/app/application.p.ini.h
@@ -4,6 +4,9 @@
                  NULL,                  // app directory
                  "ActiveState",         // vendor
                  "PP_KO_PROD_NAME",     // app name
+// #if MOZILLA_VERSION_MAJOR > 31
+                 NULL,                  // remote app name (NULL means same as app name)
+// #endif
                  "PP_KO_VERSION",       // app version
                  "PP_KO_BUILD_NUMBER",  // buildID
                  "{b1042fb5-9e9c-11db-b107-000d935d3368}", // app guid
@@ -14,4 +17,8 @@
                  "PP_MOZ_GRE_MILESTONE", // XRE maxVersion
                  "https://komodo.activestate.com/crash/submit", // crash report
                  NULL                   // profile directory
+// #if MOZILLA_VERSION_MAJOR > 31
+                 ,
+                 NULL                   // user agent string
+// #endif
              };
