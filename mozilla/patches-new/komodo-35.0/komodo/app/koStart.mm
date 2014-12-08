249a693668e11a3382cec1791785b669742a0d64
Todd Whiteman <toddw@activestate.com>
2014-12-08 14:51:05 -0800

    build: mozilla: update komodo files on mozilla 35
    (integrated from master branch change 0bdfe87 by Todd Whiteman <toddw@activestate.com>)

---
+++ mozilla/patches-new/komodo-35.0/komodo/app/koStart.mm
@@ -0,0 +1,10 @@
+/* Copyright (c) 2000-2005 ActiveState Corporation.
+ * See the file LICENSE.txt for licensing information.
+ *
+ * Startup support for an XRE-based Komodo.
+ */
+
+/* This is a placeholder for the real koStart.* code (dropped in as part of
+ * the *Komodo* build).
+ */
+
