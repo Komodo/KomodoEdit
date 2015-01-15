19dc54e5f9dbdda930c4d65510d4299145a47f74
Nathan Rijksen <n.rijksen@gmail.com>
2015-01-15 13:43:58 -0500

    Icons: Added yosemite iconset
    (integrated from master branch change a0afcda by Nathan Rijksen <n.rijksen@gmail.com>)

---
+++ src/modules/iconset_yosemite/content/manifest/init.js
@@ -0,0 +1,7 @@
+if (koSkin.shouldFlushCaches)
+{
+    Services.prefs.setCharPref("general.skins.selectedSkin", "yosemite");
+    ko.prefs.setString("iconset-base-color", "#7B7B7B");
+    ko.prefs.setString("iconset-toolbar-color", "#595959");
+    ko.prefs.setString("iconset-selected-color", "#FFFFFF");
+}
