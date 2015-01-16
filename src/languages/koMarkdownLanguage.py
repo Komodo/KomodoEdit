ff5616ebc4c700ff1abf56c5c23a4d9528d4c57b
Todd Whiteman <toddw@activestate.com>
2015-01-16 14:08:36 -0800

    markdown language: add sample text
    (integrated from master branch change 4442f28 by Todd Whiteman <toddw@activestate.com>)

--- src/languages/koMarkdownLanguage.py
+++ src/languages/koMarkdownLanguage.py
@@ -80,7 +80,37 @@ class koMarkdownLanguage(KoLanguageBase):
     defaultExtension = '.md'
     commentDelimiterInfo = {"line": [ ";" ]}
     
-    sample = """No sample is available."""
+    sample = """
+Heading
+=======
+
+Sub-heading
+-----------
+
+### Another deeper heading
+
+Paragraphs are separated by a blank line.
+
+Let 2 spaces at the end of a line to do a line break
+
+Text attributes *italic*, **bold**, `monospace`, ~~strikethrough~~ .
+
+A [link](http://example.com). <<<   No space between ] and (  >>>
+
+Shopping list:
+
+  * apples
+  * oranges
+  * pears
+
+Numbered list:
+
+  1. apples
+  2. oranges
+  3. pears
+
+The rain---not the reign---in Spain.
+"""
     
     def get_lexer(self):
         if self._lexer is None:
