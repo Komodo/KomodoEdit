64d6d82475f2b9421fb819279aa805be180e3d7b
Todd Whiteman <toddw@activestate.com>
2015-01-20 17:18:38 -0800

    mozilla: build: add Centos ldflags patch
    (integrated from master branch change 57711ba by Todd Whiteman <toddw@activestate.com>)

--- mozilla/patches-new/mozilla-35.0/gtk/__patchinfo__.py
+++ mozilla/patches-new/mozilla-35.0/gtk/__patchinfo__.py
@@ -1,12 +1,22 @@
 
 """Apply these patches to the Mozilla Mercurial checkout."""
 
+from os.path import basename, exists
 import sys
 
 def applicable(config):
     return config.mozVer == 35.0 and \
            config.patch_target == "mozilla" and \
            sys.platform.startswith('linux')
+
+def patchfile_applicable(config, filepath):
+    if basename(filepath) == "centos_buildflags.patch":
+        if exists("/etc/redhat-release"):
+            contents = file("/etc/redhat-release").read()
+            if "CentOS release 6." in contents:
+                return True
+        return False
+    return True
 
 def patch_args(config):
     # use -p1 to better match hg patches
