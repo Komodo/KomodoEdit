817849359926f3657e7ebe1981de68c15855ebee
Nathan Rijksen <n.rijksen@gmail.com>
2015-03-03 12:27:10 -0500

    fix: Iconset: Cupertino iconset config was inconsistent with default prefs
    (integrated from master branch change 5c91589 by Nathan Rijksen <n.rijksen@gmail.com>)

--- src/modules/iconset_cupertino/content/manifest/init.js
+++ src/modules/iconset_cupertino/content/manifest/init.js
@@ -1,6 +1,7 @@
 if (koSkin.shouldFlushCaches)
 {
     Services.prefs.setCharPref("general.skins.selectedSkin", "cupertino");
-    ko.prefs.setString("iconset-base-defs", "cupertino");
-    ko.prefs.setString("iconset-selected-defs", "cupertino-blue");
+    ko.prefs.setString("iconset-base-color", "#7B7B7B");
+    ko.prefs.setString("iconset-toolbar-color", "#595959");
+    ko.prefs.setString("iconset-selected-color", "#FFFFFF");
 }
