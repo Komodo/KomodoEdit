3cd6312a5b8eecf5a2bb3b48755b904035af126a
Nathan Rijksen <n.rijksen@gmail.com>
2015-04-30 12:42:42 -0400

    SDK: Added a simple Ajax library
    (integrated from master branch change 90251dc by Nathan Rijksen <n.rijksen@gmail.com>)

---
+++ src/chrome/komodo/content/sdk/ajax.js
@@ -0,0 +1,68 @@
+(function() {
+    
+    const log       = require("ko/logging").getLogger("sdk-ajax");
+    const {Cc, Ci}  = require("chrome");
+    
+    this.get = function(path, callback)
+    {
+        return this.request({url: path, method: 'GET'}, callback);
+    }
+    
+    this.post = function(path, callback)
+    {
+        return this.request({url: path, method: 'POST'}, callback);
+    }
+
+    this.request = function(params, callback)
+    {
+        if (typeof params == 'string') params = { url: params };
+        
+        var headers = params.headers || {};
+        var body = params.body;
+
+        // Todo: use common js module - the one in our current moz version is broken
+        // var req = new require("sdk/net/xhr").XMLHttpRequest;
+        var req = new _window().XMLHttpRequest({mozSystem: true});
+        req.withCredentials = params.withCredentials || false;
+
+        req.onreadystatechange = function() {
+            if (req.readyState == 4)
+                callback(req.status, req.responseText, req);
+        }
+
+        var defaultHeaders = {
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        for (let k in defaultHeaders)
+            headers = headers[k] || defaultHeaders[k];
+
+        req.open(params.method || (body ? 'POST' : 'GET'), params.url, true);
+
+        for (var field in headers)
+            req.setRequestHeader(field, headers[field]);
+
+        req.send(body);
+    }
+    
+    var _window = function()
+    {
+        var w;
+        try
+        {
+            w = winUtils.getToplevelWindow(window);
+        }
+        catch (e)
+        {
+            log.debug("getTopLevelWindow failed, falling back on window mediator");
+            
+            var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+            let windows = wm.getEnumerator("Komodo");
+            while (windows.hasMoreElements())
+                w = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+        }
+        
+        return w;
+    }
+    
+}).apply(module.exports);
