9b555d3c4fea44124fdbee1068082b555bf96cfe
Nathan Rijksen <n.rijksen@gmail.com>
2015-05-12 11:46:20 -0400

    SDK: Shell: Added getEnv and lookup methods
    (integrated from master branch change 1ea05e7 by Nathan Rijksen <n.rijksen@gmail.com>)

--- src/chrome/komodo/content/sdk/shell.js
+++ src/chrome/komodo/content/sdk/shell.js
@@ -17,22 +17,73 @@
         return cwd;
     }
     
-    this.run = function(binary, args, opts)
-    {
-        // Create environment variables
+    this.getEnv = function()
+    {   
         var env = {};
         var koEnviron = Cc["@activestate.com/koUserEnviron;1"].getService();
-        var keys = koEnviron.keys({}, {});
+        var keys = koEnviron.keys();
         for (let key of keys)
             env[key] = koEnviron.get(key);
+        
+        return env;
+    }
+    
+    this.lookup = function(command, env)
+    {
+        var ioFile = require("sdk/io/file");
+        
+        var isExecutable = function(str)
+        {
+            try
+            {
+                let file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+                file.initWithPath(str);
+                
+                if (file.isFile() && file.isExecutable())
+                    return true;
+            }
+            catch (e) {}
             
+            return false;
+        }
+        
+        if (isExecutable(command))
+            return command;
+        
+        env = env || this.getEnv();
+        let commands = [command];
+        if (require("sdk/system").platform == "WINNT" && "PATHEXT" in env)
+        {
+            var pathExt = env.PATHEXT.split(/;|:/);
+            for (let ext of pathExt)
+                commands.push(command + ext);
+        }
+        
+        var paths = (env.PATH || "").split(/;|:/);
+        for (let path of paths)
+        {
+            for (let cmd of commands)
+            {
+                let _path = ioFile.join(path, cmd);
+                if (isExecutable(_path))
+                    return _path;
+            }
+        }
+        
+        return false;
+    }
+    
+    this.run = function(binary, args, opts)
+    {
         var _opts = {
             cwd: this.getCwd(),
-            env: env
+            env: this.getEnv()
         };
         
         var _ = require("contrib/underscore");
-        _opts = _.extend(_opts, opts)
+        _opts = _.extend(_opts, opts);
+        
+        binary = this.lookup(binary) || binary;
         
         // Start child process - `npm ls --depth=0`
         var proc = require("sdk/system/child_process");
