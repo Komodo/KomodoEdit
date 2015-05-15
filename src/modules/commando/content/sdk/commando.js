4e4dd356173628e5c6341547c5ae4b1d3ea10dac
Nathan Rijksen <n.rijksen@gmail.com>
2015-05-15 12:16:36 -0400

    Commando: Only capture alt+number, cancel on other keystrokes
    (integrated from master branch change b7dc1c7 by Nathan Rijksen <n.rijksen@gmail.com>)

--- src/modules/commando/content/sdk/commando.js
+++ src/modules/commando/content/sdk/commando.js
@@ -191,19 +191,30 @@
                 break;
         }
         
-        var numbers = [0,1,2,3,4,5,6,7,8,9];
-        for (let number of numbers)
+        if (local.altPressed && e.keyCode != KeyEvent.DOM_VK_ALT)
         {
-            if (e.keyCode != KeyEvent["DOM_VK_" + number])
-                continue;
+            var numberPressed = false;
+            var numbers = [0,1,2,3,4,5,6,7,8,9];
+            for (let number of numbers)
+            {
+                if (e.keyCode != KeyEvent["DOM_VK_" + number])
+                    continue;
+                
+                numberPressed = true;
+                prevDefault = true;
+                
+                if (local.altNumber)
+                    local.altNumber += number;
+                else
+                    local.altNumber = number.toString();
+            }
             
-            if (local.altNumber)
-                local.altNumber += number;
-            else
-                local.altNumber = number.toString();
+            if ( ! numberPressed)
+            {
+                local.altNumber = null;
+                local.altPressed = false;
+            }
         }
-        
-        if (local.altPressed) prevDefault = true;
 
         local.prevKeyCode = e.keyCode;
 
