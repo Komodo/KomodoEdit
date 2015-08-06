d0a7c97bd7ba80911976d3c9f24602aebd9d9ad2
Mitchell <mitchellb@activestate.com>
2015-08-06 12:39:15 -0400

    Revert unintentional commit to static branch.
    This reverts commit ca295f8131c538e97d1ce9e1d82070a5bda7913d.

--- src/codeintel/lib/codeintel2/langintel.py
+++ src/codeintel/lib/codeintel2/langintel.py
@@ -94,7 +94,7 @@ class LangIntel(object):
                 self._langinfo_cache = self.mgr.lidb.langinfo_from_komodo_lang(self.lang)
             except langinfo.LangInfoError, ex:
                 self._langinfo_cache = None
-                log.warn("Unable to retrieve langinfo for '%s'; no codeintel will be available for that language", self.lang)
+                log.exception("error getting langinfo for '%s'", self.lang)
         return self._langinfo_cache
 
     # Code Browser integration.
