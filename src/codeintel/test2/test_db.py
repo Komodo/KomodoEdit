f2d37adc238e74281c2debf8c5cccd82b1406883
Mitchell <mitchellb@activestate.com>
2015-09-22 12:16:12 -0400

    codeintel: Added more unit tests for CSS importer for handling Less and SCSS files.
    (integrated from master branch change 9.2.1-290-ge00f135 by Mitchell <mitchellb@activestate.com>)

--- src/codeintel/test2/test_db.py
+++ src/codeintel/test2/test_db.py
@@ -1558,7 +1558,9 @@ class MockProject(object):
             return dirname(self.path)
         
 class CSSTestCase(DBTestCase):
-    test_dir = join(os.getcwd(), "tmp", "css")
+    css_test_dir = join(os.getcwd(), "tmp", "css")
+    less_test_dir = join(os.getcwd(), "tmp", "less")
+    scss_test_dir = join(os.getcwd(), "tmp", "scss")
     
     def test_css_import_handler(self):
         """
@@ -1575,13 +1577,14 @@ class CSSTestCase(DBTestCase):
         Each CSS file contains an id selector followed by a class selector.
         This test verifies that all 3 CSS files have been loaded and scanned.
         """
-        writefile(join(self.test_dir, "foo.css"), "#foo { }\n.foobar { }\n")
-        writefile(join(self.test_dir, "bar.css"), "#bar { }\n.barfoo { }\n")
-        writefile(join(self.test_dir, "nested", "baz.css"), "#baz { }\n.bazfoo { }\n")
+        writefile(join(self.css_test_dir, "foo.css"), "#foo { }\n.foobar { }\n")
+        writefile(join(self.css_test_dir, "bar.css"), "#bar { }\n.barfoo { }\n")
+        writefile(join(self.css_test_dir, "nested", "baz.css"), "#baz { }\n.bazfoo { }\n")
         import_handler = self.mgr.citadel.import_handler_from_lang("CSS")
-        importables = import_handler.find_importables_in_dir(self.test_dir)
+        importables = import_handler.find_importables_in_dir(self.css_test_dir)
+        self.failUnless(len(importables.keys()) == 3)
         for name in ("foo.css", "bar.css", join("nested", "baz.css")):
-            buf = self.mgr.buf_from_path(join(self.test_dir, name), lang="CSS")
+            buf = self.mgr.buf_from_path(join(self.css_test_dir, name), lang="CSS")
             self.failUnless(buf)
             blob = buf.blob_from_lang["CSS"]
             self.failUnless(len(blob) == 2)
@@ -1593,29 +1596,62 @@ class CSSTestCase(DBTestCase):
         Identical to the above CSS test case, but with a mixture of CSS and
         Less files.
         """
-        writefile(join(self.test_dir, "foo.less"), "#foo(@c) when (iscolor(@c)) { }\n.foobar { }\n")
-        writefile(join(self.test_dir, "bar.css"), "#bar { }\n.barfoo { }\n")
-        writefile(join(self.test_dir, "nested", "baz.less"), "#baz:extend(.a, .b) { }\n.bazfoo { }\n")
+        writefile(join(self.less_test_dir, "foo.less"), "#foo(@c) when (iscolor(@c)) { }\n.foobar { }\n")
+        writefile(join(self.less_test_dir, "bar.css"), "#bar { }\n.barfoo { }\n")
+        writefile(join(self.less_test_dir, "nested", "baz.less"), "#baz:extend(.a, .b) { }\n.bazfoo { }\n")
         import_handler = self.mgr.citadel.import_handler_from_lang("CSS")
-        importables = import_handler.find_importables_in_dir(self.test_dir)
+        importables = import_handler.find_importables_in_dir(self.less_test_dir)
+        self.failUnless(len(importables.keys()) == 3)
         
-        buf = self.mgr.buf_from_path(join(self.test_dir, "foo.less"), lang="Less")
+        buf = self.mgr.buf_from_path(join(self.less_test_dir, "foo.less"), lang="Less")
         self.failUnless(buf)
         blob = buf.blob_from_lang["Less"]
         self.failUnless(len(blob) == 2)
         self.failUnless(blob.getchildren()[0].get("ilk") == "id")
         self.failUnless(blob.getchildren()[1].get("ilk") == "class")
         
-        buf = self.mgr.buf_from_path(join(self.test_dir, "bar.css"), lang="CSS")
+        buf = self.mgr.buf_from_path(join(self.less_test_dir, "bar.css"), lang="CSS")
         self.failUnless(buf)
         blob = buf.blob_from_lang["CSS"]
         self.failUnless(len(blob) == 2)
         self.failUnless(blob.getchildren()[0].get("ilk") == "id")
         self.failUnless(blob.getchildren()[1].get("ilk") == "class")
 
-        buf = self.mgr.buf_from_path(join(self.test_dir, join("nested", "baz.less")), lang="Less")
+        buf = self.mgr.buf_from_path(join(self.less_test_dir, join("nested", "baz.less")), lang="Less")
         self.failUnless(buf)
         blob = buf.blob_from_lang["Less"]
+        self.failUnless(len(blob) == 2)
+        self.failUnless(blob.getchildren()[0].get("ilk") == "id")
+        self.failUnless(blob.getchildren()[1].get("ilk") == "class")
+            
+    def test_scss_import_handler(self):
+        """
+        Identical to the above CSS test case, but with SCSS files.
+        """
+        writefile(join(self.scss_test_dir, "foo.scss"), "#foo { }\n.foobar { }\n")
+        writefile(join(self.scss_test_dir, "bar.scss"), "#bar { }\n.barfoo { }\n")
+        writefile(join(self.scss_test_dir, "nested", "baz.css.scss"), "#baz { }\n.bazfoo { }\n")
+        import_handler = self.mgr.citadel.import_handler_from_lang("CSS")
+        importables = import_handler.find_importables_in_dir(self.scss_test_dir)
+        self.failUnless(len(importables.keys()) == 3)
+        
+        buf = self.mgr.buf_from_path(join(self.scss_test_dir, "foo.scss"), lang="SCSS")
+        self.failUnless(buf)
+        blob = buf.blob_from_lang["SCSS"]
+        self.failUnless(len(blob) == 2)
+        self.failUnless(blob.getchildren()[0].get("ilk") == "id")
+        self.failUnless(blob.getchildren()[1].get("ilk") == "class")
+        
+        buf = self.mgr.buf_from_path(join(self.scss_test_dir, "bar.scss"), lang="SCSS")
+        self.failUnless(buf)
+        blob = buf.blob_from_lang["SCSS"]
+        self.failUnless(len(blob) == 2)
+        self.failUnless(blob.getchildren()[0].get("ilk") == "id")
+        self.failUnless(blob.getchildren()[1].get("ilk") == "class")
+
+        buf = self.mgr.buf_from_path(join(self.scss_test_dir, join("nested", "baz.css.scss")), lang="SCSS")
+        self.failUnless(buf)
+        blob = buf.blob_from_lang["SCSS"]
         self.failUnless(len(blob) == 2)
         self.failUnless(blob.getchildren()[0].get("ilk") == "id")
         self.failUnless(blob.getchildren()[1].get("ilk") == "class")
