7c9cff96124430b0fc6385d7e628dc27d684e538
Nathan Rijksen <n.rijksen@gmail.com>
2015-10-04 21:43:21 -0400

    SDK: Windows: Added getAll, getWidgetWindows, getBrowserWindows
    (integrated from master branch change 9.2.1-431-g24f4a29 by Nathan Rijksen <n.rijksen@gmail.com>)

--- src/chrome/komodo/content/sdk/windows.js
+++ src/chrome/komodo/content/sdk/windows.js
@@ -27,4 +27,64 @@
         return _window;
     }
     
+    this.getAll = function()
+    {
+        var windows = [];
+        
+        var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+        let enumerated = wm.getEnumerator("");
+        while (enumerated.hasMoreElements()) {
+            let w = enumerated.getNext().QueryInterface(Ci.nsIDOMWindow);
+            windows.push(w);
+            windows = windows.concat(this.getBrowserWindows(w, true));
+        }
+        
+        windows = windows.concat(this.getWidgetWindows());
+        
+        return windows;
+    }
+    
+    this.getWidgetWindows = function()
+    {
+        var windows = [];
+        var w = this.getMain();
+        for (let id of w.ko.widgets.widgets)
+        {
+            try
+            {
+                windows.push(w.ko.widgets.getWidget(id).contentWindow);
+            }
+            catch (e)
+            {
+                log.debug("Cannot access widget " + id);
+            }
+        }
+        
+        return windows;
+    }
+    
+    this.getBrowserWindows = function(_window, recursive = false)
+    {
+        _window = _window || window;
+        
+        var windows = [];
+        var browsers = _window.document.querySelectorAll("browser");
+        for (let browser of browsers)
+        {
+            try
+            {
+                let w = browser.contentWindow;
+                windows.push(w);
+                
+                if (recursive) windows = windows.concat(this.getBrowserWindows(w, true));
+            }
+            catch (e)
+            {
+                log.debug("Cannot access browser " + browser.id || null);
+            } 
+        }
+        
+        return windows;
+    }
+    
 }).apply(module.exports);
