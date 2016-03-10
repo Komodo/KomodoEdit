e822fea689dcdb2b378a182cc6d50c5c02d2bf9e
Carey Hoffman <cgchoffman@gmail.com>
2016-03-09 21:11:09 -0800

    Wizard: Save prefs to preferences wordwrap doesn't save because oncommand is never called tab width validation code doesn't DO anything with UI Wizard loads page1 before it loads it's introduction stuff Wizard onfinish will finish AND close the window even if it returns false.
    (integrated from master branch change 89f8754 by Carey Hoffman <cgchoffman@gmail.com>)

--- src/chrome/komodo/content/startupWizard/startupWizard.js
+++ src/chrome/komodo/content/startupWizard/startupWizard.js
@@ -19,7 +19,7 @@ var elems =
 
 // Save all pref settings to be applied when the Finish button is pressed at the
 // end of the wizard.  I don't want to half set prefs then have the user cancel
-var storedPrefs = {};
+var storedPrefs = [];
 
 function onLoad()
 {
@@ -34,8 +34,8 @@ function onLoad()
         attributes:
         {
             id: "page1",
-            title: "Keybindings",
-            description: "Pick a keybinding set you'll be most comfortable with using."
+            title: "Configure Komodo",
+            description: "Configure Komodo with basic editing settings that best fit you're workflow."
         }
     })
     
@@ -50,25 +50,31 @@ function onLoad()
     var keybindingMenulist = require("ko/ui/menulist").create({
         attributes:
         {
+            id: "keybinding-list",
             label:"Keybinding sets",
             width: 100,
             
         }
     });
-    var prefname = "keybinding_scheme";
+    var prefname = "keybinding-scheme";
     for (let i in allKeybindingConfigs)
     {
         keybindingMenulist.addMenuitem({
             attributes: {
                 label: allKeybindingConfigs[i],
-                oncommand: "return storePref([\"" + prefname + "\", \"" + allKeybindingConfigs[i] + "\"]);",
                 tooltiptext: "See Preferences > Keybindings for more details"
             }
         });
     }
+     keybindingMenulist.$elem.on("command", function()
+                             {
+                                storePref(['keybinding-scheme',
+                                           this.selectedItem.label, 'string']);
+                                keybindingService.getScheme(this.selectedItem.label);
+                             });
     page1.addRow(keybindingMenulist.$elem);
     
-    // Add toggles:
+    // ADD TOGGLES:
     // Native window borders,
     // minimap scroll
     // use tabs over spaces
@@ -77,41 +83,13 @@ function onLoad()
     // auto-abbreviations
     // tab completions
     var nativeWindowPrefs = ! prefs.getBoolean("ui.hide.chrome");
-    storePref(["ui_hide_chrome", nativeWindowPrefs]);
     var minimapPref = prefs.getBoolean("editShowMinimap");
-    storePref(["editShowMinimap", minimapPref])
     var useTabsPref = prefs.getBoolean("useTabs");
-    storePref(["useTabs", useTabsPref]);
     var useWordwrap = prefs.getLong("editWrapType");
-    storePref(["editWrapType", useWordwrap]);
     var wrapSelectPref = prefs.getBoolean("editSmartWrapSelection");
-    storePref(["editSmartWrapSelection", wrapSelectPref]);
     var enableAutoSnippetsPref = prefs.getBoolean("enableAutoAbbreviations");
-    storePref(["enableAutoAbbreviations", enableAutoSnippetsPref]);
     var enableTabComplete = prefs.getBoolean("editTabCompletes");
-    storePref(["editTabCompletes", enableTabComplete]);
-    
-    // word wrap can have 3 states.  No need to over complicate the
-    // UI so if they already have wordwrap set, keep it that on
-    // with the setting they had, eg. wrap on word or char (1 or 2)
-    // Assume 1 (wrap on word) if it was never set and user wants
-    // it set now.
-    var setWordWrapPref = function(){
-        if(this.checked) 
-            {
-                if(useWordwrap == 0)
-                storePref(["useTabs", useWordwrap]);
-            }
-            else if (this.checked)
-            {
-                storePref(["useTabs", 1]);
-            }
-            else
-            {
-                storePref(["useTabs", 0]);
-            }
-    }
-    
+
     var checkboxes =
     [
         {
@@ -119,7 +97,7 @@ function onLoad()
             attributes: {
                 checked: nativeWindowPrefs,
                 label: "Enable native window borders.",
-                oncommand:  "return storePref([\"ui_hide_chrome\", !this.checked])",
+                oncommand:  "return storePref([\"ui.hide.chrome\", !this.checked, \"bool\"])",
             }
         },
         {
@@ -127,7 +105,7 @@ function onLoad()
             attributes: {
                 checked: minimapPref,
                 label: "Enable Minimap scrolling.",
-                oncommand: "return storePref([\"editShowMinimap\", this.checked])",
+                oncommand: "return storePref([\"editShowMinimap\", this.checked, \"bool\"])",
             }
         },
         {
@@ -135,15 +113,16 @@ function onLoad()
             attributes: {
                 checked: useTabsPref,
                 label: "Prefer tab characters over spaces.",
-                oncommand: "return storePref([\"useTabs\", this.checked])",
+                oncommand: "return storePref([\"useTabs\", this.checked, \"bool\"])",
             }
         },
         {
             // Enable word wrap
             attributes: {
+                id: "wordwrap",
                 checked: useWordwrap == 0 ? false : true,
                 label: "Enable line wrapping on words.",
-                oncommand: "return setWordWrapPref();"
+                tooltiptext: "See Prefereces > Editor > Smart editing for more details",
             }
         },
         {
@@ -152,7 +131,7 @@ function onLoad()
                 checked: wrapSelectPref,
                 tooltiptext: "See Prefereces > Editor > Smart editing for more details",
                 label: "Wrap selection with typed delimiter",
-                oncommand: "return storePref([\"editSmartWrapSelection\", this.checked]);"
+                oncommand: "return storePref([\"editSmartWrapSelection\", this.checked, \"bool\"]);"
             }
         },
         {
@@ -161,7 +140,7 @@ function onLoad()
                 checked: enableAutoSnippetsPref,
                 tooltiptext: "See Prefereces > Editor > Smart editing: Auto-Abbreviations for more details",
                 label: "Enable auto snippets to trigger while typing.",
-                oncommand: "return storePref([\"enableAutoAbbreviations\", this.checked]);"
+                oncommand: "return storePref([\"enableAutoAbbreviations\", this.checked, \"bool\"]);"
             }
         },
         {
@@ -169,26 +148,51 @@ function onLoad()
                 checked: enableTabComplete,
                 tooltiptext: "See Prefereces > Editor > Smart editing for more details",
                 label: "Enable Tab key word completion.",
-                oncommand: "return storePref([\"editTabCompletes\", this.checked]);"
+                oncommand: "return storePref([\"editTabCompletes\", this.checked, \"bool\"]);"
             }
         }
     ]
-    
-    
     for(let i in checkboxes)
     {
         var checkbox = require("ko/ui/checkbox").
             create({ //options
                 attributes: checkboxes[i].attributes
             });
+        // Move checkbox command assignment here
+        // checkbox.$elem.on("command", function(){...});
         page1.addRow(checkbox.$elem);
     }
-    //// End adding checkboxes toggles
+    // word wrap can have 3 states.  No need to over complicate the
+    // UI so if they already have wordwrap set, keep it that on
+    // with the setting they had, eg. wrap on word or char (1 or 2)
+    // Assume 1 (wrap on word) if it was never set and user wants
+    // it set now.
+    
+    ///THIS NEVER GETS CALLED AND I DON"T KNOW WHY
+    $('#wordwrap').on("command",/* [useWordwrap],*/ function(event)
+                      {
+                         //if(this.checked) 
+                         //   {
+                         //       if(data == 0)
+                         //       storePref(["useTabs", event.data, "long"]);
+                         //   }
+                         //   else if (this.checked)
+                         //   {
+                         //       storePref(["useTabs", 1, "long"]);
+                         //   }
+                         //   else
+                         //   {
+                         //       storePref(["useTabs", 0, "long"]);
+                         //   }
+                         log.debug("Saving the word wrap pref");
+                      });
+    //// End ADD TOGGLES
     
     // Add browser preview choice
     var browserMenulist = require("ko/ui/menulist").create({
         attributes:
         {
+            id: "browser-list",
             label:"Browser Preview Browser",
             width: 200,
             
@@ -197,18 +201,91 @@ function onLoad()
     var koWebbrowser = Components.classes['@activestate.com/koWebbrowser;1'].
                    getService(Components.interfaces.koIWebbrowser);
     var knownBrowsers = koWebbrowser.get_possible_browsers({});
+//// #if PLATFORM == "win"
+//     browserMenulist.addMenuitem({
+//                                 attributes: {
+//                                    label: 'System defined default browser',
+//                                 }
+//     });
+//// #else
+//    browserMenulist.addMenuitem({
+//                                 attributes: {
+//                                    label: 'Ask when browser is launched the next time',
+//                                 }
+//     });
+//// #endif
+    
     for (let i in knownBrowsers)
     {
         browserMenulist.addMenuitem({
             attributes: {
                 label: knownBrowsers[i],
-                oncommand: "return storePref(['browser', \"" + knownBrowsers[i] + "\"]);",
                 tooltiptext:  knownBrowsers[i]
             }
         });
     }
+
+    browserMenulist.$elem.on("command", function(e)
+                             {
+                                storePref(['browser',
+                                           this.selectedItem.label,
+                                           'string']);
+                             });
     page1.addRow(browserMenulist.$elem);
+    // END BROWSER PREF
     
+    // ADD COLOR SCHEME PREF
+    var currentColorScheme = prefs.getString("editor-scheme");
+    var schemeService = Cc['@activestate.com/koScintillaSchemeService;1'].getService();
+    var schemes = new Array();
+    schemeService.getSchemeNames(schemes, new Object());
+    schemes = schemes.value;
+    var colorSchemeList = require("ko/ui/menulist").create({
+        attributes:
+        {
+            id: "schemelist",
+            label:"Choose an editor color scheme",
+            width: 200,
+            
+        }
+    });
+    for(let i in schemes)
+    {
+        var $menuitem = colorSchemeList.addMenuitem({
+            attributes: {
+                label: schemes[i],
+                //tooltiptext:  schemes[i]
+            }
+        });
+       
+        var s = schemeService.getScheme(schemes[i]);
+        $menuitem.css(
+            {
+                "font-family": s.getCommon("default_fixed", "face"),
+                color: s.getCommon("default_fixed", "fore"),
+                background: s.getCommon("default_fixed", "back")
+            }
+        );
+        $menuitem._isDark = s.isDarkBackground;
+
+    }
+    colorSchemeList.$elem.on("command", function()
+                             {
+                                storePref(['editor-scheme',
+                                           this.selectedItem.label, 'string']);
+                             });
+    // sort by dark scheme
+    colorSchemeList.$elem.children().each(function ()
+    {
+        if (this._isDark)
+        {
+            while (this.previousSibling && ! this.previousSibling._isDark) {
+                this.parentNode.insertBefore(this, this.previousSibling);
+            }
+        }
+    });
+    page1.addRow(colorSchemeList.$elem);
+    // END COLOR SCHEME PREFS
     
     // Add set tab width pref
     var tabWidth = prefs.getLong("tabWidth")
@@ -218,16 +295,50 @@ function onLoad()
         value: tabWidth,
         width: 20,
         tooltiptext: "tab/indent width in spaces",
-        onkeyup: function(){
-            log.error("the field is: " + this.value);
-        }
+        
     }
     var tabWidthTextfield = require("ko/ui/textbox").create(
         {attributes: textboxAttrs}
     )
+    tabWidthTextfield.$elem.on("keyup", function()
+        {
+            // check if the input is a number and greater than 0
+            if(/^\d+$/.test(this.value) && this.value > 0)
+            {
+                canFinish = true;
+                storePref(["tabWidth", this.value, "long"]);
+                storePref(["indentWidth", this.value, "long"]);
+                $(this).css("border-style", "none");
+                $("#tabWarning").remove();
+            }
+            else
+            {
+                // THIS IS NOT WORKING
+                try{
+                    canFinish = false;
+                    log.debug("THis is broken - 1");
+                    $("#tabWarning").remove();
+                    var $warningMsg = $($.create("hbox",
+                        {
+                            id:"tabWarning",
+                            value: "Input must be a number and greater than 0",
+                            text: "Input must be a number and greater than 0"
+                        }).toString());
+                    $warningMsg.css(
+                                    {
+                                        "color": "red",
+                                        "border-style": "solid"
+                                    });
+                    $(this).children().append($warningMsg);
+                    $(this).css("border-color", "red")
+                    log.debug("THis is broken - 2");
+                } catch (e){
+                    log.exception("Something went wrong: " + e);
+                }
+            }
+        });
     page1.addRow(tabWidthTextfield.$elem);
     // Done adding tab width prefs
-    
     
     //*
     // Here I'll just add page after page.  This will be long and ugly but the
@@ -247,8 +358,19 @@ function onLoad()
  */
 function storePref(prefset)
 {
-    storedPrefs[prefset[0]] = prefset[1];
-    log.debug("Storing pref: " + storedPrefs[prefset[0]]);
+    // Don't allow pref to be set multiple times
+    for(let i in storedPrefs)
+    {
+        if(storedPrefs[i][0] == prefset[0])
+        {
+            // replace existing saved prefset with the new set
+            storedPrefs.splice(i, 1, prefset);
+            log.debug("Storing pref: " + storedPrefs[storedPrefs.length - 1]);
+            return;
+        }
+    }
+    storedPrefs.push(prefset);
+    log.debug("Storing pref: " + storedPrefs[storedPrefs.length - 1]);
 }
 
 /**
@@ -257,10 +379,37 @@ function storePref(prefset)
  */
 function finished()
 {
-    // Apply pref settings saved in prefSet
-    //below sets the native border pref
-    //prefs.setBoolean("ui.hide.chrome",
-    //                 ! this.elem.element().checked);
+    log.debug("can finish: " + canFinish);
+    if(!canFinish)
+    {
+        // this doesn't work either...window just closes.
+        return false;
+    }
+    for(let i in storedPrefs)
+    {
+        // [name, value, type]
+        var prefset = storedPrefs[i];
+        switch(prefset[2])
+        {
+            case "bool":
+                log.debug("setting pref: " + prefset[0] + " to " + prefset[1])
+                prefs.setBoolean(prefset[0], prefset[1]);
+                break;
+            case "string":
+                log.debug("setting pref: " + prefset[0] + " to " + prefset[1])
+                prefs.setStringPref(prefset[0], prefset[1]);
+                break;
+            case "long":
+                log.debug("setting pref: " + prefset[0] + " to " + prefset[1])
+                prefs.setLong(prefset[0], prefset[1]);
+                break;
+            default:
+                log.warn("Could not set preferences: " + prefset[0] +
+                         " with value: " + prefset[1] +
+                         " and type: " + prefset[2]);
+        }
+    }
+    
     prefs.setBoolean("wizard.finished", true);
     var wizardComplete = new Event('wizard.complete');
     window.dispatchEvent(wizardComplete);
@@ -270,16 +419,12 @@ function finished()
 
 function ok()
 {
-    var wizardComplete = new Event('wizard.complete');
-    window.dispatchEvent(wizardComplete);
-    return;
+    return finished();
 }
 
 function cancel()
 {
- //    THIS MUST BE REMOVED!!!!
-    prefs.setBoolean("wizard.finished", true);
-    var wizardComplete = new Event('wizard.complete');
+    //var wizardComplete = new Event('wizard.complete');
     window.dispatchEvent(wizardComplete);
     
     return true;
