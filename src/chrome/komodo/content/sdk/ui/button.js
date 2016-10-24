93887f40923f232dd1fa053359a0999e616ea98f
Nathan Rijksen <n.rijksen@gmail.com>
2016-10-24 15:29:26 -0400

    SDK: (toolbar)button: Fold menu mechanic into the button SDK
    (integrated from the KomodoIDE master branch change ae88e8b by Nathan Rijksen <n.rijksen@gmail.com>)
    
    https://github.com/Komodo/KomodoIDE/commit/ae88e8b

--- src/chrome/komodo/content/sdk/ui/button.js
+++ src/chrome/komodo/content/sdk/ui/button.js
@@ -26,6 +26,91 @@ module.exports = Module;
             this.$element.on("command", callback);
         };
         
+         /**
+         * Check if the button has the menu-button type set
+         *
+         * @returns {boolean}
+         */
+        this.isMenuButton = function ()
+        {
+            if(this.element.getAttribute("type") != "menu-button" && this.element.getAttribute("type") != "menu")
+            {
+                var log = require("ko/logging").getLogger("ko/ui/button");
+                log.warn("button must have 'type: menu-button' to use " +
+                         "'addMenuItem/s' functions.");
+                return false;
+            }
+            return true;
+        };
+        
+         /**
+          * Add items to the container
+          *
+          * @argument {Array[string, ko/ui/obj, ko/dom/obj, DOM, Object]} item item to be added
+          * to the container.
+          *
+          * Basically, whatever addMenuItem can handle, this can handle it's array
+          *
+          * Object refers to an Options object used through this SDK. The options
+          * should contain an attributes property to assign a label at the very
+          * least:
+          *
+          *  {
+          *      attributes:
+          *      {
+          *          label:"itemLable"
+          *      }
+          *  }
+          */ 
+        this.addMenuItems = function (menuitems)
+        {
+            if( ! this.isMenuButton() )
+            {
+                return;
+            }
+            for (let menuitem of menuitems) {
+                this.addMenuItem(menuitem);
+            }
+        };
+        
+        /**
+         * Add an item to the container
+         *
+         * @argument {string, ko/ui/obj, ko/dom/obj, DOM, Object} item item to be added
+         * to the container.
+         *
+         * Object refers to an Options object used through this SDK. The options
+         * should contain an attributes property to assign a label at the very
+         * least:
+         *
+         *  {
+         *      attributes:
+         *      {
+         *          label:"itemLable"
+         *      }
+         *  }
+         */ 
+        this.addMenuItem = function (menuitem)
+        {
+            if( ! this.isMenuButton() )
+            {
+                return;
+            }
+            
+            if ( ! this.menupopup )
+            {
+                this.menupopup = require("./menupopup").create();
+                this.element.appendChild(this.menupopup.element);
+            }
+            
+            var element = this.menupopup.addMenuItem(menuitem);
+            
+            if (element.getAttribute("selected") == "true" && "selectedItem" in this.element)
+            {
+                this.element.selectedItem = element;
+            }
+        };
+        
     }).apply(this.Model); 
     
 }).apply(Module);
--- src/chrome/komodo/content/sdk/ui/toolbarbutton.js
+++ src/chrome/komodo/content/sdk/ui/toolbarbutton.js
@@ -6,9 +6,8 @@
  *
  */
 
-var parent = require("./element");
+var parent = require("./button");
 var Module = Object.assign({}, parent);
-const log = require("ko/logging").getLogger("toolbarbutton");
 module.exports = Module;
 
 // Main module (module.exports)
@@ -19,97 +18,7 @@ module.exports = Module;
     (function() {
         
         this.name = "toolbarbutton";
-        this.init = this.initWithLabel;
-        this.menupopup = null;
         
-        this.onCommand = function (callback)
-        {
-            this.$element.on("command", callback);
-        };
-        
-         /**
-         * Check if the button has the menu-button type set
-         *
-         * @returns {boolean}
-         */
-        this.isMenuButton = function ()
-        {
-            if( ! this.element.type ==="menu-button" )
-            {
-                log.warn("Toolbarbutton must have 'type: menu-button' to use " +
-                         "'addMenuItem/s' functions.");
-                return false;
-            }
-            return true;
-        };
-        
-         /**
-          * Add items to the container
-          *
-          * @argument {Array[string, ko/ui/obj, ko/dom/obj, DOM, Object]} item item to be added
-          * to the container.
-          *
-          * Basically, whatever addMenuItem can handle, this can handle it's array
-          *
-          * Object refers to an Options object used through this SDK. The options
-          * should contain an attributes property to assign a label at the very
-          * least:
-          *
-          *  {
-          *      attributes:
-          *      {
-          *          label:"itemLable"
-          *      }
-          *  }
-          */ 
-        this.addMenuItems = function (menuitems)
-        {
-            if( ! this.isMenuButton() )
-            {
-                return;
-            }
-            for (let menuitem of menuitems) {
-                this.addMenuItem(menuitem);
-            }
-        };
-        
-        /**
-         * Add an item to the container
-         *
-         * @argument {string, ko/ui/obj, ko/dom/obj, DOM, Object} item item to be added
-         * to the container.
-         *
-         * Object refers to an Options object used through this SDK. The options
-         * should contain an attributes property to assign a label at the very
-         * least:
-         *
-         *  {
-         *      attributes:
-         *      {
-         *          label:"itemLable"
-         *      }
-         *  }
-         */ 
-        this.addMenuItem = function (menuitem)
-        {
-            if( ! this.isMenuButton() )
-            {
-                return;
-            }
-            
-            if ( ! this.menupopup )
-            {
-                this.menupopup = require("./menupopup").create();
-                this.element.appendChild(this.menupopup.element);
-            }
-            
-            var element = this.menupopup.addMenuItem(menuitem);
-            
-            if (element.getAttribute("selected") == "true" && "selectedItem" in this.element)
-            {
-                this.element.selectedItem = element;
-            }
-        };
     }).apply(this.Model); 
     
 }).apply(Module);
