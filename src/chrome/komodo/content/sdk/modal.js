e59651a39195d21dfbcaed394148e39a1afab357
Nathan Rijksen <n.rijksen@gmail.com>
2016-11-07 11:48:42 -0500

    SDK: Added the modal SDK
    (integrated from the KomodoIDE master branch change b0cfab4 by Nathan Rijksen <n.rijksen@gmail.com>)
    
    https://github.com/Komodo/KomodoIDE/commit/b0cfab4

---
+++ src/chrome/komodo/content/sdk/modal.js
@@ -0,0 +1,109 @@
+(function()
+{
+    
+    this.open = (title, fields, callback, okLabel, cancelLabel) =>
+    {
+        okLabel = okLabel || "Ok";
+        cancelLabel = cancelLabel || callback ? "Cancel" : "Close";
+        
+        var panel = require("ko/ui/panel").create({
+            attributes: {
+                backdrag: true,
+                noautohide: true,
+                width: "450px",
+                class: "dialog modal-ui"
+            }
+        });
+        
+        panel.addRow(
+            require("ko/ui/label").create(title),
+            { attributes: { align: "center", pack: "center", class: "ui-title" } }
+        );
+        
+        for (let key in fields)
+        {
+            let row = panel.addRow();
+            let field = fields[key];
+            
+            if (field.label)
+                row.add(require("ko/ui/label").create(field.label + ":"));
+            
+            let elem = require("ko/ui/" + (field.type || "textbox")).create(field.options || undefined);
+            row.add(elem);
+            
+            field.elem = elem;
+            
+            if (field.entries && elem.entries)
+                elem.entries(field.entries);
+            
+            if (field.value && elem.value)
+                elem.value(field.value);
+        }
+        
+        var errorLabel = require("ko/ui/label").create({ attributes: { class: "fullwidth state-error" } });
+        errorLabel.hide();
+        panel.addRow(
+            errorLabel,
+            { attributes: { align: "center", pack: "center", class: "ui-error" } }
+        );
+        
+        var buttonRow = panel.addRow({ attributes: { align: "center", pack: "center" } });
+        buttonRow.addClass("buttons-ui");
+        
+        if (callback)
+        {
+            var okButton = require("ko/ui/button").create(okLabel);
+            okButton.onCommand(onFormComplete.bind(this, panel, fields, callback));
+            buttonRow.add(okButton);
+        }
+        
+        var cancelButton = require("ko/ui/button").create(cancelLabel);
+        cancelButton.onCommand(function()
+        {
+            panel.remove();
+            
+            if (callback)
+                callback();
+        });
+        buttonRow.add(cancelButton);
+        
+        panel.open();
+    };
+    
+    var onFormComplete = (panel, fields, callback) =>
+    {
+        var result = {};
+        var hasResults = false;
+        var missing = [];
+        
+        for (let key in fields)
+        {
+            hasResults = true;
+            
+            if ("value" in fields[key].elem)
+                result[key] = fields[key].elem.value();
+            else
+                result[key] = null;
+            
+            if (fields[key].required && (result[key] === null || result[key] === ""))
+                missing.push(fields[key].label);
+        }
+        
+        if (missing.length)
+        {
+            var error = "Please enter a value for: " + missing.join(", ");
+            panel.$element.find(".ui-error label").show().attr("value", error);
+            return;
+        }
+        
+        if ( ! hasResults)
+            result = true;
+        
+        var validate = callback(result);
+        if (validate !== true && validate !== undefined)
+            panel.$element.find(".ui-error label").show().attr("value", validate);
+        else
+            panel.remove();
+    };
+    
+}).apply(module.exports);
