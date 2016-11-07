f29c3bcdd6cca94bb8853d5d2e67a19249eeb1d3
Nathan Rijksen <n.rijksen@gmail.com>
2016-11-07 11:49:25 -0500

    SDK: UI: Added the filepath UI SDK
    (integrated from the KomodoIDE master branch change 1d23e38 by Nathan Rijksen <n.rijksen@gmail.com>)
    
    https://github.com/Komodo/KomodoIDE/commit/1d23e38

---
+++ src/chrome/komodo/content/sdk/ui/filepath.js
@@ -0,0 +1,63 @@
+/**
+ * @copyright (c) 2016 ActiveState Software Inc.
+ * @license Mozilla Public License v. 2.0
+ * @author NathanR, CareyH
+ */
+
+var parent = require("./container");
+var Module = Object.assign({}, parent);
+module.exports = Module;
+
+(function() {
+    
+    this.Model = Object.assign({}, this.Model);
+    
+    (function() {
+        
+        this.name = "hbox";
+        this.attributes = { class: "ui-filepath" };
+        
+        this.init = function()
+        {
+            this.textbox = require("ko/ui/textbox").create();
+            
+            this.defaultInit.apply(this, arguments);
+            
+            var button = require("ko/ui/button").create("...", { attributes: { flex: 1 } });
+            button.onCommand(() =>
+            {
+                var ko = require("ko/windows").getMain().ko;
+                
+                var type = this.options.type || "file";
+                var value = this.textbox.value();
+                
+                switch (type)
+                {
+                    default:
+                    case "file":
+                        this.textbox.value(ko.filepicker.browseForFile(value));
+                        break;
+                    case "dir":
+                        this.textbox.value(ko.filepicker.browseForDir(value));
+                        break;
+                    case "exe":
+                        this.textbox.value(ko.filepicker.browseForExeFile(value));
+                        break;
+                    case "files":
+                        this.textbox.value(ko.filepicker.browseForFiles(value));
+                        break;
+                    case "remote":
+                        this.textbox.value(ko.filepicker.browseForRemoteDir(value));
+                        break;
+                }
+            });
+            
+            this.addElement(this.textbox);
+            this.addElement(button);
+        };
+        
+        this.value = function() { return this.textbox.value.apply(this.textbox, arguments); };
+        
+    }).apply(this.Model);
+    
+}).apply(Module);
