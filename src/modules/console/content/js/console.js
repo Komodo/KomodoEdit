8e8a694d1907311b983c863d31881c46d448dfd4
Defman21 <i@defman.me>
2017-04-06 23:44:51 +0300

    Console: don't save empty strings to history so it won't break; fixed #2458
    Signed-off-by: Defman21 <i@defman.me>

--- src/modules/console/content/js/console.js
+++ src/modules/console/content/js/console.js
@@ -201,11 +201,12 @@ window.app = {};
             
             if (result !== undefined) this.print("output", result);
         }
-        
-        history.unshift(input);
-        history = history.slice(0, 50);
-        historyPos = -1;
-        prefs.setString("console_history", JSON.stringify(history));
+         if (input.replace(/\s+/g, '') != "") {
+            history.unshift(input);
+            history = history.slice(0, 50);
+            historyPos = -1;
+            prefs.setString("console_history", JSON.stringify(history));
+         }
         
         elem.input.value = "";
     }
