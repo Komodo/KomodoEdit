26c1e6b22ae5b2545ac758380fbbc2c594067712
Nathan Rijksen <n.rijksen@gmail.com>
2017-05-29 17:48:37 -0400

    SDK: Prefs: Handle caching in the SDK
    (integrated from the KomodoIDE master branch change 72c36da3ef by Nathan Rijksen <n.rijksen@gmail.com>)
    
    https://github.com/Komodo/KomodoIDE/commit/72c36da3ef

--- src/chrome/komodo/content/sdk/prefs.js
+++ src/chrome/komodo/content/sdk/prefs.js
@@ -7,223 +7,383 @@
 
 const {Cc, Ci} = require("chrome");
 const w = require("ko/windows").getMain();
+const log = require("ko/logging").getLogger("ko/prefs");
+
+log.setLevel(10);
+
+const LEVEL_GLOBAL = 'global';
+const LEVEL_PROJECT = 'project';
+const LEVEL_FILE = 'file';
+
+var storage = require("ko/session-storage").get("pref-catagories").storage
+
+var PreferenceSet = function(prefset, level = LEVEL_GLOBAL)
+{
+    var cache = {};
+
+    var init = () =>
+    {
+        /**
+         * Wrap the Komodo global preferences (XPCOM) object.
+         */
+        for (let name of Object.keys(prefset.__proto__))
+        {
+            if (name == "QueryInterface")
+                continue;
+            if (name in this)
+                continue;
+            if (typeof(prefset[name]) == "function")
+            {
+                this[name] = prefset[name].bind(prefset);
+            }
+            else
+            {
+                // Wrap in a closure, so name (n) remains the same.
+                ((n) =>
+                {
+                    Object.defineProperty(this, n,
+                    {
+                        get: function() { return prefset[n]; },
+                        set: function(newValue) { prefset[n] = newValue; },
+                    });
+                })(name);
+            }
+        }
+    };
+
+    var cached = (method, name, fallback) =>
+    {
+        log.debug(`Calling ${method}(${name}, ${fallback})`);
+
+        var baseMethod = `${method}Pref`.replace('PrefPref', '');
+
+        if (baseMethod != method && fallback === undefined)
+        {
+            switch (method)
+            {
+                case "getString":
+                    fallback = "";
+                    break;
+                case "getBoolean":
+                    fallback = false;
+                    break;
+                case "getLong":
+                    fallback = 0;
+                    break;
+            }
+        }
+
+        var result;
+        if (name in cache)
+        {
+            log.debug("Using cache");
+            result = cache[name];
+        }
+        else
+        {
+            log.debug("Calling koPref component");
+
+            try
+            {
+                result = prefset[baseMethod](name);
+            }
+            catch(e)
+            {
+                result = null;
+            }
+
+            cache[name] = result;
+        }
+
+        if (result === null)
+        {
+            if (fallback === undefined)
+                throw Error(`The preference '${name}' does not exist in '${level}'`);
+
+            log.debug("Returning fallback");
+            return fallback;
+        }
+
+        return result;
+    };
+
+    /**
+     * Check if the preference exists (is defined)
+     *
+     * @function hasPref
+     *
+     * @param {String}      name     The preference name
+     *
+     * @returns {Boolean}
+     */
+    this.hasPref = (name) =>
+    {
+        if (name in cache && cache[name] !== null)
+            return true;
+        return prefset.hasPref.apply(prefset, arguments);
+    };
+
+    /**
+     * Check if the preference exists (is defined) on the current scope (not the parent scopes)
+     *
+     * @function hasPrefHere
+     *
+     * @param {String}      name     The preference name
+     *
+     * @returns {Boolean}
+     */
+    // Handled by init logic
+
+    /**
+     * Get a string preference, defaults to an empty string
+     *
+     * @function getString
+     *
+     * @param {String}      name     The preference name
+     * @param {String}      default  The default to return if the pref was not set
+     *
+     * @returns {String}
+     */
+    this.getString = cached.bind(this, "getString");
+
+    this.getStringPref = cached.bind(this, "getStringPref");
+
+    /**
+     * Set a string preference
+     *
+     * @function setString
+     *
+     * @param {String}      name    The preference name
+     * @param {String}      value   The preference value
+     */
+    // Handled by init logic
+
+    /**
+     * Get a boolean preference, defaults to false
+     *
+     * @function getBoolean
+     *
+     * @param {String}      name     The preference name
+     * @param {Boolean}     default  The default to return if the pref was not set
+     *
+     * @returns {Boolean}
+     */
+    this.getBoolean = cached.bind(this, "getBoolean");
+
+    this.getBooleanPref = cached.bind(this, "getBooleanPref");
+
+    /**
+     * Set a boolean preference
+     *
+     * @function setBoolean
+     *
+     * @param {String}      name    The preference name
+     * @param {Boolean}     value   The preference value
+     *
+     * @returns {Boolean}
+     */
+    // Handled by init logic
+
+    /**
+     * Get a long (integer) preference, defaults to 0
+     *
+     * @function getLong
+     *
+     * @param {String}      name     The preference name
+     * @param {Long}        default  The default to return if the pref was not set
+     *
+     * @returns {Long}
+     */
+    this.getLong = (name) => cached.bind(this, "getLong");
+
+    this.getLongPref = cached.bind(this, "getLongPref");
+
+    /**
+     * Set a long (integer) preference
+     *
+     * @function setLong
+     *
+     * @param {String}      name    The preference name
+     * @param {Long}        value   The preference value
+     *
+     * @returns {Long}
+     */
+    // Handled by init logic
+
+    /**
+     * Get a child prefset
+     *
+     * @function getPref
+     *
+     * @param {String}      name     The preference name
+     *
+     * @returns {prefset}
+     */
+    this.getPref = cached.bind(this, "getPref");
+
+    /**
+     * Set a prefset value
+     *
+     * @function setPref
+     *
+     * @param {String}      name    The preference name
+     * @param {prefset}     value   The preference value
+     *
+     * @returns {void}
+     */
+    // Handled by init logic
+
+    var observer =
+    {
+        observing: {},
+        observe: function(subject, topic, data)
+        {
+            if (topic in cache)
+            {
+                delete cache[topic];
+            }
+
+            observer.observing[topic].forEach(function(callback)
+            {
+                callback(subject, topic, data);
+            });
+        }
+    };
+
+    /**
+     * Handle change event for a pref
+     *
+     * @function onChange
+     *
+     * @param {String}      name     The preference name
+     * @param {Function}    callback
+     *
+     * @returns {void}
+     */
+    this.onChange = function(pref, callback)
+    {
+        if ( ! (pref in observer.observing))
+        {
+            observer.observing[pref] = [];
+            prefs.prefObserverService.addObserver(observer, pref, false);
+        }
+
+        observer.observing[pref].push(callback);
+    };
+
+    /**
+     * Remove a callback handler for a pref change event
+     *
+     * @function removeOnChange
+     *
+     * @param {String}      name     The preference name
+     * @param {Function}    callback 
+     *
+     * @returns {void}
+     */
+    this.removeOnChange = function(pref, callback)
+    {
+        if ( ! (pref in observer.observing)) return;
+
+        observer.observing[pref].forEach(function(_callback, index)
+        {
+            if (callback == _callback)
+            {
+                observer.observing[pref].splice(index,1);
+
+                if ( ! observer.observing[pref].length)
+                {
+                    delete observer.observing[pref];
+                    prefs.prefObserverService.removeObserver(observer, pref, false);
+                    return false;
+                }
+            }
+        });
+    };
+
+    init();
+};
 
 /**
- * Wrap the Komodo global preferences (XPCOM) object.
- */
-
-/**
- * The prefs SDK allows you to access Komodo's preferences
+ * The prefs SDK allows you to access Komodo's preferences, it returns an
+ * instance of PreferenceSet
  *
  * @module ko/prefs
  */
-var exports = module.exports;
-var prefs = Cc["@activestate.com/koPrefService;1"].
-                getService(Ci.koIPrefService).prefs;
-for (var name of Object.keys(prefs.__proto__)) {
-    if (name == "QueryInterface") {
-        continue;
-    }
-    if (typeof(prefs[name]) == "function") {
-        exports[name] = prefs[name].bind(prefs);
-    } else {
-        // Wrap in a closure, so name (n) remains the same.
-        (function(n) {
-            Object.defineProperty(exports, n, {
-                get: function() { return prefs[n]; },
-                set: function(newValue) { prefs[n] = newValue; },
-            });
-        })(name);
-    }
-}
+module.exports = new PreferenceSet(
+    Cc["@activestate.com/koPrefService;1"].getService(Ci.koIPrefService).prefs
+);
 
-/**
- * Register a preference set to be displayed in the prefs window
- *
- * @param {String}      name    The preference name that will be displayed in the preferences window
- * @param {String}      path    The chrome path to your *.xul file
- * @param {String}      insertAfter     ID of the element to insert after
- */
-exports.registerCatagory = function(name, path, insertAfter=null )
+(function()
 {
-    let id = name.replace(/\s/g,"_");
-    let prefCatagoryStorage = require("ko/simple-storage").get("pref-catagories").storage;
-    if ( ! prefCatagoryStorage.registered )
+    /**
+     * Return the current projects prefs
+     * Falls back to global if there is no project open
+     *
+     * @function project
+     *
+     * @returns {PreferenceSet}
+     */
+    this.project = () =>
     {
-        prefCatagoryStorage.registered = [];
-        prefCatagoryStorage.catagories = [];
-    }
-    if ( prefCatagoryStorage.registered.indexOf(path) < 0)
-    {
-        prefCatagoryStorage.registered.push(path);
-        prefCatagoryStorage.catagories.push({name:name,path:path,id:id,insertAfter:insertAfter});
-    }
-};
-
-exports.getRegisteredCatagories = function()
-{
-    return require("ko/simple-storage").get("pref-catagories").storage.catagories;
-};
-
-var observer =
-{
-    observing: {},
-    observe: function(subject, topic, data)
-    {
-        observer.observing[topic].forEach(function(callback)
+        var prefs;
+        if(w.ko.projects.manager.currentProject)
         {
-            callback(subject, topic, data);
-        });
-    }
-}
-
-exports.onChange = function(pref, callback)
-{
-    if ( ! (pref in observer.observing))
-    {
-        observer.observing[pref] = [];
-        prefs.prefObserverService.addObserver(observer, pref, false);
-    }
-
-    observer.observing[pref].push(callback);
-}
-
-exports.removeOnChange = function(pref, callback)
-{
-    if ( ! (pref in observer.observing)) return;
-
-    observer.observing[pref].forEach(function(_callback, index)
-    {
-        if (callback == _callback)
-        {
-            observer.observing[pref].splice(index,1);
-
-            if ( ! observer.observing[pref].length)
-            {
-                delete observer.observing[pref];
-                prefs.prefObserverService.removeObserver(observer, pref, false);
-                return false;
-            }
+            return new PreferenceSet(w.ko.projects.manager.currentProject.prefset, LEVEL_PROJECT);
         }
-    });
-}
+        else
+        {
+            return this;
+        }
+    };
 
-/**
- * Return the current projects prefs
- * Falls back to global if there is no project open
- */
-exports.project = function()
-{
-    var prefs;
-    if(w.ko.projects.manager.currentProject)
+    /**
+     * Return the current files prefs
+     * Falls back to project level if there is no file open
+     * Then Falls back to global level if there is no project open
+     *
+     * @function file
+     *
+     * @returns {PreferenceSet}
+     */
+    this.file = function()
     {
-        return w.ko.projects.manager.currentProject.prefset;
-    }
-    else
+        if(w.ko.views.manager.currentView)
+        {
+            return new PreferenceSet(w.ko.views.manager.currentView.koDoc.prefs, LEVEL_FILE);
+        }
+        else
+        {
+            return this.project();
+        }
+    };
+        
+    /**
+     * Register a preference set to be displayed in the prefs window
+     *
+     * @param {String}      name    The preference name that will be displayed in the preferences window
+     * @param {String}      path    The chrome path to your *.xul file
+     * @param {String}      insertAfter     ID of the element to insert after
+     */
+    this.registerCatagory = function(name, path, insertAfter=null )
     {
-        return exports;
-    }
-};
-
-/**
- * Return the current files prefs
- * Falls back to project level if there is no file open
- * Then Falls back to global level if there is no project open
- */
-exports.file = function()
-{
-    if(ko.views.manager.currentView)
+        let id = name.replace(/\s/g,"_");
+        let prefCatagoryStorage = storage;
+        if ( ! prefCatagoryStorage.registered )
+        {
+            prefCatagoryStorage.registered = [];
+            prefCatagoryStorage.catagories = [];
+        }
+        if ( prefCatagoryStorage.registered.indexOf(path) < 0)
+        {
+            prefCatagoryStorage.registered.push(path);
+            prefCatagoryStorage.catagories.push({ name: name, path: path, id: id, insertAfter: insertAfter });
+        }
+    };
+    
+    this.getRegisteredCatagories = function()
     {
-        return w.ko.views.manager.currentView.koDoc.prefs;
-    }
-    else
-    {
-        return exports.project();
-    }
-};
+        return storage.catagories;
+    };
 
-/**
- * Check if the preference exists (is defined)
- *
- * @function hasPref
- *
- * @param {String}      name     The preference name
- *
- * @returns {Boolean}
- */
-
-/**
- * Check if the preference exists (is defined) on the current scope (not the parent scopes)
- *
- * @function hasPrefHere
- *
- * @param {String}      name     The preference name
- *
- * @returns {Boolean}
- */
-
-/**
- * Get a string preference, defaults to an empty string
- *
- * @function getString
- *
- * @param {String}      name     The preference name
- * @param {String}      default  The default to return if the pref was not set
- *
- * @returns {String}
- */
-
-/**
- * Set a string preference
- *
- * @function setString
- *
- * @param {String}      name    The preference name
- * @param {String}      value   The preference value
- */
-
-
-/**
- * Get a boolean preference, defaults to false
- *
- * @function getBoolean
- *
- * @param {String}      name     The preference name
- * @param {Boolean}     default  The default to return if the pref was not set
- *
- * @returns {Boolean}
- */
-
-/**
- * Set a boolean preference
- *
- * @function setBoolean
- *
- * @param {String}      name    The preference name
- * @param {Boolean}     value   The preference value
- *
- * @returns {Boolean}
- */
-
-/**
- * Get a long (integer) preference, defaults to 0
- *
- * @function getLong
- *
- * @param {String}      name     The preference name
- * @param {Long}        default  The default to return if the pref was not set
- *
- * @returns {Long}
- */
-
-/**
- * Set a long (integer) preference
- *
- * @function setLong
- *
- * @param {String}      name    The preference name
- * @param {Long}        value   The preference value
- *
- * @returns {Long}
- */
-
+}).apply(module.exports);
