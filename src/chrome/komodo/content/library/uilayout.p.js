421be25467d8897e2a38ce31af26753e0b787a34
Nathan Rijksen <n.rijksen@gmail.com>
2017-07-07 15:15:42 -0400

    UX: Added a version check window, which will notify people if a new major version of Komodo is available
    rn=
    
    (integrated from the KomodoIDE 10.2.x branch change a0c090bb66 by Nathan Rijksen <n.rijksen@gmail.com>)
    
    https://github.com/Komodo/KomodoIDE/commit/a0c090bb66

--- src/chrome/komodo/content/library/uilayout.p.js
+++ src/chrome/komodo/content/library/uilayout.p.js
@@ -1785,6 +1785,15 @@ this.onload = function uilayout_onload()
         else
             window.addEventListener("toolbox-ready", runTutorial);
     }
+
+    try
+    {
+        ko.launch.newVersionCheck();
+    }
+    catch (e)
+    {
+        log.exception(e, "newVersionCheck failed");
+    }
 }
 
 this.updateWindowButtons = function (w) {
