ead11847b0ce8e9b8f1703d25323d5e3eb433ba7
Nathan Rijksen <n.rijksen@gmail.com>
2017-07-13 12:25:54 -0400

    SDK: UI: Allow richlist items to be added with just a string
    (integrated from the KomodoIDE master branch change 1273b11b56 by Nathan Rijksen <n.rijksen@gmail.com>)
    
    https://github.com/Komodo/KomodoIDE/commit/1273b11b56

--- src/chrome/komodo/content/sdk/ui/richlistbox.js
+++ src/chrome/komodo/content/sdk/ui/richlistbox.js
@@ -82,7 +82,10 @@ module.exports = Module;
         this.addListItem = function (item)
         {
             var element;
-            if ("isSdkElement" in item)
+            if (typeof item == "string") {
+                element = require("ko/ui/richlistitem").create(item).element;
+            }
+            else if ("isSdkElement" in item)
             {
                 element = item.element;
             }
--- src/chrome/komodo/content/sdk/ui/richlistitem.js
+++ src/chrome/komodo/content/sdk/ui/richlistitem.js
@@ -19,7 +19,32 @@ module.exports = Module;
 
         this.name = "richlistitem";
 
-        this.init = this.initWithElement;
+        this.init = function(label, options = {})
+        {
+            if (label && typeof label == "object")
+            {
+                options = label;
+                label = null;
+            }
+            
+            if (options.label)
+            {
+                label = options.label;
+            }
+            
+            this.defaultInit(options);
+
+            if (label)
+            {
+                var labelElem = require("ko/ui/label").create(label);
+                this.addElement(labelElem);
+            }
+            
+            if ( ! this.attributes.value && label)
+            {
+                this.$element.attr("value", label);
+            }
+        };
 
     }).apply(this.Model);
 
