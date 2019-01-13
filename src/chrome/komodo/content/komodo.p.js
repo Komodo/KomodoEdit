bac888a4febb8b3751050b4891621ead84910d6d
Stephen Sigwart <stephen@runsignup.com>
2019-01-13 12:04:42 -0500

    Remove undefined function call resulting in:
    [ERROR] console-logger: ReferenceError: authenticateUser is not defined (2) in chrome://komodo/content/komodo.js:477

--- src/chrome/komodo/content/komodo.p.js
+++ src/chrome/komodo/content/komodo.p.js
@@ -474,7 +474,8 @@ function onloadDelay() {
             xtk.domutils.fireEvent(window, "komodo-post-startup");
             require("ko/profiler").save("event-post-startup");
             require("ko/profiler").stop("event-post-startup");
-            authenticateUser(); 
+            // Seems to a leftover from IDE.  There is no such function.
+            // authenticateUser();
         }, 2500);
 
 // #if BUILD_FLAVOUR == "dev"
